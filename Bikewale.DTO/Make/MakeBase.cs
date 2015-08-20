727cb21b2e60f3cf4708c2374b806b0aba021823
unknown <ashish.kamble@user177.aepl.com>
2015-08-20 17:06:03 +0530

    Added 1st API

---
+++ Bikewale.DTO/Make/MakeBase.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.DTO.Make
+{
+    public class MakeBase
+    {
+        public int MakeId { get; set; }
+        public string MakeName { get; set; }
+        public string MaskingName { get; set; }
+    }
+}
