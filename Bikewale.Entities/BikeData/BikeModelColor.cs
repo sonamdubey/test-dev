64b1f9040fd185a7b1cb4c91237db9b339af46d9
unknown <sumit.kate@user358.aepl.com>
2015-09-08 14:55:04 +0530

    Added missing data for model page controller and merged the BW and DPQ in one response

---
+++ Bikewale.Entities/BikeData/BikeModelColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.Entities.BikeData
+{
+    public class BikeModelColor
+    {
+        public uint ModelId { get; set; }
+        public string ColorName { get; set; }
+        public string HexCode { get; set; }
+    }
+}
