c255506182329be5344e081ed06d579e214b0cf4
unknown <sumit.kate@user358.aepl.com>
2015-12-21 10:52:59 +0530

    removed the old methods of BWHttpClient

--- Bikewale.Utility/BWHttpClient.cs
+++ Bikewale.Utility/BWHttpClient.cs
@@ -14,76 +14,11 @@ namespace Bikewale.Utility
 {
     /// <summary>
     /// Modified By     :   Ashish
+    /// Modified By     :   Sumit Kate on 23 Dec 2015
+    /// Description     :   Removed the Old methods
     /// </summary>
     public sealed class BWHttpClient : IDisposable
     {
-        /// <summary>
-        /// Written By : Ashwini Todkar on 7 Oct 2014
-        /// Summary    : Method to get data from web api
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType">Json,text,xml or application</param>
-        /// <param name="apiUrl"></param>
-        /// <param name="responseType"></param>
-        /// <returns></returns>
-        public async Task<T> GetApiResponse<T>(string hostUrl, string requestType, string apiUrl, T responseType)
-        {
-            T objTask = default(T);
-            HttpClient httpClient = null;
-            #region commented old code
-            //using (var client = CreateHttpClient())
-            //{
-            //        // New code:       
-            //        //sets the base URI for HTTP requests
-            //        client.BaseAddress = new Uri(hostUrl);
-            //        client.DefaultRequestHeaders.Accept.Clear();
-
-            //        //sets the Accept header to "application/json", which tells the server to send data in JSON format.
-            //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-
-            //        //HTTP GET
-            //        using (HttpResponseMessage _response = await client.GetAsync(apiUrl))
-            //        {
-            //          //_response.EnsureSuccessStatusCode(); //Throw if not a success code.
-
-            //          if (_response.IsSuccessStatusCode)
-            //          {
-            //            if (_response.StatusCode == System.Net.HttpStatusCode.OK) //Check 200 OK Status
-            //              objTask = await _response.Content.ReadAsAsync<T>();
-            //          }
-            //        }
-            //}  
-            #endregion
-            try
-            {
-                //SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                //SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-
-                ////sets the Accept header to "application/json", which tells the server to send data in JSON format.
-                //SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-                httpClient = SingletonBWHttpClient.Instance;
-
-                //HTTP GET
-                using (HttpResponseMessage _response = await httpClient.GetAsync(apiUrl))
-                {
-                    //_response.EnsureSuccessStatusCode(); //Throw if not a success code.
-
-                    if (_response.IsSuccessStatusCode)
-                    {
-                        if (_response.StatusCode == System.Net.HttpStatusCode.OK) //Check 200 OK Status
-                            objTask = await _response.Content.ReadAsAsync<T>();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.GetApiResponse<T>");
-                objErr.SendMail();
-            }
-            return objTask;
-        }
-
         public async Task<T> GetApiResponse<T>(APIHost apiHost, string requestType, string apiUrl, T responseType)
         {
             T objTask = default(T);
@@ -129,59 +64,7 @@ namespace Bikewale.Utility
             }
             return objTask;
         }
-
-        /// <summary>
-        /// Created By : Sadhana Upadhyay on 11 Nov 2014
-        /// Summary : Method to get data from web api synchronously
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType"></param>
-        /// <param name="apiUrl"></param>
-        /// <param name="responseType"></param>
-        /// <returns></returns>
-        public T GetApiResponseSync<T>(string hostUrl, string requestType, string apiUrl, T responseType)
-        {
-            T objTask = default(T);
-            try
-            {
-                // New code:       
-                //sets the base URI for HTTP requests
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-
-                //sets the Accept header to "application/json", which tells the server to send data in JSON format.
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-
-                //HTTP GET
-                using (HttpResponseMessage _response = SingletonBWHttpClient.Instance.GetAsync(apiUrl).Result)
-                {
-                    //_response.EnsureSuccessStatusCode(); //Throw if not a success code.                    
-                    if (_response.IsSuccessStatusCode)
-                    {
-                        if (_response.StatusCode == System.Net.HttpStatusCode.OK) //Check 200 OK Status        
-                        {
-                            objTask = _response.Content.ReadAsAsync<T>().Result;
-                            _response.Content.Dispose();
-                            _response.Content = null;
-                        }
-                    }
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.GetApiResponseSync<T>");
-                objErr.SendMail();
-            }
-            catch (Exception err)
-            {
-                ErrorClass objErr = new ErrorClass(err, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.GetApiResponseSync<T>");
-                objErr.SendMail();
-            }
-
-            return objTask;
-        }
-
+             
         public T GetApiResponseSync<T>(APIHost apiHost, string requestType, string apiUrl, T responseType)
         {
             T objTask = default(T);
@@ -228,63 +111,6 @@ namespace Bikewale.Utility
                 ErrorClass objErr = new ErrorClass(err, String.Format("Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.GetApiResponseSync<T>({0},{1})", apiHost, apiUrl));
                 objErr.SendMail();
             }
-            return objTask;
-        }
-
-        /// <summary>
-        /// Created By : Lucky Rathore on 18 Nov 2015
-        /// Summary : Method to get data from web api synchronously and add Parameter in request Header.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType"></param>
-        /// <param name="apiUrl"></param>
-        /// <param name="responseType"></param>
-        /// <returns></returns>
-        public T GetApiResponseSync<T>(string hostUrl, string requestType, string apiUrl, T responseType, IDictionary<string, string> headerParameters)
-        {
-            T objTask = default(T);
-            try
-            {
-
-                // New code:       
-                //sets the base URI for HTTP requests
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-
-                //sets the Accept header to "application/json", which tells the server to send data in JSON format.
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-
-                //Add parameter
-                if (headerParameters != null && headerParameters.Count() > 0)
-                {
-                    foreach (var param in headerParameters)
-                    {
-                        SingletonBWHttpClient.Instance.DefaultRequestHeaders.Add(param.Key, param.Value);
-                    }
-                }
-
-                //HTTP GET
-                using (HttpResponseMessage _response = SingletonBWHttpClient.Instance.GetAsync(apiUrl).Result)
-                {
-                    //_response.EnsureSuccessStatusCode(); //Throw if not a success code.                    
-                    if (_response.IsSuccessStatusCode)
-                    {
-                        if (_response.StatusCode == System.Net.HttpStatusCode.OK) //Check 200 OK Status        
-                        {
-                            objTask = _response.Content.ReadAsAsync<T>().Result;
-                            _response.Content.Dispose();
-                            _response.Content = null;
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.GetApiResponseSync<T>");
-                objErr.SendMail();
-            }
-
             return objTask;
         }
 
@@ -356,36 +182,6 @@ namespace Bikewale.Utility
             return objTask;
         }
 
-        public async Task<bool> PostAsync<T>(string hostUrl, string requestType, string apiUrl, T postObject)
-        {
-            bool isSuccess = false;
-            try
-            {
-                // TODO - Send HTTP requests
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-                // HTTP POST
-
-                using (HttpResponseMessage response = await SingletonBWHttpClient.Instance.PostAsJsonAsync(apiUrl, postObject))
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        //// Get the URI of the created resource.
-                        //Uri gizmoUrl = response.Headers.Location;
-                        isSuccess = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.PostAsync<T>");
-                objErr.SendMail();
-            }
-            return isSuccess;
-
-        }
-
         public async Task<bool> PostAsync<T>(APIHost apiHost, string requestType, string apiUrl, T postObject)
         {
             bool isSuccess = false;
@@ -433,41 +229,6 @@ namespace Bikewale.Utility
 
         }
 
-        /// <summary>
-        /// Written By : Ashwini Todkar on 8 Nov 2014
-        /// Method to call delete api synchronously
-        /// </summary>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType"></param>
-        /// <param name="apiUrl"></param>
-        /// <returns></returns>
-        public bool DeleteSync(string hostUrl, string requestType, string apiUrl)
-        {
-            //Task<bool> t = default(Task<bool>);
-            bool isSuccess = false;
-
-            try
-            {
-
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-
-                using (var response = SingletonBWHttpClient.Instance.DeleteAsync(apiUrl).Result)
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        isSuccess = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.DeleteSync");
-                objErr.SendMail();
-            }
-
-            return isSuccess;
-        }
-
         public bool DeleteSync(APIHost apiHost, string requestType, string apiUrl)
         {
             //Task<bool> t = default(Task<bool>);
@@ -512,41 +273,6 @@ namespace Bikewale.Utility
             return isSuccess;
         }
 
-        /// <summary>
-        /// Written By : Ashwini Todkar on 8 Nov 2014
-        /// </summary>
-        /// <typeparam name="T">Generic object to Post</typeparam>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType"></param>
-        /// <param name="apiUrl"></param>
-        /// <param name="objResponse"></param>
-        /// <returns></returns>
-        public U PostSync<T, U>(string hostUrl, string requestType, string apiUrl, T objToPost)
-        {
-            U objResponse = default(U);
-
-            try
-            {
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-
-                using (var response = SingletonBWHttpClient.Instance.PostAsJsonAsync(apiUrl, objToPost).Result)
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        objResponse = response.Content.ReadAsAsync<U>().Result;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.PostSync<T, U>");
-                objErr.SendMail();
-            }
-            return objResponse;
-        }
-
         public U PostSync<T, U>(APIHost apiHost, string requestType, string apiUrl, T objToPost)
         {
             U objResponse = default(U);
@@ -578,53 +304,6 @@ namespace Bikewale.Utility
                         {
                             objResponse = response.Content.ReadAsAsync<U>().Result;
                         }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.PostSync<T, U>");
-                objErr.SendMail();
-            }
-            return objResponse;
-        }
-
-        /// <summary>
-        /// Created By : Lucky Rathore
-        /// Description : Implementiaon of HTTP Post Method with adding Headed parameter in HTTP request Header.
-        /// </summary>
-        /// <typeparam name="T">Data to Send.</typeparam>
-        /// <typeparam name="U">return Type of Function.</typeparam>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType"></param>
-        /// <param name="apiUrl"></param>
-        /// <param name="objToPost"></param>
-        /// <param name="headerParameter"></param>
-        /// <returns></returns>
-        public U PostSync<T, U>(string hostUrl, string requestType, string apiUrl, T objToPost, IDictionary<string, string> headerParameters)
-        {
-            U objResponse = default(U);
-
-            try
-            {
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-
-                if (headerParameters != null && headerParameters.Count() > 0)
-                {
-                    foreach (var param in headerParameters)
-                    {
-                        SingletonBWHttpClient.Instance.DefaultRequestHeaders.Add(param.Key, param.Value);
-                    }
-                }
-
-
-                using (var response = SingletonBWHttpClient.Instance.PostAsJsonAsync(apiUrl, objToPost).Result)
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        objResponse = response.Content.ReadAsAsync<U>().Result;
                     }
                 }
             }
@@ -694,42 +373,6 @@ namespace Bikewale.Utility
                     }
                 }
             }
-            return objResponse;
-        }
-
-        /// <summary>
-        /// Written By : Sangram Nandkhile on 8 October 2015
-        /// Put Request and fetch response
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="hostUrl"></param>
-        /// <param name="requestType"></param>
-        /// <param name="apiUrl"></param>
-        /// <param name="objResponse"></param>
-        /// <returns></returns>
-        public U PutSync<T, U>(string hostUrl, string requestType, string apiUrl, T objToPost)
-        {
-            U objResponse = default(U);
-
-            try
-            {
-                SingletonBWHttpClient.Instance.BaseAddress = new Uri(hostUrl);
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Clear();
-                SingletonBWHttpClient.Instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestType));
-
-                var response = SingletonBWHttpClient.Instance.PutAsJsonAsync(apiUrl, objToPost).Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    objResponse = response.Content.ReadAsAsync<U>().Result;
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorClass objErr = new ErrorClass(ex, "Exception : Bikewale.Utility.BWHttpClient.BWHttpClient.PutSync<T, U>");
-                objErr.SendMail();
-            }
-
             return objResponse;
         }
 
