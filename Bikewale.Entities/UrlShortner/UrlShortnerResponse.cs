b31d26d526c95f370a7c646fe67d2ac18e78c5dc
Sadhana <sadhana.upadhyay@carwale.com>
2016-01-08 16:12:12 +0530

    tiny url logic

---
+++ Bikewale.Entities/UrlShortner/UrlShortnerResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.Entities.UrlShortner
+{
+    public class UrlShortnerResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("longUrl")]
+        public string LongUrl { get; set; }
+    }
+}
---
+++ Bikewale.Utility/UrlShortner.cs
@@ -0,0 +1,54 @@
+﻿using Bikewale.Entities.UrlShortner;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.Utility
+{
+    public class UrlShortner
+    {
+        public static UrlShortnerResponse GetShortUrl(string longUrl)
+        {
+            string shortUrl = string.Empty;
+
+            UrlShortnerResponse objResponse = null;
+
+            string jsonData = "{\"longUrl\":\"" + longUrl + "\"}";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(jsonData);
+            string contentType = "application/json";
+            
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/urlshortener/v1/url?key=" + BWConfiguration.Instance.GoogleApiKey);
+            Request.Method = "POST";
+            Request.ContentType = contentType;
+            Request.ContentLength = byteArray.Length;
+            Stream dataStream = Request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            string responseLine = string.Empty;
+
+            //  SEND MESSAGE
+            try
+            {
+                WebResponse Response = Request.GetResponse();
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+                responseLine = Reader.ReadToEnd();
+                Reader.Close();
+
+                objResponse = JsonConvert.DeserializeObject<UrlShortnerResponse>(responseLine);
+            }
+            catch (Exception e)
+            {
+                responseLine = e.Message.ToString();
+            }
+
+            return objResponse;
+        }
+    }
+}
