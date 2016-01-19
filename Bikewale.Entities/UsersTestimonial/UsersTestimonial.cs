b8596fb1aedf603203d14661ff2f875e2e3139ee
unknown <sumit.kate@user358.aepl.com>
2016-01-19 10:44:26 +0530

    Created Entity, interface and DAL for UsersTestimonial

---
+++ Bikewale.Entities/UsersTestimonial/UsersTestimonial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.Entities.UsersTestimonial
+{
+    /// <summary>
+    /// Created by  :   Sumit Kate on 19 Jan 2016
+    /// Summary     :   User's Testimonial entity
+    /// </summary>
+    public class UsersTestimonial
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string HostUrl { get; set; }
+        public string UserImgUrl { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
