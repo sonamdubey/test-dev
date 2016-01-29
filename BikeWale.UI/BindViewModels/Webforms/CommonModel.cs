7d75786e88ea4a3b6740709161c802d004f9b8db
cw-sangram <sangram.nandkhile@carwale.com>
2016-01-29 16:29:57 +0530

    Static function null check added

--- BikeWale.UI/BindViewModels/Webforms/CommonModel.cs
+++ BikeWale.UI/BindViewModels/Webforms/CommonModel.cs
@@ -9,7 +9,10 @@ namespace Bikewale.BindViewModels.Webforms
         public static uint GetTotalDiscount(List<PQ_Price> discountedPriceList)
         {
             uint discount = 0;
-            discount = discountedPriceList.Select(o => o.Price).Aggregate((x, y) => x + y);
+            if (discountedPriceList != null && discountedPriceList.Count > 0)
+            {
+                discount = discountedPriceList.Select(o => o.Price).Aggregate((x, y) => x + y);
+            }
             return discount;
         }
     }
