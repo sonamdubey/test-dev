5d8e4eeecf9499c0c0a72507b2683100f034e55b
unknown <sumit.kate@user358.aepl.com>
2016-02-04 19:09:16 +0530

    DTO

---
+++ Bikewale.DTO/BikeBooking/BookingListingFilterDTO.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.DTO.BikeBooking
+{
+    public class BookingListingFilterDTO
+    {
+        [JsonProperty("makeIds")]
+        public string MakeIds { get; set; }
+        [JsonProperty("displacement")]
+        public string Displacement { get; set; }
+        [JsonProperty("budget")]
+        public string Budget { get; set; }
+        [JsonProperty("mileage")]
+        public string Mileage { get; set; }
+        [JsonProperty("rideStyle")]
+        public string RideStyle { get; set; }
+        [JsonProperty("antiBreakingSystem")]
+        public string AntiBreakingSystem { get; set; }
+        [JsonProperty("brakeType")]
+        public string BrakeType { get; set; }
+        [JsonProperty("alloyWheel")]
+        public string AlloyWheel { get; set; }
+        [JsonProperty("startType")]
+        public string StartType { get; set; }
+        [JsonProperty("pageNo")]
+        public string PageNo { get; set; }
+        [JsonProperty("pageSize")]
+        public string PageSize { get; set; }
+        [JsonProperty("so")]
+        public string so { get; set; }
+        [JsonProperty("sc")]
+        public string sc { get; set; }
+    }
+}
---
+++ Bikewale.Service/Controllers/BikeBooking/BikeBookingListingController.cs
@@ -0,0 +1,20 @@
+﻿using Bikewale.DTO.BikeBooking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Bikewale.Service.Controllers.BikeBooking
+{
+    public class BikeBookingListingController : ApiController
+    {
+        [ResponseType(typeof(IEnumerable<BikeBookingListingDTO>))]
+        public IHttpActionResult Get([FromBody]BookingListingFilterDTO filter)
+        {
+            return Ok();
+        }
+    }
+}
