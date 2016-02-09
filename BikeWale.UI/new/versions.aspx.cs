f32f5a66d693c5df2d6dab1fffeb5a18da2377e7
cw-sangram <sangram.nandkhile@carwale.com>
2016-02-09 18:41:29 +0530

    fetch version details for wego (disc/drum) added

--- BikeWale.UI/new/versions.aspx.cs
+++ BikeWale.UI/new/versions.aspx.cs
@@ -274,6 +274,10 @@ namespace Bikewale.New
             ctrlUserReviews.Filter = Entities.UserReviews.FilterBy.MostRecent;
 
             ToggleOfferDiv();
+            if (!IsPostBack && urlVersionId!=0)
+            {
+                FetchVariantDetails(urlVersionId);
+            }
             Trace.Warn("Trace 20 : Page Load ends");
             // Clear trailing query string -- added on 09-feb-2016 by Sangram
             PropertyInfo isreadonly = typeof(System.Collections.Specialized.NameValueCollection).GetProperty("IsReadOnly", BindingFlags.Instance | BindingFlags.NonPublic);
