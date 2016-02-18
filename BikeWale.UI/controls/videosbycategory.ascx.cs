0fe4a6317f6514611fddf0d6909193a77959e52c
cw-vivekgupta <vivek.gupta@carwale.com>
2016-02-18 14:15:06 +0530

    videos by category user control created

---
+++ BikeWale.UI/controls/videosbycategory.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace Bikewale.controls
+{
+    public partial class VideosByCategory : System.Web.UI.UserControl
+    {
+        protected Repeater rptCategoryVideos;
+
+        protected override void OnInit(EventArgs e)
+        {
+            this.Load += new EventHandler(Page_Load);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindVideosByCategory();
+        }
+
+        public void BindVideosByCategory()
+        {
+
+        }
+    }
+}
