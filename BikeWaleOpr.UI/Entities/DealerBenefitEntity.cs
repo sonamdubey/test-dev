f701e5a5320ffdbbc179e2260308a275c313ea9a
cw-sangram <sangram.nandkhile@carwale.com>
2016-03-10 15:14:04 +0530

    Add a new Entity DealerBenefitEntiry in OPR - This entity will be used in fetching and saving benefit text

---
+++ BikeWaleOpr.UI/Entities/DealerBenefitEntity.cs
@@ -0,0 +1,19 @@
+﻿using BikeWaleOpr.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikewaleOpr.Entities
+{
+    public class DealerBenefitEntity
+    {
+        uint Id { get; set; }
+        uint BenefitCatId { get; set; }
+        string BenefitText { get; set; }
+        bool IsActive { get; set; }
+        CityEntityBase ObjCity { get; set; }
+        MakeEntityBase ObjMake { get; set; }
+        ModelEntityBase ObjModel { get; set; }
+    }
+}
