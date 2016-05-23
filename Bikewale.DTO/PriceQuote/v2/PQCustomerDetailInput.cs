2d9524499a5024b2c294a2152f394bee9202cda0
cw-sumitkate <sumit.kate@carwale.com>
2016-05-23 10:51:26 +0530

    New version of PQCustomerDetails API is created - Allow APP to submit the lead from dealer locator section - PQ is generated at Service Layer of Bikewale

---
+++ Bikewale.DTO/PriceQuote/v2/PQCustomerDetailInput.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace Bikewale.DTO.PriceQuote.v2
+{
+    /// <summary>
+    /// Price Quote Customer Details Input entity version 2
+    /// Author  :   Sumit Kate
+    /// Date    :   23 May 2016    
+    /// </summary>
+    public class PQCustomerDetailInput
+    {
+        [JsonProperty("dealerId")]
+        public uint DealerId { get; set; }
+        [JsonProperty("pqId")]
+        public uint PQId { get; set; }
+        [JsonProperty("customerName")]
+        public string CustomerName { get; set; }
+        [JsonProperty("customerMobile")]
+        public string CustomerMobile { get; set; }
+        [JsonProperty("customerEmail")]
+        public string CustomerEmail { get; set; }
+        [JsonProperty("clientIP")]
+        public string ClientIP { get; set; }
+        [JsonProperty("pageUrl")]
+        public string PageUrl { get; set; }
+        [JsonProperty("versionId")]
+        public string VersionId { get; set; }
+        [JsonProperty("cityId")]
+        public string CityId { get; set; }
+        //Added By  : Sadhana Upadhyay on 29 Dec 2015
+        [JsonProperty("leadSourceId")]
+        public ushort? LeadSourceId { get; set; }
+        [JsonProperty("deviceId")]
+        public string DeviceId { get; set; }
+    }
+}
---
+++ Bikewale.DTO/PriceQuote/v2/PQCustomerDetailOutput.cs
@@ -0,0 +1,26 @@
+﻿using Bikewale.DTO.PriceQuote.BikeBooking;
+using Newtonsoft.Json;
+using System;
+
+namespace Bikewale.DTO.PriceQuote.v2
+{
+    /// <summary>
+    /// Price quote customer details output entity version 2
+    /// Author  :   Sumit Kate
+    /// Date    :   23 May 2016
+    /// </summary>
+    public class PQCustomerDetailOutput
+    {
+        [JsonProperty("pqId")]
+        public UInt64 PQId { get; set; }
+
+        [JsonProperty("isSuccess")]
+        public bool IsSuccess { get; set; }
+
+        [JsonProperty("dealer")]
+        public DealerDetailsDTO Dealer { get; set; }
+
+        [JsonProperty("noOfAttempts")]
+        public sbyte NoOfAttempts { get; set; }
+    }
+}
