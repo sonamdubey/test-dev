52cd7bf8608abf5fcb52151edf289b54be2b84fc
cw-sumitkate <sumit.kate@carwale.com>
2016-06-29 18:20:36 +0530

    Added Serialize attribute - To enable caching

--- Bikewale.Entities/BikeData/MostPopularBikesBase.cs
+++ Bikewale.Entities/BikeData/MostPopularBikesBase.cs
@@ -1,23 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Bikewale.Entities.BikeData
 {
+    [Serializable, DataContract]
     public class MostPopularBikesBase
     {
+        [DataMember]
         public BikeMakeEntityBase objMake { get; set; }
+        [DataMember]
         public BikeModelEntityBase objModel { get; set; }
+        [DataMember]
         public BikeVersionsListEntity objVersion { get; set; }
+        [DataMember]
         public string BikeName { get; set; }
+        [DataMember]
         public string HostURL { get; set; }
+        [DataMember]
         public string OriginalImagePath { get; set; }
-        public int ReviewCount {get; set; }
+        [DataMember]
+        public int ReviewCount { get; set; }
+        [DataMember]
         public double ModelRating { get; set; }
+        [DataMember]
         public Int64 VersionPrice { get; set; }
+        [DataMember]
         public MinSpecsEntity Specs { get; set; }
-        public ushort BikePopularityIndex { get; set; }                 
+        [DataMember]
+        public ushort BikePopularityIndex { get; set; }
     }
 }
