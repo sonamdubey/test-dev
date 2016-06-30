449c037e9b8a13b0c131b047dda7574abc29c24a
cw-sumitkate <sumit.kate@carwale.com>
2016-06-30 16:17:37 +0530

    Changed Cache time and fixed user reviews list - Changed Cache time for state and dealers in state

--- Bikewale.Entities/UserReviews/ReviewTaggedBikeEntity.cs
+++ Bikewale.Entities/UserReviews/ReviewTaggedBikeEntity.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Bikewale.Entities.BikeData;
+﻿using Bikewale.Entities.BikeData;
+using System;
+using System.Runtime.Serialization;
 
 namespace Bikewale.Entities.UserReviews
 {
+    [Serializable, DataContract]
     public class ReviewTaggedBikeEntity
     {
-
+        [DataMember]
         private BikeMakeEntityBase objmakeBase = new BikeMakeEntityBase();
+        [DataMember]
         public BikeMakeEntityBase MakeEntity { get { return objmakeBase; } set { objmakeBase = value; } }
-
+        [DataMember]
         private BikeModelEntityBase objModelBase = new BikeModelEntityBase();
+        [DataMember]
         public BikeModelEntityBase ModelEntity { get { return objModelBase; } set { objModelBase = value; } }
-
+        [DataMember]
         private BikeVersionEntityBase objVersionBase = new BikeVersionEntityBase();
+        [DataMember]
         public BikeVersionEntityBase VersionEntity { get { return objVersionBase; } set { objVersionBase = value; } }
-
+        [DataMember]
         public uint ReviewsCount { get; set; }
+        [DataMember]
         public uint Price { get; set; }
     }
 }
