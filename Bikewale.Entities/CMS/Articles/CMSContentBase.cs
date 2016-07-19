fee5caf213d090bc5bc0423ef7dacb796be54595
Vivek Gupta <vivek.gupta@carwale.com>
2016-07-19 13:12:03 +0530

    GRPC objects cached - GRPC objects cached for news

--- Bikewale.Entities/CMS/Articles/CMSContentBase.cs
+++ Bikewale.Entities/CMS/Articles/CMSContentBase.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Bikewale.Entities.CMS.Articles
 {
+    [Serializable, DataContract]
     public class CMSContent
     {
+        [DataMember]
         public IList<ArticleSummary> Articles { get; set; }
+        [DataMember]
         public uint RecordCount { get; set; }
     }
 }
