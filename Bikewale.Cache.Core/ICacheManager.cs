e662d9f5428332c8e9c444495a74e05c15a99446
cw-sumitkate <sumit.kate@carwale.com>
2016-07-25 15:46:07 +0530

    removed the public keyword from interface

--- Bikewale.Cache.Core/ICacheManager.cs
+++ Bikewale.Cache.Core/ICacheManager.cs
@@ -5,7 +5,7 @@ namespace Bikewale.Interfaces.Cache.Core
     public interface ICacheManager
     {
         T GetFromCache<T>(string key, TimeSpan cacheDuration, Func<T> dbCallback);
-        public T GetFromCache<T>(string key, TimeSpan cacheDuration, Func<T> dbCallback, out bool isDataFromCache);
+        T GetFromCache<T>(string key, TimeSpan cacheDuration, Func<T> dbCallback, out bool isDataFromCache);
         void RefreshCache(string key);
     }
 }
