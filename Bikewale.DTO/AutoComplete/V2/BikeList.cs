1bdc25c3b06f4223a3add3ee533d2d24d130e741
Sajal Gupta <sajal.gupta@aepl.com>
2016-08-01 17:44:20 +0530

    Api Optimisation - Made new Dtos accordingly for new version of API and also made new   version of autosuggest api.

---
+++ Bikewale.DTO/AutoComplete/V2/BikeList.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.DTO.AutoComplete.V2
+{
+    /// <summary>
+    /// Created By : Sajal Gupta
+    /// Created On : 01/08/2016
+    /// </summary>
+    public class BikeList
+    {
+        [JsonProperty("suggestionListV2")]
+        public List<SuggestionList> Bikes { get; set; }
+    }
+}
