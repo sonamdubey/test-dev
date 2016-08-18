36ee2bcb123bafe9a101f3ffff5a6ffe48cb7a43
Aditi Srivastava <aditi.srivastava@aepl.com>
2016-08-18 17:36:34 +0530

    Configured model gallery page to include Model Image Configured model gallery page to include Model Image as first image on desktop and msite.

---
+++ Bikewale.Entities/CMS/Photos/ModelPhotos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bikewale.Entities.BikeData;
+using System.Runtime.Serialization;
+
+namespace Bikewale.Entities.CMS.Photos
+{
+    [Serializable, DataContract]
+    public class ModelPhotos
+    {
+        [DataMember]
+        public string ModelName { get; set; }
+        [DataMember]
+        public string HostURL { get; set; }
+        [DataMember]
+        public string OriginalImgPath { get; set; }
+    }
+
+}
