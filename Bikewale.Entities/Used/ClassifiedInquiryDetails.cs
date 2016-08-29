d5254e9fb09bf8c15087285740da9769b44be3de
cw-sushilkumar <kanojia.sushil@gmail.com>
2016-08-29 11:21:39 +0530

    Added other bike deatils and mindetails for used bikes - Added other bike deatils and mindetails for used bikes
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- Bikewale.Entities/Used/ClassifiedInquiryDetails.cs
+++ Bikewale.Entities/Used/ClassifiedInquiryDetails.cs
@@ -1,10 +1,13 @@
 ﻿using Bikewale.Entities.BikeData;
 using Bikewale.Entities.Location;
 using System;
-using System.Collections.Generic;
 
 namespace Bikewale.Entities.Used
 {
+    /// <summary>
+    /// Created By : Sushil Kumar on 27th August 2016
+    /// Description : Bike ClassifiedInquiryDetails entity for used bikes
+    /// </summary>
     [Serializable]
     public class ClassifiedInquiryDetails
     {
@@ -13,7 +16,7 @@ namespace Bikewale.Entities.Used
         public BikeVersionEntityBase Version { get; set; }
         public StateEntityBase State { get; set; }
         public CityEntityBase City { get; set; }
-        public IList<BikePhoto> Photo { get; set; }
-        public ushort PhotosCount { get { if (Photo != null) { return Convert.ToUInt16(Photo.Count); } return 0; } }
+        public BikeDetailsMin MinDetails { get; set; }
+        public BikeDetails OtherDetails { get; set; }
     }
 }
