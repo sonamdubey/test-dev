f6f638c86491e791929c72a19501d122aa8c6498
Ashish Kamble <ashish.kamble@carwale.com>
2016-09-07 11:56:21 +0530

    This will fix tracking beacon for existing users and news feed fix - Tracking beacon will be sent for the existing customers as well - News feed is fixed for the msn - Currently carwale news page is called with application id 2 and returned the data through XML

--- Bikewale.Utility/CurrentUser.cs
+++ Bikewale.Utility/CurrentUser.cs
@@ -20,6 +20,7 @@ namespace Bikewale.Utility
         public static void GenerateUniqueCookie()
         {
             HttpCookie bwc = HttpContext.Current.Request.Cookies["BWC"];
+            HttpCookie _cwv = HttpContext.Current.Request.Cookies["_cwv"];
             if (bwc == null)
             {
                 //create the cwc cookie with a unique random value
@@ -41,6 +42,21 @@ namespace Bikewale.Utility
                 bwvCookie.Value = bwcCookieValue + "." + bwvCookieValue + "." + currServerTimeStamp + "." + currServerTimeStamp + "." + currServerTimeStamp + ".1" + ".r";
                 bwvCookie.Expires = DateTime.Now.AddYears(2);
                 CookieManager.Add(bwvCookie);
+            }
+            else if (bwc != null && _cwv == null)
+            {
+                //also add the cookie for cwv, to identify the session or the visit and also the number of times this visitor has come.
+                //Since this has been implemented later, hence add value for visit count only when cwc is being initialized
+                //the format for _cwv cookie is : cwcValue.random_key_for_cwv.visit_start_timestamp.visit_last_timestamp.visit_count
+                string bwvCookieValue = RandomNoGenerator.GetUniqueKey(10); //add 10 digit unique key for the cwv
+
+                Cookie bwvCookie = new Cookie("_cwv");
+
+                long currServerTimeStamp = GetCurrentUnixTimeStamp();
+                bwvCookie.Value = bwc.Value + "." + bwvCookieValue + "." + currServerTimeStamp + "." + currServerTimeStamp + "." + currServerTimeStamp + ".1" + ".r";
+                bwvCookie.Expires = DateTime.Now.AddYears(2);
+                CookieManager.Add(bwvCookie);
+                
             }
         }
 
