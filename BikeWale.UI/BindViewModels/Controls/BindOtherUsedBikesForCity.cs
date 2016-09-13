ef3113e9bee8ef6d5f9fec8c3f3d2b35b3e4d863
cw-sushilkumar <kanojia.sushil@gmail.com>
2016-09-13 14:21:49 +0530

    Added null check for otherBikesinCity.FirstOrDefault() in other used bikes - Added null check for otherBikesinCity.FirstOrDefault() in other used bikes
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- BikeWale.UI/BindViewModels/Controls/BindOtherUsedBikesForCity.cs
+++ BikeWale.UI/BindViewModels/Controls/BindOtherUsedBikesForCity.cs
@@ -57,7 +57,7 @@ namespace Bikewale.BindViewModels.Controls
                         var objCache = container.Resolve<IUsedBikeDetailsCacheRepository>();
                         otherBikesinCity = objCache.GetOtherBikesByCityId(inquiryId, cityId, topCount);
 
-                        if (otherBikesinCity != null)
+                        if (otherBikesinCity != null && otherBikesinCity.FirstOrDefault() != null)
                         {
                             CityName = otherBikesinCity.FirstOrDefault().RegisteredAt;
                             CityMaskingName = otherBikesinCity.FirstOrDefault().CityMaskingName;
