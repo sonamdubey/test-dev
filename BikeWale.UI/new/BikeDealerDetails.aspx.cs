1fec2202a2b61e0ce4cdd8568879d7d1c1fe39dc
Dutiyesh Salunkhe <dutiyesh.salunkhe@carwale.com>
2016-09-21 16:16:38 +0530

    Add dealer details page for desktop - Add dealer details page - Update bw sprite

---
+++ BikeWale.UI/new/BikeDealerDetails.aspx.cs
@@ -0,0 +1,294 @@
+﻿using Bikewale.Cache.BikeData;
+using Bikewale.Cache.Core;
+using Bikewale.Cache.DealersLocator;
+using Bikewale.DAL.BikeData;
+using Bikewale.DAL.Dealer;
+using Bikewale.Entities.BikeData;
+using Bikewale.Entities.DealerLocator;
+using Bikewale.Entities.Location;
+using Bikewale.Interfaces.BikeData;
+using Bikewale.Interfaces.Cache.Core;
+using Bikewale.Interfaces.Dealer;
+using Bikewale.Memcache;
+using Bikewale.Notifications;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bikewale.New
+{
+    /// <summary>
+    /// Created By : Sushil Kumar on 19th March 2016
+    /// Class to show the bike dealers details
+    /// </summary>
+    public class BikeDealerDetails : Page
+    {
+        protected string makeName = string.Empty, modelName = string.Empty, cityName = string.Empty, areaName = string.Empty, makeMaskingName = string.Empty, cityMaskingName = string.Empty, urlCityMaskingName = string.Empty;
+        protected uint cityId, makeId;
+        protected ushort totalDealers;
+        protected Repeater rptMakes, rptCities, rptDealers;
+        protected string clientIP = string.Empty, pageUrl = string.Empty;
+        protected bool areDealersPremium = false;
+
+
+        protected override void OnInit(EventArgs e)
+        {
+            InitializeComponent();
+        }
+
+        void InitializeComponent()
+        {
+            base.Load += new EventHandler(this.Page_Load);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Modified By :Lucky Rathore on 12 July 2016.
+            Form.Action = Request.RawUrl;
+            string originalUrl = Request.ServerVariables["HTTP_X_ORIGINAL_URL"];
+            if (String.IsNullOrEmpty(originalUrl))
+                originalUrl = Request.ServerVariables["URL"];
+
+            Bikewale.Common.DeviceDetection dd = new Bikewale.Common.DeviceDetection(originalUrl);
+            dd.DetectDevice();
+
+            if (ProcessQueryString())
+            {
+                GetMakeIdByMakeMaskingName(makeMaskingName);
+
+                if (makeId > 0 && cityId > 0)
+                {
+                    BindMakesDropdown();
+                    BindCitiesDropdown();
+                    BindDealerList();
+                }
+                else
+                {
+                    Response.Redirect(Bikewale.Common.CommonOpn.AppPath + "pageNotFound.aspx", false);
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                    this.Page.Visible = false;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Created By  : Sushil Kumar
+        /// Created On  : 20th March 2016
+        /// Description : To bind dealerlist
+        /// </summary>
+        private void BindDealerList()
+        {
+            DealersEntity _dealers = null;
+            try
+            {
+                using (IUnityContainer container = new UnityContainer())
+                {
+                    container.RegisterType<IDealerCacheRepository, DealerCacheRepository>()
+                             .RegisterType<ICacheManager, MemcacheManager>()
+                             .RegisterType<IDealer, DealersRepository>()
+                            ;
+                    var objCache = container.Resolve<IDealerCacheRepository>();
+                    _dealers = objCache.GetDealerByMakeCity(cityId, makeId);
+
+                    if (_dealers != null && _dealers.TotalCount > 0)
+                    {
+                        rptDealers.DataSource = _dealers.Dealers;
+                        rptDealers.DataBind();
+                        totalDealers = _dealers.TotalCount;
+
+                        if (totalDealers > 0)
+                        {
+                            int _countStdDealers = _dealers.Dealers.Count(m => m.DealerType < 2);// counting only standard or invalid dealers
+                            int _countPreDel = _dealers.Dealers.Count(m => m.DealerType > 1);// counting only deluxe or premium dealers
+                            if (_countStdDealers < 3 && _countPreDel > 0)
+                            {
+                                areDealersPremium = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Response.Redirect(Bikewale.Common.CommonOpn.AppPath + "pageNotFound.aspx", false);
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        this.Page.Visible = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn(ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, "BindDealerList");
+                objErr.SendMail();
+            }
+        }
+
+
+        /// <summary>
+        /// Created By  : Sushil Kumar
+        /// Created On  : 20th March 2016
+        /// Description : To bind makes list to dropdown
+        /// Modified by :   Sumit Kate on 29 Mar 2016
+        /// Description :   Get the makes list of BW and AB dealers
+        /// </summary>
+        private void BindMakesDropdown()
+        {
+            IEnumerable<BikeMakeEntityBase> _makes = null;
+            try
+            {
+                using (IUnityContainer container = new UnityContainer())
+                {
+                    container.RegisterType<IBikeMakesCacheRepository<int>, BikeMakesCacheRepository<BikeMakeEntity, int>>()
+                             .RegisterType<ICacheManager, MemcacheManager>()
+                             .RegisterType<IBikeMakes<BikeMakeEntity, int>, BikeMakesRepository<BikeMakeEntity, int>>()
+                            ;
+                    var objCache = container.Resolve<IBikeMakesCacheRepository<int>>();
+                    _makes = objCache.GetMakesByType(EnumBikeType.Dealer);
+                    if (_makes != null && _makes.Count() > 0)
+                    {
+                        rptMakes.DataSource = _makes;
+                        rptMakes.DataBind();
+                        var firstMake = _makes.FirstOrDefault(x => x.MakeId == makeId);
+                        if (firstMake != null)
+                        {
+                            makeName = firstMake.MakeName;
+                            makeMaskingName = firstMake.MaskingName;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn(ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, "BindMakesDropdown");
+                objErr.SendMail();
+            }
+        }
+
+
+        /// <summary>
+        /// Created By  : Sushil Kumar
+        /// Created On  : 20th March 2016
+        /// Description : To bind cities list to dropdown
+        /// </summary>
+        private void BindCitiesDropdown()
+        {
+            IEnumerable<CityEntityBase> _cities = null;
+            try
+            {
+                using (IUnityContainer container = new UnityContainer())
+                {
+                    container.RegisterType<IDealer, DealersRepository>();
+
+                    var objCities = container.Resolve<IDealer>();
+                    _cities = objCities.FetchDealerCitiesByMake(makeId);
+                    if (_cities != null && _cities.Count() > 0)
+                    {
+                        rptCities.DataSource = _cities;
+                        rptCities.DataBind();
+                        var firstCity = _cities.FirstOrDefault(x => x.CityId == cityId);
+                        if (firstCity != null)
+                        {
+                            cityName = firstCity.CityName;
+                            cityMaskingName = firstCity.CityMaskingName;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn(ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, "BindCitiesDropdown");
+                objErr.SendMail();
+            }
+        }
+
+
+        /// <summary>
+        /// Created By  : Sushil Kumar
+        /// Created On  : 20th March 2016
+        /// Description : To get makeId from make masking name
+        /// </summary>
+        private void GetMakeIdByMakeMaskingName(string maskingName)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(maskingName))
+                {
+                    string _makeId = MakeMapping.GetMakeId(maskingName);
+                    if (string.IsNullOrEmpty(_makeId) || !uint.TryParse(_makeId, out makeId))
+                    {
+                        Response.Redirect(Bikewale.Common.CommonOpn.AppPath + "pageNotFound.aspx", false);
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        this.Page.Visible = false;
+                    }
+                }
+                else
+                {
+                    Response.Redirect(Bikewale.Common.CommonOpn.AppPath + "pageNotFound.aspx", false);
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                    this.Page.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn(ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, "GetMakeIdByMakeMaskingName");
+                objErr.SendMail();
+            }
+        }
+
+
+
+        #region Private Method to process querystring
+        /// <summary>
+        /// Created By : Sushil Kumar
+        /// Created On : 16th March 2016 
+        /// Description : Private Method to query string fro make masking name and cityId
+        /// </summary>
+        private bool ProcessQueryString()
+        {
+            var currentReq = HttpContext.Current.Request;
+            bool isValidQueryString = false;
+            try
+            {
+                if (currentReq.QueryString != null && currentReq.QueryString.HasKeys())
+                {
+                    makeMaskingName = currentReq.QueryString["make"];
+                    urlCityMaskingName = currentReq.QueryString["city"];
+                    if (!String.IsNullOrEmpty(urlCityMaskingName) && !String.IsNullOrEmpty(makeMaskingName))
+                    {
+                        cityId = CitiMapping.GetCityId(urlCityMaskingName);
+                        isValidQueryString = true;
+                    }
+                    else
+                    {
+                        Response.Redirect(Bikewale.Common.CommonOpn.AppPath + "pageNotFound.aspx", false);
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        this.Page.Visible = false;
+                    }
+                    clientIP = Bikewale.Common.CommonOpn.GetClientIP();
+                    pageUrl = currentReq.ServerVariables["URL"];
+                }
+                else
+                {
+                    Response.Redirect(Bikewale.Common.CommonOpn.AppPath + "pageNotFound.aspx", false);
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                    this.Page.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn("ProcessQueryString Ex: ", ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, currentReq.ServerVariables["URL"]);
+                objErr.SendMail();
+            }
+            return isValidQueryString;
+        }
+        #endregion
+    }   // End of class
+}   // End of namespace
---
+++ BikeWale.UI/src/dealer/details.js
@@ -0,0 +1,298 @@
+﻿var redMarkerImage = 'http://imgd3.aeplcdn.com/0x0/bw/static/design15/map-marker-red.png';
+
+$(document).ready(function () {
+    dropdown.setDropdown();
+});
+
+function initializeMap() {
+    var mapCanvas = document.getElementById("dealer-map");
+    var mapProp = {
+        scrollwheel: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        center: new google.maps.LatLng(19.0720625, 72.8550991),
+        zoom: 15,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+
+    var map = new google.maps.Map(mapCanvas, mapProp);
+
+    var marker = new google.maps.Marker({
+        position: mapProp.center,
+        icon: redMarkerImage
+    });
+
+    marker.setMap(map);
+}
+
+initializeMap();
+
+$('.dropdown-select-wrapper').on('click', '.dropdown-label', function () {
+    dropdown.active($(this));
+});
+
+$('.dropdown-select-wrapper').on('click', '.dropdown-menu-list.dropdown-with-select li', function () {
+    var element = $(this);
+    if (!element.hasClass('active')) {
+        dropdown.selectItem($(this));
+        dropdown.selectOption($(this));
+    }
+    else {
+        element.closest('.dropdown-menu').removeClass('dropdown-active');
+    }
+});
+
+var dropdown = {
+    setDropdown: function () {
+        var selectDropdown = $('.dropdown-select');
+
+        selectDropdown.each(function () {
+            dropdown.setMenu($(this));
+        });
+    },
+
+    setMenu: function (element) {
+        $('<div class="dropdown-menu"></div>').insertAfter(element);
+        dropdown.setStructure(element);
+    },
+
+    setStructure: function (element) {
+        var elementValue = element.find('option:selected').text(),
+			menu = element.next('.dropdown-menu');
+        menu.append('<p class="dropdown-label">' + elementValue + '</p><div class="dropdown-list-wrapper"><p class="dropdown-selected-item">' + elementValue + '</p><ul class="dropdown-menu-list dropdown-with-select"></ul></div>');
+        dropdown.setOption(element);
+    },
+
+    setOption: function (element) {
+        var selectedIndex = element.find('option:selected').index(),
+			menu = element.next('.dropdown-menu'),
+			menuList = menu.find('ul');
+
+        element.find('option').each(function (index) {
+            if ($(this).val() != 0 && $(this).val().length != 0) { // check for dropdown label value
+                menuList.append('<li data-option-value="' + $(this).val() + '">' + $(this).text() + '</li>');
+            }
+        });
+    },
+
+    active: function (label) {
+        $('.dropdown-select-wrapper').find('.dropdown-menu').removeClass('dropdown-active');
+        label.closest('.dropdown-menu').addClass('dropdown-active');
+        if (label.closest('.dropdown-select-wrapper').hasClass('invalid')) {
+            validate.bike.hideError(label);
+        }
+    },
+
+    inactive: function () {
+        $('.dropdown-select-wrapper').find('.dropdown-menu').removeClass('dropdown-active');
+    },
+
+    selectItem: function (element) {
+        var elementValue = element.attr('data-option-value');
+        if (elementValue != 0 || elementValue.length != 0) { // check for dropdown label value
+            var elementText = element.text(),
+			    menu = element.closest('.dropdown-menu'),
+			    dropdownLabel = menu.find('.dropdown-label'),
+			    selectedItem = menu.find('.dropdown-selected-item');
+
+            element.siblings('li').removeClass('active');
+            element.addClass('active');
+            selectedItem.text(elementText);
+            dropdownLabel.text(elementText);
+            menu.removeClass('dropdown-active');
+        }
+    },
+
+    selectOption: function (element) {
+        var elementValue = element.attr('data-option-value'),
+			wrapper = element.closest('.dropdown-select-wrapper'),
+			selectDropdown = wrapper.find('.dropdown-select');
+
+        selectDropdown.val(elementValue).trigger('change');
+    }
+};
+
+$(document).on('click', function (event) {
+    event.stopPropagation();
+    var dropdown = $('.dropdown-menu');
+
+    if (dropdown.hasClass('dropdown-active')) {
+        if (!dropdown.is(event.target) && dropdown.has(event.target).length == 0) {
+            dropdown.removeClass('dropdown-active');
+        }
+    }
+});
+
+var assistanceGetName = $('#assistanceGetName'),
+    assistanceGetEmail = $('#assistanceGetEmail'),
+    assistanceGetMobile = $('#assistanceGetMobile'),
+    assistGetModel = $('#assistGetModel');
+
+/* input focus */
+assistanceGetName.on("focus", function () {
+    validate.onFocus(assistanceGetName);
+});
+
+assistanceGetEmail.on("focus", function () {
+    validate.onFocus(assistanceGetEmail);
+});
+
+assistanceGetMobile.on("focus", function () {
+    validate.onFocus(assistanceGetMobile);
+});
+
+/* input blur */
+assistanceGetName.on("blur", function () {
+    validate.onBlur(assistanceGetName);
+});
+
+assistanceGetEmail.on("blur", function () {
+    validate.onBlur(assistanceGetEmail);
+});
+
+assistanceGetMobile.on("blur", function () {
+    validate.onBlur(assistanceGetMobile);
+});
+
+$('#submitAssistanceFormBtn').on('click', function () {
+    var isValidDetails = false;
+    isValidDetails &= validateBike(assistGetModel);
+    isValidDetails = ValidateUserDetail(assistanceGetName, assistanceGetEmail, assistanceGetMobile);
+    if (isValidDetails) {
+        // valid
+    }
+});
+
+function ValidateUserDetail(fullName, emailid, mobile) {
+    return validateUserInfo(fullName, emailid, mobile);
+};
+
+var validateUserInfo = function (leadUsername, leadEmailId, leadMobileNo) {
+    var isValid = true;
+    isValid = validateUserName(leadUsername);
+    isValid &= validateEmailId(leadEmailId);
+    isValid &= validateMobileNo(leadMobileNo);
+    return isValid;
+};
+
+var validateUserName = function (leadUsername) {
+    var isValid = true,
+		nameLength = leadUsername.val().length;
+    if (leadUsername.val().indexOf('&') != -1) {
+        validate.setError(leadUsername, 'Invalid name');
+        isValid = false;
+    }
+    else if (nameLength == 0) {
+        validate.setError(leadUsername, 'Please enter your name');
+        isValid = false;
+    }
+    else if (nameLength >= 1) {
+        validate.hideError(leadUsername);
+        isValid = true;
+    }
+    return isValid;
+};
+
+var validateEmailId = function (leadEmailId) {
+    var isValid = true,
+		emailVal = leadEmailId.val(),
+		reEmail = /^[A-z0-9._+-]+@[A-z0-9.-]+\.[A-z]{2,6}$/;
+    if (emailVal == "") {
+        validate.setError(leadEmailId, 'Please enter email id');
+        isValid = false;
+    }
+    else if (!reEmail.test(emailVal)) {
+        validate.setError(leadEmailId, 'Invalid Email');
+        isValid = false;
+    }
+    return isValid;
+};
+
+var validateMobileNo = function (leadMobileNo) {
+    var isValid = true,
+		mobileVal = leadMobileNo.val(),
+		reMobile = /^[1-9][0-9]{9}$/;
+    if (mobileVal == "") {
+        validate.setError(leadMobileNo, "Please enter your mobile no.");
+        isValid = false;
+    }
+    else if (mobileVal[0] == "0") {
+        validate.setError(leadMobileNo, "Mobile no. should not start with zero");
+        isValid = false;
+    }
+    else if (!reMobile.test(mobileVal) && isValid) {
+        validate.setError(leadMobileNo, "Mobile no. should be 10 digits only");
+        isValid = false;
+    }
+    else
+        validate.hideError(leadMobileNo)
+    return isValid;
+};
+
+var validateBike = function (bike) {
+    if (assistGetModel.val().length == 0) {
+        validate.bike.setError(bike, 'Select a bike');
+        return true;
+    }
+    else {
+        validate.bike.hideError(bike);
+        return false;
+    }
+
+}
+
+/* form validation */
+var validate = {
+    setError: function (element, message) {
+        var elementLength = element.val().length;
+        errorTag = element.siblings('span.error-text');
+
+        errorTag.show().text(message);
+        if (!elementLength) {
+            element.closest('.input-box').removeClass('not-empty').addClass('invalid');
+        }
+        else {
+            element.closest('.input-box').addClass('not-empty invalid');
+        }
+    },
+
+    hideError: function (element) {
+        element.closest('.input-box').removeClass('invalid').addClass('not-empty');
+        element.siblings('span.error-text').text('');
+    },
+
+    onFocus: function (inputField) {
+        if (inputField.closest('.input-box').hasClass('invalid')) {
+            validate.hideError(inputField);
+        }
+    },
+
+    onBlur: function (inputField) {
+        var inputLength = inputField.val().length;
+        if (!inputLength) {
+            inputField.closest('.input-box').removeClass('not-empty');
+        }
+        else {
+            inputField.closest('.input-box').addClass('not-empty');
+        }
+    },
+
+    bike: {
+        setError: function (element, message) {
+            var dropdownWrapper = element.closest('.dropdown-select-wrapper'),
+                errorTag = dropdownWrapper.find('.error-text');
+
+            dropdownWrapper.addClass('invalid');
+            errorTag.show().text(message);
+        },
+
+        hideError: function (element) {
+            var dropdownWrapper = element.closest('.dropdown-select-wrapper'),
+                errorTag = dropdownWrapper.find('.error-text');
+
+            dropdownWrapper.removeClass('invalid');
+            errorTag.text('');
+        }
+    }
+}
