f7f13b055ebe0e674fe6c0decf52bf15bcb3c053
Sajal Gupta <sajal.gupta@aepl.com>
2016-10-06 14:02:13 +0530

    Added metas, rewriterule, deviceDetection for desktop page and mobile page of browse used bikes by city

--- BikeWale.UI/used/BikesInCity.aspx.cs
+++ BikeWale.UI/used/BikesInCity.aspx.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web;
-using System.Web.UI;
+﻿using Bikewale.Common;
+using System;
 using System.Web.UI.WebControls;
 
 namespace Bikewale.Used
@@ -9,7 +7,7 @@ namespace Bikewale.Used
     /// <summary>
     /// Created By : Ashwini Todkar on 3 April 2014
     /// </summary>
-    
+
     public class BikesInCity : System.Web.UI.Page
     {
         protected Repeater rptCity;
@@ -22,8 +20,21 @@ namespace Bikewale.Used
             base.Load += new EventHandler(Page_Load);
         }
 
+        /// <summary>
+        /// Modified by : Sajal Gupta on 06-10-2016
+        /// Desc : Added device detection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Device Detection
+            string originalUrl = Request.ServerVariables["HTTP_X_ORIGINAL_URL"];
+            if (String.IsNullOrEmpty(originalUrl))
+                originalUrl = Request.ServerVariables["URL"];
+            DeviceDetection dd = new DeviceDetection(originalUrl);
+            dd.DetectDevice();
+
             if (!IsPostBack)
             {
                 BindCities();
