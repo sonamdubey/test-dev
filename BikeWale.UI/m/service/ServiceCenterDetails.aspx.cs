fe0cbf0c7e245d87a6ba2d9bb630fbead69f93d6
dutiyesh <dutiyesh.salunkhe@carwale.com>
2016-11-01 15:12:19 +0530

    Add service center details page for mobile - Add new service center details page

---
+++ BikeWale.UI/m/service/ServiceCenterDetails.aspx.cs
@@ -0,0 +1,244 @@
+﻿using Bikewale.Cache.BikeData;
+using Bikewale.Cache.Core;
+using Bikewale.Cache.DealersLocator;
+using Bikewale.CoreDAL;
+using Bikewale.DAL.BikeData;
+using Bikewale.DAL.Dealer;
+using Bikewale.Entities.BikeData;
+using Bikewale.Entities.DealerLocator;
+using Bikewale.Entities.PriceQuote;
+using Bikewale.Interfaces.BikeData;
+using Bikewale.Interfaces.Cache.Core;
+using Bikewale.Interfaces.Dealer;
+using Bikewale.Mobile.Controls;
+using Bikewale.Notifications;
+using Microsoft.Practices.Unity;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+
+namespace Bikewale.Mobile.Service
+{
+    /// <summary>
+    /// Modified By : Sushil Kumar
+    /// Modified On : 25 March 2016
+    /// Description : To show dealer details based on dealer id an campaign id.
+    /// Modified By : Lucky Rathore on 30 March 2016
+    /// Description : dealerLat, dealerLong, dealerName, dealerArea, dealerCity added and _dealerQuery removed.
+    /// </summary>
+    public class ServiceCenterDetails : System.Web.UI.Page
+    {
+        protected Repeater rptModels, rptModelList;
+        protected uint dealerId, campaignId = 0, cityId;
+        protected int dealerBikesCount = 0;
+        protected DealerDetailEntity dealerDetails;
+        protected bool isDealerDetail;
+        protected string cityName = string.Empty;
+        protected string makeName = string.Empty, dealerName = string.Empty, dealerArea = string.Empty, dealerCity = string.Empty;
+        protected double dealerLat, dealerLong;
+        protected DealersCard ctrlDealerCard;
+        protected LeadCaptureControl ctrlLeadCapture;
+        protected String clientIP = CommonOpn.GetClientIP();
+        protected string maskingNumber;
+        protected string makeMaskingName;
+        protected int makeId;
+        protected string cityMaskingName = String.Empty;
+
+        protected override void OnInit(EventArgs e)
+        {
+            this.Load += new EventHandler(Page_Load);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (ProcessQueryString() && dealerId > 0)
+            {
+                GetDealerDetails();
+            }
+
+        }
+
+        #region Get Dealer Details
+        /// <summary>
+        /// Created By : Sushil Kumar
+        /// Created On : 25th March 2016 
+        /// Description : To get dealer details and bikes available at dealership
+        /// Modified By : Lucky Rathore on 30 March 2016
+        /// Description : dealerLat, dealerLong, dealerName, dealerArea, dealerCity Intialize, renamed dealer from _dealer.
+        /// Modified By : Sajal Gupta on 26-09-2016
+        /// Description : Changed method to get details only on basis of (dealerId and makeid) and added details of dealer to the controller.
+        /// </summary>
+        private void GetDealerDetails()
+        {
+            DealerBikesEntity dealer = null;
+            try
+            {
+                using (IUnityContainer container = new UnityContainer())
+                {
+                    container.RegisterType<IDealerCacheRepository, DealerCacheRepository>()
+                             .RegisterType<ICacheManager, MemcacheManager>()
+                             .RegisterType<IDealer, DealersRepository>()
+                            ;
+                    var objCache = container.Resolve<IDealerCacheRepository>();
+                    dealer = objCache.GetDealerDetailsAndBikesByDealerAndMake(dealerId, makeId);
+
+                    if (dealer != null && dealer.DealerDetails != null)
+                    {
+                        dealerDetails = dealer.DealerDetails;
+
+                        isDealerDetail = true;
+
+                        cityMaskingName = dealerDetails.CityMaskingName;
+
+                        dealerName = dealerDetails.Name;
+                        dealerArea = dealerDetails.Area.AreaName;
+                        dealerCity = dealerDetails.City;
+                        if (dealerDetails.Area != null)
+                        {
+                            dealerLat = dealerDetails.Area.Latitude;
+                            dealerLong = dealerDetails.Area.Longitude;
+                        }
+                        ctrlDealerCard.MakeId = (uint)dealerDetails.MakeId;
+                        ctrlDealerCard.makeMaskingName = dealerDetails.MakeMaskingName;
+                        ctrlDealerCard.makeName = dealerDetails.MakeName;
+                        ctrlDealerCard.CityId = (uint)dealerDetails.CityId;
+                        ctrlDealerCard.cityName = dealerCity;
+                        ctrlDealerCard.PageName = "Dealer_Details";
+                        ctrlDealerCard.TopCount = 6;
+                        ctrlDealerCard.PQSourceId = (int)PQSourceEnum.Mobile_dealer_details_Get_offers;
+                        ctrlDealerCard.LeadSourceId = 15;
+
+                        makeName = dealerDetails.MakeName;
+                        campaignId = dealerDetails.CampaignId;
+                        ctrlDealerCard.DealerId = (int)dealerId;
+
+                        ctrlLeadCapture.CityId = (uint)dealerDetails.CityId;
+
+                        maskingNumber = dealerDetails.MaskingNumber;
+
+                        if (dealer.Models != null && dealer.Models.Count() > 0)
+                        {
+                            rptModels.DataSource = dealer.Models;
+                            rptModels.DataBind();
+                            dealerBikesCount = dealer.Models.Count();
+                        }
+                    }
+                    else
+                    {
+                        Response.Redirect("/pagenotfound.aspx", false);
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        this.Page.Visible = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn(ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, "GetDealerDetails");
+                objErr.SendMail();
+            }
+
+        }
+        #endregion
+
+        #region Private Method to process querystring
+        /// <summary>
+        /// Created By : Sushil Kumar
+        /// Created On : 16th March 2016 
+        /// Description : Private Method to parse encoded query string and get values for dealerId and campaignId
+        /// Modified By : Lucky Rathore on 30 March 2016
+        /// Description : Renamed dealerQuery from _dealerQuery.
+        /// Modified By : Sajal Gupta on 29-09-2016
+        /// Description : Changed query string parametres.
+        /// Modified by :   Sumit Kate on 03 Oct 2016
+        /// Description :   Handle Make masking name rename 301 redirection
+        /// </summary>
+        private bool ProcessQueryString()
+        {
+            var currentReq = HttpContext.Current.Request;
+            MakeMaskingResponse objResponse = null;
+            bool isSucess = true;
+            try
+            {
+
+                if (currentReq.QueryString != null && currentReq.QueryString.HasKeys())
+                {
+                    uint.TryParse(currentReq.QueryString["dealerId"], out dealerId);
+                    makeMaskingName = currentReq.QueryString["makemaskingname"];
+
+                    if (!String.IsNullOrEmpty(makeMaskingName))
+                    {
+
+                        using (IUnityContainer container = new UnityContainer())
+                        {
+                            container.RegisterType<IBikeMakesCacheRepository<int>, BikeMakesCacheRepository<BikeMakeEntity, int>>()
+                                  .RegisterType<ICacheManager, MemcacheManager>()
+                                  .RegisterType<IBikeMakes<BikeMakeEntity, int>, BikeMakesRepository<BikeMakeEntity, int>>()
+                                 ;
+                            var objCache = container.Resolve<IBikeMakesCacheRepository<int>>();
+
+                            objResponse = objCache.GetMakeMaskingResponse(makeMaskingName);
+                        }
+                    }
+                    else
+                    {
+                        Response.Redirect(CommonOpn.AppPath + "pageNotFound.aspx", false);
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        this.Page.Visible = false;
+                        isSucess = false;
+                    }
+                    return true;
+                }
+                else
+                {
+                    Response.Redirect("/pagenotfound.aspx", false);
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                    this.Page.Visible = false;
+                    isSucess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn("ProcessQueryString Ex: ", ex.Message);
+                ErrorClass objErr = new ErrorClass(ex, currentReq.ServerVariables["URL"]);
+                objErr.SendMail();
+                isSucess = false;
+            }
+            finally
+            {
+                if (objResponse != null)
+                {
+                    if (objResponse.StatusCode == 200)
+                    {
+                        makeId = Convert.ToInt32(objResponse.MakeId);
+                        isSucess = true;
+                    }
+                    else if (objResponse.StatusCode == 301)
+                    {
+                        CommonOpn.RedirectPermanent(Request.RawUrl.Replace(makeMaskingName, objResponse.MaskingName));
+                        isSucess = false;
+                    }
+                    else
+                    {
+                        Response.Redirect(CommonOpn.AppPath + "pageNotFound.aspx", false);
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        this.Page.Visible = false;
+                        isSucess = false;
+                    }
+                }
+                else
+                {
+                    Response.Redirect(CommonOpn.AppPath + "pageNotFound.aspx", false);
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                    this.Page.Visible = false;
+                    isSucess = false;
+                }
+            }
+            return isSucess;
+        }
+        #endregion
+    }
+}
---
+++ BikeWale.UI/m/src/service/details.js
@@ -0,0 +1,152 @@
+﻿var originPlace, userLocation = { "latitude": "", "longitude": "" }, userAddress = "";
+
+$(document).ready(function () {
+    $('.chosen-select').chosen();
+});
+
+initializeMap();
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            savePosition,
+            showError,
+            { enableHighAccuracy: true, maximumAge: 600000 }
+        );
+    }
+}
+
+$(document).on("click", "#getUserLocation", function () { getLocation(); });
+
+function savePosition(position) {
+    userLocation = {
+        "latitude": position.coords.latitude,
+        "longitude": position.coords.longitude
+    }
+    if (userAddress.trim() == "") {
+        $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + userLocation.latitude + "," + userLocation.longitude + "&key=" + googleMapAPIKey, function (data) {
+            if (data.status == "OK" && data.results.length > 0) {
+                userAddress = data.results[0].formatted_address;
+            }
+            else {
+                userAddress = "";
+            }
+            $("#locationSearch").val("").val(userAddress);
+            google.maps.event.trigger(originPlace, 'place_changed');
+        });
+    }
+    setUserLocation(position);
+}
+
+function setUserLocation(position) {
+    $("#anchorGetDir").attr("href", "https://maps.google.com/?saddr=" + position.coords.latitude + "," + position.coords.longitude + "&daddr=" + dealerLat + "," + dealerLong + '');
+}
+$("#assistanceBrandInput").on("keyup", function () {
+    locationFilter($(this));
+});
+
+function initializeMap() {
+    getLocation();
+    originPlace = new google.maps.places.Autocomplete(
+      (document.getElementById('locationSearch')),
+      {
+          types: ['geocode'],
+          componentRestrictions: { country: "in" }
+      });
+
+    google.maps.event.addListener(originPlace, 'place_changed', function () {
+
+        var place = originPlace.getPlace();
+        if (!(place && place.geometry)) {
+            origin_place_id = new google.maps.LatLng(userLocation.latitude, userLocation.longitude);
+        }
+        else {
+
+            origin_place_id = place.geometry.location
+            userLocation.latitude = place.geometry.location.lat();
+            userLocation.longitude = place.geometry.location.lng();
+            userAddress = place.formatted_address.trim();
+        };
+
+        travel_mode = google.maps.TravelMode.DRIVING;
+
+        var directionsService = new google.maps.DirectionsService;
+
+        if (originPlace != null && originPlace.getPlace() != null && originPlace.getPlace().name != null && originPlace.getPlace().name.trim() != "") {
+            route(origin_place_id, travel_mode, directionsService);
+            $('.location-details').show();
+        }
+
+
+    });
+}
+
+
+function route(origin_place_id, travel_mode, directionsService) {
+
+    _lat = dealerLat;
+    _lng = dealerLong;
+    destination_place_id = new google.maps.LatLng(_lat, _lng);
+
+    if (!origin_place_id || !destination_place_id) {
+        return;
+    }
+
+    directionsService.route({
+        origin: origin_place_id,
+        destination: destination_place_id,
+        travelMode: travel_mode
+    }, function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            getCommuteInfo(response);
+        }
+        else {
+            $(".location-details").addClass("hide");
+        }
+    });
+}
+
+function getCommuteInfo(result) {
+    var totalDistance = 0;
+    var totalDuration = 0;
+    var legs = result.routes[0].legs;
+    for (var i = 0; i < legs.length; ++i) {
+        totalDistance += legs[i].distance.value;
+        totalDuration += legs[i].duration.value;
+    }
+    $('#commuteDistance').text((totalDistance / 1000).toFixed(2) + " kms");
+    $('#commuteDuration').text(totalDuration.toString().toHHMMSS());
+    $(".location-details").removeClass("hide");
+}
+
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            break;
+    }
+}
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10);
+    var hrs = Math.floor(sec_num / 3600);
+    var mins = Math.floor((sec_num - (hrs * 3600)) / 60);
+    var secs = sec_num - (hrs * 3600) - (mins * 60);
+
+    if (hrs < 10) { hrs = "0" + hrs; }
+    if (mins < 10) { mins = "0" + mins; }
+    if (secs < 10) { secs = "0" + secs; }
+    var time = hrs + ' hrs ' + mins + ' mins ';// + secs +" s";
+    return time;
+}
+
