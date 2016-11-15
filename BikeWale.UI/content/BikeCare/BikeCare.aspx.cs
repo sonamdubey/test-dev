4af8f172e3f643e83822f3d0169ff6592bfb8c1d
subodh jain <subodh.jain@carwale.com>
2016-11-15 18:37:32 +0530

    Maintenance Tips Desktop page bike care landing page
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

---
+++ BikeWale.UI/content/BikeCare/BikeCare.aspx.cs
@@ -0,0 +1,56 @@
+﻿using Bikewale.BindViewModels.Webforms.EditCMS;
+using Bikewale.Entities.CMS.Articles;
+using Bikewale.Mobile.Controls;
+using System;
+
+namespace Bikewale.Content
+{
+    public class BikeCare : System.Web.UI.Page
+    {
+        BikeCareModels objBikeCare = null;
+        public LinkPagerControl ctrlPager;
+        protected uint makeId, modelId, totalArticles;
+        public string pgPrevUrl = string.Empty, pgNextUrl = string.Empty, pgTitle = string.Empty, pgDescription = string.Empty, pgKeywords = string.Empty;
+        protected CMSContent objArticleList;
+        public int startIndex = 0, endIndex = 0;
+        protected override void OnInit(EventArgs e)
+        {
+            this.Load += new EventHandler(Page_Load);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BikeCareTips();
+        }
+        /// <summary>
+        /// Created By:- Subodh jain 15 Nov 2016
+        /// Summary :- Bike Care Landing page Binding
+        /// </summary>
+        private void BikeCareTips()
+        {
+            objBikeCare = new BikeCareModels();
+            if (objBikeCare != null)
+            {
+                try
+                {
+
+                    objBikeCare.BindLinkPager(ctrlPager);
+                    objArticleList = objBikeCare.objArticleList;
+                    pgTitle = objBikeCare.title;
+                    pgDescription = objBikeCare.description;
+                    pgKeywords = objBikeCare.keywords;
+                    pgPrevUrl = objBikeCare.prevUrl;
+                    pgNextUrl = objBikeCare.nextUrl;
+                    totalArticles = objBikeCare.totalRecords;
+                    startIndex = objBikeCare.startIndex;
+                    endIndex = objBikeCare.endIndex;
+                }
+                catch (Exception ex)
+                {
+                    Bikewale.Notifications.ErrorClass objErr = new Bikewale.Notifications.ErrorClass(ex, "BikeCare.BikeCareTips");
+                    objErr.SendMail();
+                }
+            }
+        }
+    }
+}
