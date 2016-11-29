bbcccceea4ceed93e9653ea3dc606b673d5462a5
subodh jain <subodh.jain@carwale.com>
2016-11-29 16:45:59 +0530

    changed URL
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- Bikewale.Notifications/MailTemplates/UsedBikes/PhotoRequestToCustomerForSevenDays.cs
+++ Bikewale.Notifications/MailTemplates/UsedBikes/PhotoRequestToCustomerForSevenDays.cs
@@ -29,7 +29,7 @@ namespace Bikewale.Notifications.MailTemplates.UsedBikes
                 sb.AppendFormat(@"Dear {4},<p>Thanks for choosing BikeWale to sell your <Make Model> bike.You are missing on opportunities because you have not uploaded any photo yet.</p>
                       <p>Were you expecting more responses than what you have received? Our data suggests that Ads without photos get far lesser responses than Ads with high-quality photos.
                        BikeWale recommends you to add high-quality photos for your bike to get more responses.</p>
-                       To add photos click on the <a href='{2}/used/sell/?id={3}'>link</a><p>Cheers!</p><p>Team BikeWale</p>", _make, _model, Utility.BWConfiguration.Instance.BwHostUrl, _profileId, _customerName);
+                       To add photos click on the <a href='{2}/used/sell/?id={3}#uploadphoto'>link</a><p>Cheers!</p><p>Team BikeWale</p>", _make, _model, Utility.BWConfiguration.Instance.BwHostUrl, _profileId, _customerName);
             }
 
             return sb.ToString();
--- Bikewale.Notifications/MailTemplates/UsedBikes/PhotoRequestToCustomerForThreeDays.cs
+++ Bikewale.Notifications/MailTemplates/UsedBikes/PhotoRequestToCustomerForThreeDays.cs
@@ -29,7 +29,7 @@ namespace Bikewale.Notifications.MailTemplates.UsedBikes
                 sb.AppendFormat(@"Dear {4},<p>Thanks for choosing BikeWale to sell your {0} {1} bike. You are missing on opportunities because you have not uploaded any photo yet.
                     We have seen that buyers are showing more interest on Ads with high-quality photos.</p>
                     <p>Our data suggests that Ads with photos get 50% more responses than Ads without photos. BikeWale recommends you to add high-quality photos for your bike to get more responses. </p>
-                    To add photos click on the <a href='{2}/used/sell/?id={3}'>link</a>
+                    To add photos click on the <a href='{2}/used/sell/?id={3}#uploadphoto'>link</a>
                     <p>Cheers!</p><p>Team BikeWale</p>", _make, _model, Utility.BWConfiguration.Instance.BwHostUrl, _profileId, _customerName);
             }
 
