1301c0871810c393a1971a24219bfe03b0c91f91
subodh jain <subodh.jain@carwale.com>
2016-11-30 12:19:04 +0530

    url Changes
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- Bikewale.Notifications/MailTemplates/UsedBikes/PhotoRequestToCustomerForSevenDays.cs
+++ Bikewale.Notifications/MailTemplates/UsedBikes/PhotoRequestToCustomerForSevenDays.cs
@@ -29,7 +29,7 @@ namespace Bikewale.Notifications.MailTemplates.UsedBikes
                 sb.AppendFormat(@"Dear {3},<p>Thanks for choosing BikeWale to sell your <Make Model> bike.You are missing on opportunities because you have not uploaded any photo yet.</p>
                       <p>Were you expecting more responses than what you have received? Our data suggests that Ads without photos get far lesser responses than Ads with high-quality photos.
                        BikeWale recommends you to add high-quality photos for your bike to get more responses.</p>
-                       To add photos click on the <a href='{4}/used/sell/?id={2}&hash=uploadphoto'", _make, _model, _profileId, _customerName, Bikewale.Utility.BWConfiguration.Instance.BwHostUrl);
+                       To add photos click on the <a href='{4}/used/sell/?id={2}&hash=uploadphoto'>link</a><p>Cheers!</p><p>Team BikeWale</p>", _make, _model, _profileId, _customerName, Bikewale.Utility.BWConfiguration.Instance.BwHostUrl);
             }
 
             return sb.ToString();
