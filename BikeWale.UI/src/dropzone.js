25a1f4dc39854027ca5f9e9731fb0676d240e1b5
cw-sumitkate <sumit.kate@carwale.com>
2016-12-01 15:22:42 +0530

    Handle fallback option for sell bike - photo upload is supported only on listed browsers. - Show warning message when non-supported browser is used to upload the photo in sell bike form
    Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

--- BikeWale.UI/src/dropzone.js
+++ BikeWale.UI/src/dropzone.js
@@ -737,20 +737,82 @@
             if (existingFallback = this.getExistingFallback()) {
                 return existingFallback;
             }
-            fieldsString = "<div class=\"dz-fallback\">";
-            if (this.options.dictFallbackText) {
-                fieldsString += "<p>" + this.options.dictFallbackText + "</p>";
-            }
-            fieldsString += "<input type=\"file\" name=\"" + (this._getParamName(0)) + "\" " + (this.options.uploadMultiple ? 'multiple="multiple"' : void 0) + " /><input type=\"submit\" value=\"Upload!\"></div>";
+            fieldsString = "<div class=\"dz-fallback\">";            
+            fieldsString += "<div>Icon Header</div>";
+            fieldsString += "<p class='font18 margin-bottom5 text-grey text-bold text-center'>Your current browser is outdated!</p>"
+            fieldsString += "<p class='font14 margin-bottom5 text-grey text-center'>" + this.getBrowserName() + " browser does not support photo upload functionality. Try upgrading to any of the following browsers</p>"
+            fieldsString += "<div><img src='http://imgd2.aeplcdn.com/0x0/bw/static/sprites/d/image-upload-support.png' ></img></div>"
+            fieldsString += "</div>"
             fields = Dropzone.createElement(fieldsString);
-            if (this.element.tagName !== "FORM") {
-                form = Dropzone.createElement("<form action=\"" + this.options.url + "\" enctype=\"multipart/form-data\" method=\"" + this.options.method + "\"></form>");
-                form.appendChild(fields);
-            } else {
-                this.element.setAttribute("enctype", "multipart/form-data");
-                this.element.setAttribute("method", this.options.method);
+            
+            return fields;
+        };
+
+        Dropzone.prototype.getBrowserName = function () {
+            debugger;
+            var nVer = navigator.appVersion;
+            var nAgt = navigator.userAgent;
+            var browserName = navigator.appName;
+            var fullVersion = '' + parseFloat(navigator.appVersion);
+            var majorVersion = parseInt(navigator.appVersion, 10);
+            var nameOffset, verOffset, ix;
+
+            // In Opera 15+, the true version is after "OPR/" 
+            if ((verOffset = nAgt.indexOf("OPR/")) != -1) {
+                browserName = "Opera";
+                fullVersion = nAgt.substring(verOffset + 4);
             }
-            return form != null ? form : fields;
+                // In older Opera, the true version is after "Opera" or after "Version"
+            else if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+                browserName = "Opera";
+                fullVersion = nAgt.substring(verOffset + 6);
+                if ((verOffset = nAgt.indexOf("Version")) != -1)
+                    fullVersion = nAgt.substring(verOffset + 8);
+            }
+                // In MSIE, the true version is after "MSIE" in userAgent
+            else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+                browserName = "Microsoft Internet Explorer";
+                fullVersion = nAgt.substring(verOffset + 5);
+            }
+                // In Chrome, the true version is after "Chrome" 
+            else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+                browserName = "Chrome";
+                fullVersion = nAgt.substring(verOffset + 7);
+            }
+                // In Safari, the true version is after "Safari" or after "Version" 
+            else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+                browserName = "Safari";
+                fullVersion = nAgt.substring(verOffset + 7);
+                if ((verOffset = nAgt.indexOf("Version")) != -1)
+                    fullVersion = nAgt.substring(verOffset + 8);
+            }
+                // In Firefox, the true version is after "Firefox" 
+            else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+                browserName = "Firefox";
+                fullVersion = nAgt.substring(verOffset + 8);
+            }
+                // In most other browsers, "name/version" is at the end of userAgent 
+            else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
+                      (verOffset = nAgt.lastIndexOf('/'))) {
+                browserName = nAgt.substring(nameOffset, verOffset);
+                fullVersion = nAgt.substring(verOffset + 1);
+                if (browserName.toLowerCase() == browserName.toUpperCase()) {
+                    browserName = navigator.appName;
+                }
+            }
+            // trim the fullVersion string at semicolon/space if present
+            if ((ix = fullVersion.indexOf(";")) != -1)
+                fullVersion = fullVersion.substring(0, ix);
+            if ((ix = fullVersion.indexOf(" ")) != -1)
+                fullVersion = fullVersion.substring(0, ix);
+
+            majorVersion = parseInt('' + fullVersion, 10);
+            if (isNaN(majorVersion)) {
+                fullVersion = '' + parseFloat(navigator.appVersion);
+                majorVersion = parseInt(navigator.appVersion, 10);
+            }
+
+            return browserName + ' ' + majorVersion;
         };
 
         Dropzone.prototype.getExistingFallback = function () {
