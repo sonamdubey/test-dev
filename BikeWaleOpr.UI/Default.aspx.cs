8ee891e1b2d37765271b084481c96468ee2eebca
Sajal Gupta <sajal.gupta@aepl.com>
2016-12-23 11:48:00 +0530

    Fixed code review changes

--- BikeWaleOpr.UI/Default.aspx.cs
+++ BikeWaleOpr.UI/Default.aspx.cs
@@ -38,13 +38,19 @@ namespace BikeWaleOpr
             this.Load += new EventHandler(Page_Load);
         }
 
+        /// <summary>
+        /// Modified by : Sajal gupta on 22-12-2016
+        /// Desc : Added condition for showing notification panel to nandu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
             if (CurrentUser.Id == "-1")
             {
                 Response.Redirect("/users/login.aspx");
             }
-            else if (CurrentUser.Id == "1225")
+            else if (CurrentUser.Id == "1225") //customer id of nandu
             {
                 isShownNotification = true;
             }
@@ -53,15 +59,19 @@ namespace BikeWaleOpr
             NotificationTrigger();
         }
 
+        /// <summary>
+        /// Created by Sajal Gupta o 22-12-2016
+        /// Desc : Mail trigger to nandu and abhishek .
+        /// </summary>
         protected void NotificationTrigger()
         {
             dataObj = _objModelsRepo.GetLastSoldUnitData();
 
             if (isShownNotification && dataObj.IsEmailToSend == 1)
             {
-                string[] arr1 = new string[] { "abhishek.singh@carwale.com", "sajal.gupta@carwale.com" };                
+                string[] arr1 = new string[] { "abhishek.singh@carwale.com" };
                 ComposeEmailBase objEmail = new ModelSoldUnitMailTemplate("NandKishor Sanas", dataObj.LastUpdateDate);
-                objEmail.Send("nandkishor.sanas@carwale.com", "Please update last month model sold data", "", arr1, null);                
+                objEmail.Send("nandkishor.sanas@carwale.com", "Please update last month model sold data", "", arr1, null);
             }
         }
     }
