1bab02305ea4dd2d31c7aa147710f66185b1e8ca
Ashish Kamble <ashish.kamble@carwale.com>
2017-01-02 10:52:51 +0530

    This commit will add new project for bikewale opr in mvc - bikewale opr ui change - bikewale opr ui built using materializecss - bikewale opr mvc project - dapper used for database operations - layered architecture for opr

---
+++ BikewaleOpr.Service/Controllers/Content/MakesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bikewale.Notifications;
+using BikewaleOpr.DALs.Bikedata;
+using BikewaleOpr.Interface.BikeData;
+using Microsoft.Practices.Unity;
+
+namespace BikewaleOpr.Service.Controllers.Content
+{    
+    public class MakesController : ApiController
+    {
+        [Route("api/makes/{makeid}/synopsis/"), HttpGet]
+        //[HttpGet]
+        public IHttpActionResult GetSynopsis(int makeId)
+        {
+            if (makeId > 0)
+            {
+                string synopsis = string.Empty;
+
+                try
+                {
+                    using (IUnityContainer container = new UnityContainer())
+                    {
+                        container.RegisterType<IBikeMakes, BikeMakesRepository>();
+                        IBikeMakes objRepo = container.Resolve<IBikeMakes>();
+
+                        synopsis = objRepo.Getsynopsis(makeId);
+                    }
+                }
+                catch (Exception ex)
+                {                    
+                    ErrorClass objErr = new ErrorClass(ex, "GetSynopsis");
+                    objErr.SendMail();
+                }
+
+                if (!String.IsNullOrEmpty(synopsis))
+                    return Ok(synopsis);
+                else
+                    return NotFound();
+            }
+            else
+                return BadRequest();        
+        }
+    }
+}
