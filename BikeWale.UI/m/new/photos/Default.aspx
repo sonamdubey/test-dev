0ab1499a4bbe892e5e2ac6d232b6843cdb477397
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-01-10 18:14:33 +0530

    Update popup gallery - Update popup gallery on photos page

--- BikeWale.UI/m/new/photos/Default.aspx
+++ BikeWale.UI/m/new/photos/Default.aspx
@@ -25,12 +25,12 @@
 <body>
     <form id="form1" runat="server">
         <!-- #include file="/includes/headBW_Mobile.aspx" -->
-         <% if (vmModelPhotos != null)
-           {
-               var objImages = vmModelPhotos.objImageList; %>
+        <% if (vmModelPhotos != null)
+        {
+            var objImages = vmModelPhotos.objImageList; %>
         <section>
             <div class="container box-shadow section-bottom-margin">
-                <h1 class="section-header"><%= vmModelPhotos.bikeName %> Photos</h1>
+                <h1 class="section-header bg-white"><%= vmModelPhotos.bikeName %> Photos</h1>
                  <% int i = 0; if (vmModelPhotos.totalPhotosCount > 0)
                    { %>
                 <ul class="photos-grid-list">
@@ -61,30 +61,87 @@
                 <div class="clear"></div>
             </div>
         </section>
-       <% if(!isUpcoming) { %>
+        <% if(!isUpcoming) { %>
         <section>
             <div class="container bg-white box-shadow padding-15-20 section-bottom-margin">
                 <h2 class="margin-bottom15">Know more about this bike</h2>
-                   <BW:GenericBikeInfo  ID="ctrlGenericBikeInfo" runat="server" />
+                <BW:GenericBikeInfo  ID="ctrlGenericBikeInfo" runat="server" />
             </div>
         </section>
          <% } %>
         <% } %>
 
-    <%if (ctrlVideos.FetchedRecordsCount > 0)
+        <%if (ctrlVideos.FetchedRecordsCount > 0)
         { %>
         <section>
             <div class="container bg-white box-shadow padding-15-20 section-bottom-margin">
-                    <h2 class="margin-bottom15"><%= vmModelPhotos.bikeName %> Videos</h2>
-                    <BW:Videos runat="server" ID="ctrlVideos" />
+                <h2 class="margin-bottom15"><%= vmModelPhotos.bikeName %> Videos</h2>
+                <BW:Videos runat="server" ID="ctrlVideos" />
             </div>
         </section>
-    <% } %>
+        <% } %>
 
-         <BW:SimilarBikeWithPhotos  ID="ctrlSimilarBikesWithPhotos" runat="server" />
-         <BW:ModelGallery ID="ctrlModelGallery" runat="server" />
+        <BW:SimilarBikeWithPhotos  ID="ctrlSimilarBikesWithPhotos" runat="server" />
+        <%--<BW:ModelGallery ID="ctrlModelGallery" runat="server" />--%>
         <!-- model-gallery-container ends here -->
 
+        <div id="gallery-container" class="gallery-container">
+            <div class="gallery-header">
+                <h2 class="font16 text-white gallery-title">Bajaj Pulsar AS200 Photos</h2>
+                <div class="gallery-close-btn position-abt pos-top15 pos-right15 bwmsprite cross-md-white cur-pointer"></div>
+                <ul class="horizontal-tabs-wrapper" data-bind="with: galleryHeader">
+                    <li data-bind="click: activateTab, css: photosTabActive() ? 'active' : ''">Photos</li>
+                    <li data-bind="click: activateTab, css: photosTabActive() ? '' : 'active'">Videos</li>
+                </ul>
+            </div>
+
+            <div class="gallery-body">
+                <div data-bind="visible: galleryHeader().photosTabActive()">Photos</div>
+
+                <div data-bind="visible: !galleryHeader().photosTabActive()">Videos</div>
+            </div>
+
+            <div class="gallery-footer" data-bind="css: $root.galleryHeader().photosTabActive() ? '' : 'grid-2-tab', with: galleryFooter">
+                <div class="footer-tabs-wrapper" data-bind="css: screenActive() ? 'footer-shadow': ''">
+                    <div data-bind="visible: $root.galleryHeader().photosTabActive()" class="footer-tab all-option-tab position-rel tab-separator">
+                        <span class="bwmsprite grid-icon margin-right10"></span>
+                        <span class="inline-block font14">All photos</span>
+                    </div>
+                    <div data-bind="visible: !$root.galleryHeader().photosTabActive()" class="footer-tab all-option-tab position-rel tab-separator">
+                        <span class="bwmsprite grid-icon margin-right10"></span>
+                        <span class="inline-block font14">All videos</span>
+                    </div>
+
+                    <div data-bind="visible: $root.galleryHeader().photosTabActive()" class="footer-tab grid-3-tab">
+                        <span class="bwmsprite color-palette"></span>
+                    </div>
+
+                    <div class="footer-tab grid-3-tab" data-bind="click: activateShareScreen, css: shareScreen() ? 'tab-active' : ''">
+                        <span class="bwmsprite share-icon"></span>
+                    </div>
+
+                    <div class="footer-tab grid-3-tab" data-bind="click: activateModelScreen, css: modelScreen() ? 'tab-active' : ''">
+                        <span class="bwmsprite info-icon"></span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+
+                <!-- share -->
+                <div id="share-tab-screen" class="footer-tab-card" data-bind="visible: shareScreen()">
+                    <ul class="social-share-list">
+                        <li>Whatsapp</li>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                    </ul>
+                </div>
+
+                <!-- model info -->
+                <div id="info-tab-screen" class="footer-tab-card" data-bind="visible: modelScreen()">
+                    <h2>Bajaj Pulsar RS200</h2>
+                </div>
+
+            </div>
+        </div>
 
         <script type="text/javascript" src="<%= staticUrl != "" ? "https://st1.aeplcdn.com" + staticUrl : "" %>/m/src/frameworks.js?<%= staticFileVersion %>"></script>
         <!-- #include file="/includes/footerBW_Mobile.aspx" -->
--- BikeWale.UI/m/src/photos.js
+++ BikeWale.UI/m/src/photos.js
@@ -8,149 +8,108 @@
 
         lastPhoto.append(morePhotoCount);
     }
-
-    $('.carousel-navigation-photos .swiper-slide').first().addClass('swiper-slide-active');
-    showImgTitle(galleryTop);
-    $('#videos').hide();
 });
+
+var gallery = function() {
+    var self = this;
+
+    self.galleryHeader = ko.observable(new galleryHeader);
+    self.galleryFooter = ko.observable(new galleryFooter);
+};
+
+var galleryHeader = function () {
+    var self = this;
+
+    self.photosTabActive = ko.observable(true);
+
+    self.activateTab = function () {
+        self.photosTabActive(!self.photosTabActive());
+    };
+
+};
+
+var galleryFooter = function () {
+    var self = this;
+
+    self.screenActive = ko.observable(false);
+
+    self.shareScreen = ko.observable(false);
+    self.modelScreen = ko.observable(false);
+
+    self.activateShareScreen = function () {
+        // reset other screens
+        self.modelScreen(false);
+
+        // toggle screen
+        self.shareScreen(!self.shareScreen());
+        self.screenActive(self.shareScreen());
+
+        // append state to maintain history
+        appendState('shareScreen');
+    };
+
+    self.activateModelScreen = function () {
+        self.shareScreen(false);
+
+        self.modelScreen(!self.modelScreen());
+        self.screenActive(self.modelScreen());
+
+        appendState('modelScreen');
+    };
+};
+
+var vmGallery = new gallery();
+
+ko.applyBindings(vmGallery, document.getElementById('gallery-container'));
 
 $('.photos-grid-list').on('click', 'li', function () {
-    var clickedImg = $(this),
-        imgIndex = clickedImg.index(),
-        parentGridType = clickedImg.closest('.photos-grid-list');
 
-    if (parentGridType.hasClass('remainder-grid-list')) {
-        var gridOneLength = $('.photos-grid-list').first().find('li').length;
-
-        imgIndex = gridOneLength + imgIndex; // (grid type 1's length + grid type remainder's index)
-    }
-
-    gallery.open();
+    galleryPopup.open();
     window.dispatchEvent(new Event('resize'));
-    appendState('gallery');
+    appendState('galleryPopup');
 
-    $("#photos-tab").trigger('click');
-    var clickedSlide = $('.carousel-navigation-photos .swiper-slide')[imgIndex];
-    $('.carousel-navigation-photos .swiper-slide').removeClass('swiper-slide-active');
-    $(clickedSlide).addClass('swiper-slide-active');
-    galleryThumbs.slideTo(imgIndex, 500);
-    galleryTop.slideTo(imgIndex, 500);
-});
-
-var slideToClick = function (swiper) {
-    var clickedSlide = swiper.slides[swiper.clickedIndex];
-    $('.carousel-navigation-photos .swiper-slide').removeClass('swiper-slide-active');
-    $(clickedSlide).addClass('swiper-slide-active');
-    galleryTop.slideTo(swiper.clickedIndex, 500);
-};
-
-var videosThumbs = new Swiper('.carousel-navigation-videos', {
-    slideActiveClass: '',
-    spaceBetween: 0,
-    slidesPerView: 'auto',
-    slideToClickedSlide: true,
-    preloadImages: false,
-    lazyLoading: true,
-    lazyLoadingInPrevNext: true,
-    watchSlidesProgress: true,
-    watchSlidesVisibility: true
-});
-
-var galleryThumbs = new Swiper('.carousel-navigation-photos', {
-    slideActiveClass: '',
-    spaceBetween: 0,
-    slidesPerView: 'auto',
-    slideToClickedSlide: true,
-    preloadImages: false,
-    lazyLoading: true,
-    lazyLoadingInPrevNext: true,
-    watchSlidesProgress: true,
-    watchSlidesVisibility: true,
-    onTap: slideToClick
-});
-
-var slidegalleryThumbs = function (swiper) {
-    galleryThumbs.slideTo(swiper.activeIndex, 500);
-    galleryThumbs.slides.removeClass('swiper-slide-active');
-    galleryThumbs.slides[swiper.activeIndex].className += ' swiper-slide-active';
-
-    showImgTitle(galleryTop);
-};
-
-var galleryTop = new Swiper('.carousel-stage-photos', {
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    spaceBetween: 10,
-    preloadImages: false,
-    lazyLoading: true,
-    lazyLoadingInPrevNext: true,
-    watchSlidesProgress: true,
-    watchSlidesVisibility: true,
-    onSlideChangeEnd: slidegalleryThumbs
 });
 
 $(".gallery-close-btn").on('click', function () {
-    gallery.close();
+    galleryPopup.close();
     history.back();
-});
-
-var currentStagePhoto, currentStageActiveImage;
-function showImgTitle(swiper) {
-    imgTitle = $(galleryTop.slides[swiper.activeIndex]).find('img').attr('title');
-    imgTotalCount = galleryThumbs.slides.length;
-    $(".media-title").text(imgTitle);
-    $(".gallery-count").text(swiper.activeIndex + 1 + " of " + imgTotalCount.toString());
-    currentStagePhoto = $(".connected-carousels-photos .stage-photos");
-    currentStageActiveImage = currentStagePhoto.find(".swiper-slide.swiper-slide-active img");
-    currentStagePhoto.find('.carousel-stage-photos').css({ 'height': currentStageActiveImage.height() });
-}
-
-var videoiFrame = document.getElementById("video-iframe");
-
-$("#photos-tab, #videos-tab").click(function () {
-    firstVideo();
-});
-
-$("#videos-tab").click(function () {
-    $('.carousel-navigation-videos .swiper-slide').removeClass('active');
-    $('.carousel-navigation-videos .swiper-slide').first().addClass('active');
-});
-
-var firstVideo = function () {
-    var a = $(".carousel-navigation-videos .swiper-wrapper").first(".swiper-slide");
-    var newSrc = a.find("img").attr("iframe-data");
-    videoiFrame.setAttribute("src", newSrc);
-};
-
-var navigationVideosLI = $(".carousel-navigation-videos .swiper-slide");
-navigationVideosLI.click(function () {
-    navigationVideosLI.removeClass("active");
-    $(this).addClass("active");
-    var newSrc = $(this).find("img").attr("iframe-data");
-    videoiFrame.setAttribute("src", newSrc);
-    window.dispatchEvent(new Event('resize'));
 });
 
 var appendState = function (state) {
     window.history.pushState(state, '', '');
 };
 
+var galleryContainer = $('#gallery-container'),
+    shareTabScreen = $('#share-tab-screen'),
+    modelInfoTabScreen = $('#info-tab-screen');
+
 $(window).on('popstate', function (event) {
-    if ($('.model-gallery-container').is(':visible')) {
-        gallery.close();
+    if (galleryContainer.is(':visible')) {
+        if (!shareTabScreen.is(':visible') && !modelInfoTabScreen.is(':visible')) {
+            galleryPopup.close();
+        }
+        else {
+            if (shareTabScreen.is(':visible')) {
+                vmGallery.galleryFooter().shareScreen(false);
+            }
+
+            if (modelInfoTabScreen.is(':visible')) {
+                vmGallery.galleryFooter().modelScreen(false);
+            }
+        }
     }
 });
 
-var gallery = {
+var galleryPopup = {
     open: function () {
         lockPopup();
-        $('.model-gallery-container').show();
+        galleryContainer.show();
         $('body').addClass('gallery-popup-active');
     },
 
     close: function () {
         unlockPopup();
-        $('.model-gallery-container').hide();
+        galleryContainer.hide();
         $('body').removeClass('gallery-popup-active');
     }
 };
