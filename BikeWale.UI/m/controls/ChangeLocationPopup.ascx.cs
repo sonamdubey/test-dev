fba894824f4d4bdd52d22443a5413ca3d95356cd
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-01-17 16:55:50 +0530

    Added dummy popup control for loation change mobile - Added dummy popup control for loation change mobile
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

---
+++ BikeWale.UI/m/controls/ChangeLocationPopup.ascx.cs
@@ -0,0 +1,38 @@
+﻿using Bikewale.Entities.Location;
+using System;
+
+namespace Bikewale.Mobile.Controls
+{
+    /// <summary>
+    /// Created By : 
+    /// Description : 
+    /// </summary>
+    public class ChangeLocationPopup : System.Web.UI.UserControl
+    {
+        public uint UrlCityId { get; set; }
+        public string UrlCityName { get; set; }
+        public string CustomMessage { get; set; }
+
+        protected uint CookieCityId;
+        protected string CookieCityName = string.Empty;
+        protected bool IsLocationChange = false;
+
+        protected override void OnInit(EventArgs e)
+        {
+            InitializeComponent();
+        }
+
+        void InitializeComponent()
+        {
+            base.Load += new EventHandler(Page_Load);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GlobalCityAreaEntity objLocation = Bikewale.Utility.GlobalCityArea.GetGlobalCityArea();
+            CookieCityId = objLocation.CityId;
+            CookieCityName = objLocation.City;
+            IsLocationChange = (UrlCityId > 0 && UrlCityId != CookieCityId);
+        }
+    }
+}
