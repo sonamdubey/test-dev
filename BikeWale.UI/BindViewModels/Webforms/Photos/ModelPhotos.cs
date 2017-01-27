9c11b8aa28207fab3ba6a15dba155c3a1fb13276
cw-sangram <sangram.nandkhile@carwale.com>
2017-01-27 19:41:10 +0530

    edited title for photos

--- BikeWale.UI/BindViewModels/Webforms/Photos/ModelPhotos.cs
+++ BikeWale.UI/BindViewModels/Webforms/Photos/ModelPhotos.cs
@@ -138,7 +138,7 @@ namespace Bikewale.BindViewModels.Webforms.Photos
                 if (objMake != null && objModel != null)
                 {
                     pageMetas = new PageMetaTags();
-                    pageMetas.Title = String.Format("{0} {1} Images | {1} Photos - BikeWale", bikeName, objModel.ModelName);
+                    pageMetas.Title = String.Format("{0} Images | {1} Photos - BikeWale", bikeName, objModel.ModelName);
                     pageMetas.Keywords = string.Format("{0} photos, {0} pictures, {0} images, {1} {0} photos", objModel.ModelName, objMake.MakeName);
                     pageMetas.Description = String.Format("View images of {0} in different colors and angles. Check out {2} photos of {1} on BikeWale", objModel.ModelName, bikeName, totalPhotosCount);
                     pageMetas.CanonicalUrl = String.Format("https://www.bikewale.com/{0}-bikes/{1}/images/", objMake.MaskingName, objModel.MaskingName);
