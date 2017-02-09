da5830d5faf9e8dc7dc9b2368403a0f8161e0bd8
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-02-09 14:52:21 +0530

    Change heading text - Change heading text - Add year-wise swiper

--- BikeWale.UI/m/new/newbikelaunch/BikesByMake.aspx.cs
+++ BikeWale.UI/m/new/newbikelaunch/BikesByMake.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Bikewale.Mobile.New.Newbikelaunch
 {
-	public partial class BikeByMakes : System.Web.UI.Page
+	public partial class BikesByMake : System.Web.UI.Page
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
