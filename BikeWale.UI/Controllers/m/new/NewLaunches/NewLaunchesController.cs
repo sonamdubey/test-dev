3b56a5c4924e91bf7f0373bf7d0114c2078dd902
Ashish Kamble <ashish.kamble@carwale.com>
2017-02-13 12:42:36 +0530

    Bikewale M site MVC layout changes

---
+++ BikeWale.UI/Controllers/m/new/NewLaunches/NewLaunchesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers.Mobile.NewLaunches
+{
+    public class NewLaunchesController : Controller
+    {
+        [Route("m/newlaunches/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
