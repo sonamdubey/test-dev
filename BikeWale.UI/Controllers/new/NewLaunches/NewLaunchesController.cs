e9a042ff577308ef97bc4633debb3668d4444726
subodh jain <subodh.jain@carwale.com>
2017-02-15 16:45:52 +0530

    Desktop New Launched page Added Brand widget
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- BikeWale.UI/Controllers/new/NewLaunches/NewLaunchesController.cs
+++ BikeWale.UI/Controllers/new/NewLaunches/NewLaunchesController.cs
@@ -1,6 +1,8 @@
-﻿using Bikewale.Interfaces.BikeData.NewLaunched;
+﻿using Bikewale.Entities.BikeData.NewLaunched;
+using Bikewale.Interfaces.BikeData.NewLaunched;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
-
 namespace Bikewale.Controllers.Desktop.NewLaunches
 {
     public class NewLaunchesController : Controller
@@ -15,6 +17,13 @@ namespace Bikewale.Controllers.Desktop.NewLaunches
         [Route("newlaunches/")]
         public ActionResult Index()
         {
+            int TopCount = 10;
+            IEnumerable<BikesCountByMakeEntityBase> makes = _newLaunches.GetMakeList();
+            if (makes != null && makes.Count() > 0)
+            {
+                ViewBag.TopMakes = makes.Take(TopCount);
+                ViewBag.OtherMakes = makes.Count() > TopCount ? makes.Skip(TopCount).OrderBy(m => m.Make.MakeName) : null;
+            }
             ViewBag.Years = _newLaunches.YearList();
             return View("~/views/newlaunches/index.cshtml");
         }
