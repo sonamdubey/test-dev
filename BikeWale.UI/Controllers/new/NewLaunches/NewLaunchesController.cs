f98e4d932f040a3ecbef6eec18a31c6c1e465877
Sajal Gupta <sajal.gupta@aepl.com>
2017-02-15 14:12:35 +0530

    inserted year wise widgets on new bikelaunches page

--- BikeWale.UI/Controllers/new/NewLaunches/NewLaunchesController.cs
+++ BikeWale.UI/Controllers/new/NewLaunches/NewLaunchesController.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using Bikewale.Interfaces.BikeData.NewLaunched;
+using System.Web.Mvc;
 
 namespace Bikewale.Controllers.Desktop.NewLaunches
 {
     public class NewLaunchesController : Controller
     {
+        private readonly INewBikeLaunchesBL _newLaunches = null;
+
+        public NewLaunchesController(INewBikeLaunchesBL newLaunches)
+        {
+            _newLaunches = newLaunches;
+        }
+
         [Route("newlaunches/")]
         public ActionResult Index()
         {
+            ViewBag.Years = _newLaunches.YearList();
             return View("~/views/newlaunches/index.cshtml");
         }
     }
