50ba07a2012542965e23ec7aa2abf953ecaffb7b
Sajal Gupta <sajal.gupta@aepl.com>
2017-02-16 16:31:33 +0530

    - Skip Year in year wise widget in newly launched bikes by year page. - Changed widget names of make widgets on newly launched pages. - changed brand widget to fetch data from model instead from view bag on newly launched page.

--- BikeWale.UI/Controllers/new/NewLaunches/DNewLaunchesController.cs
+++ BikeWale.UI/Controllers/new/NewLaunches/DNewLaunchesController.cs
@@ -53,7 +53,7 @@ namespace Bikewale.Controllers.Desktop.NewLaunches
             {
                 ViewBag.BrandCountList = (_newLaunches.GetMakeList().Take(9));
             }
-            return PartialView("~/Views/Shared/_NewLaunchedByBrand.cshtml");
+            return PartialView("~/Views/Shared/_NewLaunchedByMakeWithCount.cshtml", ViewBag.BrandCountList);
         }
 
         [Route("newlaunches/year/{launchYear}/")]
@@ -63,10 +63,20 @@ namespace Bikewale.Controllers.Desktop.NewLaunches
             return View("~/views/newlaunches/bikesbyyear.cshtml");
         }
 
+        /// <summary>
+        /// modified by : Sajal Gupta on 16-02-2017
+        /// Description : skip year from list which is present.
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
         [Route("newlaunches/years/")]
-        public ActionResult Years()
+        public ActionResult Years(int? year)
         {
             IEnumerable<BikesCountByYearEntityBase> objYears = _newLaunches.YearList();
+
+            if (year != null && year.HasValue)
+                objYears = objYears.Where(x => x.Year != year.Value);
+
             return PartialView("~/Views/Shared/_NewLaunchedByYear.cshtml", objYears);
         }
     }
