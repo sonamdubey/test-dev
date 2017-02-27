b9a3175b4290939a142d2651117465a0d65e0978
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-02-27 14:43:24 +0530

    Added backend functions for Videos/make page

---
+++ BikeWale.UI/Controllers/m/new/Videos/VideosMobileController.cs
@@ -0,0 +1,36 @@
+﻿using Bikewale.Common;
+using Bikewale.Entities.BikeData;
+using Bikewale.Entities.Videos;
+using Bikewale.Interfaces.Videos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers.Mobile.Videos
+{
+    /// <summary>
+    /// Created by : Aditi Srivastava on 27 Feb 2017
+    /// Summary    : Mobile Controller for make video page
+    /// </summary>
+	public class VideosMobileController :Controller
+	{
+         private readonly IVideosCacheRepository _videos = null;
+
+         public VideosMobileController(IVideosCacheRepository videos)
+        {
+            _videos = videos;
+        }
+        [Route("m/videos/make/{maskingname}/")]
+        public ActionResult Index(string makeMaskingName)
+        {
+            MakeMaskingResponse makeInfo = new MakeMaskingResponse();
+            makeInfo = new MakeHelper().GetMakeByMaskingName(makeMaskingName);
+
+            IEnumerable<BikeVideoModelEntity> objModelVideos = _videos.GetModelVideos(makeInfo.MakeId);
+
+            return View("~/Views/m/Videos/Makes.cshtml", objModelVideos);
+        }
+	}
+}
---
+++ BikeWale.UI/Controllers/new/Videos/VideosController.cs
@@ -0,0 +1,39 @@
+﻿using Bikewale.Common;
+using Bikewale.Entities.BikeData;
+using Bikewale.Entities.Videos;
+using Bikewale.Interfaces.Videos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers.Desktop.Videos
+{
+	public class VideosController : Controller
+	{
+        /// <summary>
+        /// Created by : Aditi Srivastava on 27 Feb 2017
+        /// Summary    : Desktop Controller for make video page
+        /// </summary>
+        public class VideosMobileController : Controller
+        {
+            private readonly IVideosCacheRepository _videos = null;
+
+            public VideosMobileController(IVideosCacheRepository videos)
+            {
+                _videos = videos;
+            }
+            [Route("videos/make/{maskingname}/")]
+            public ActionResult Index(string makeMaskingName)
+            {
+                MakeMaskingResponse makeInfo = new MakeMaskingResponse();
+                makeInfo = new MakeHelper().GetMakeByMaskingName(makeMaskingName);
+
+                IEnumerable<BikeVideoModelEntity> objModelVideos = _videos.GetModelVideos(makeInfo.MakeId);
+
+                return View("~/Views/Videos/Makes.cshtml", objModelVideos);
+            }
+        }
+	}
+}
