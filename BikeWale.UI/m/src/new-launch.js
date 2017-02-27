21a39c1b5a6190435e81b06815d1c7b0da8039ae
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-02-27 10:36:00 +0530

    Fix alignment issue on desktop generic page - Fix alignment issue on desktop generic page - Remove hover effect javascript from mobile - Fix scroll animation on mobile new bike launches

--- BikeWale.UI/m/src/new-launch.js
+++ BikeWale.UI/m/src/new-launch.js
@@ -388,7 +388,7 @@ var newLaunches = function () {
                 slideInDrawer.close(ele);
                 window.location.hash = qs;
                 self.IsLoading(false);
-                $('html, body').animate({ scrollTop: $('#newlaunched-bikes').offset().top })
+                $('html, body').scrollTop($('#newlaunched-bikes').offset().top);
             });
         }
         else {
