2b2c40efcce19c9c0b305de9a354a026484665ef
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-02-28 18:00:01 +0530

    Added widgets to make wise video page - Mobile & Desktop

--- BikeWale.UI/Controllers/m/new/Videos/VideosMobileController.cs
+++ BikeWale.UI/Controllers/m/new/Videos/VideosMobileController.cs
@@ -1,12 +1,10 @@
 ﻿using Bikewale.Common;
 using Bikewale.Entities.BikeData;
 using Bikewale.Entities.Videos;
-using Bikewale.Interfaces.BikeData;
 using Bikewale.Interfaces.Videos;
-using System;
+using Bikewale.Utility;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Bikewale.Controllers.Mobile.Videos
@@ -18,29 +16,27 @@ namespace Bikewale.Controllers.Mobile.Videos
 	public class VideosMobileController :Controller
 	{
          private readonly IVideosCacheRepository _videos = null;
-         private readonly IBikeModelsCacheRepository<int> _modelCache = null;
 
-
-         public VideosMobileController(IVideosCacheRepository videos, IBikeModelsCacheRepository<int> modelCache)
+         public VideosMobileController(IVideosCacheRepository videos)
         {
             _videos = videos;
-            _modelCache = modelCache;
         }
+
         [Route("m/videos/make/{makeMaskingname}/")]
         public ActionResult Index(string makeMaskingName)
         {
             MakeMaskingResponse makeInfo = new MakeMaskingResponse();
             makeInfo = new MakeHelper().GetMakeByMaskingName(makeMaskingName);
-
+            ViewBag.CityId = GlobalCityArea.GetGlobalCityArea().CityId;
+            ViewBag.TopCount = 9;
+            ViewBag.MakeId = makeInfo.MakeId;
+            ViewBag.makeMaskingName = makeMaskingName;
             ViewBag.MakeName = "";
             IEnumerable<BikeVideoModelEntity> objModelVideos = _videos.GetModelVideos(makeInfo.MakeId);
             if (objModelVideos != null)
             {
                 ViewBag.MakeName = objModelVideos.FirstOrDefault().objMake.MakeName;
             }
-            int TotalItems = 9;
-            IEnumerable<MostPopularBikesBase> objPopularBikes = _modelCache.GetMostPopularBikes(TotalItems,(int)makeInfo.MakeId);
-            ViewBag.PopularBikes = objPopularBikes;
             return View("~/Views/m/Videos/Makes.cshtml", objModelVideos);
         }
 	}
--- BikeWale.UI/Controllers/new/Videos/VideosController.cs
+++ BikeWale.UI/Controllers/new/Videos/VideosController.cs
@@ -2,10 +2,9 @@
 using Bikewale.Entities.BikeData;
 using Bikewale.Entities.Videos;
 using Bikewale.Interfaces.Videos;
-using System;
+using Bikewale.Utility;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Bikewale.Controllers.Desktop.Videos
@@ -13,19 +12,27 @@ namespace Bikewale.Controllers.Desktop.Videos
     public class VideosController : Controller
     {
         private readonly IVideosCacheRepository _videos = null;
-
+        
         public VideosController(IVideosCacheRepository videos)
         {
             _videos = videos;
         }
+
         [Route("videos/make/{makeMaskingName}/")]
         public ActionResult Index(string makeMaskingName)
         {
             MakeMaskingResponse makeInfo = new MakeMaskingResponse();
             makeInfo = new MakeHelper().GetMakeByMaskingName(makeMaskingName);
-
+            ViewBag.CityId = GlobalCityArea.GetGlobalCityArea().CityId;
+            ViewBag.TopCount = 9;
+            ViewBag.MakeId = makeInfo.MakeId;
+            ViewBag.makeMaskingName = makeMaskingName;
+            ViewBag.MakeName = "";
             IEnumerable<BikeVideoModelEntity> objModelVideos = _videos.GetModelVideos(makeInfo.MakeId);
-
+            if (objModelVideos != null)
+            {
+                ViewBag.MakeName = objModelVideos.FirstOrDefault().objMake.MakeName;
+            }
             return View("~/Views/Videos/Makes.cshtml", objModelVideos);
         }
     }
