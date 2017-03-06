a8d1463a9fc90690800bf152641901dfe6b217cd
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-03-06 17:24:43 +0530

    Modify color tab focus event - Modify color tab focus event

--- BikeWale.UI/src/model.js
+++ BikeWale.UI/src/model.js
@@ -450,6 +450,7 @@ $(document).ready(function () {
 
     var tabsHashParameter = window.location.hash;
     if (tabsHashParameter) {
+        $('html, body').scrollTop($(tabsHashParameter).offset().top - 130);
         $('.overall-specs-tabs-wrapper a[href^=' + tabsHashParameter + ']').trigger('click');
     }
 
