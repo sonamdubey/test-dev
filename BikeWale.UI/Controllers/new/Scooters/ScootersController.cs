ece4c7dce2a3e7d4c65dd01b03a3d13a65230edc
subodh jain <subodh.jain@carwale.com>
2017-03-09 17:12:15 +0530

    Scooter landing New Launches widget
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- BikeWale.UI/Controllers/new/Scooters/ScootersController.cs
+++ BikeWale.UI/Controllers/new/Scooters/ScootersController.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Bikewale.Entities.BikeData.NewLaunched;
+using Bikewale.Interfaces.BikeData.NewLaunched;
 using System.Web.Mvc;
 
 namespace Bikewale.Controllers.Desktop.Scooters
 {
     public class ScootersController : Controller
     {
-        // GET: Scooters
+        private readonly INewBikeLaunchesBL _newLaunches = null;
+        public ScootersController(INewBikeLaunchesBL newLaunches)
+        {
+            _newLaunches = newLaunches;
+        }
+
         [Route("scooters/")]
         public ActionResult Index()
         {
+            PopulateNewlaunch();
             return View("~/views/scooters/index.cshtml");
         }
-
+        [Route("m/scooters/")]
+        public ActionResult MIndex()
+        {
+            PopulateNewlaunch();
+            return View("~/views/m/scooters/index.cshtml");
+        }
+        /// <summary>
+        /// Created By :- Subodh Jain 09 March 2017
+        /// Summary :- Populate New launchs
+        /// </summary>
+        private void PopulateNewlaunch()
+        {
+            var filters = new InputFilter()
+            {
+                PageSize = 9,
+                BodyStyle = 5
+            };
+            NewLaunchedBikeResult objNewLaunchesBikes = _newLaunches.GetBikes(filters);
+            ViewBag.NewLaunchesList = objNewLaunchesBikes;
+        }
         [Route("scooters/make/")]
         public ActionResult BikesByMake()
         {
