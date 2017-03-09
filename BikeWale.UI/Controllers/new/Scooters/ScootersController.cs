aae124ac89d81578d31b587da116e4ee74409ba7
subodh jain <subodh.jain@carwale.com>
2017-03-09 19:40:26 +0530

    Scooters landing Page Added UpComing Widget
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- BikeWale.UI/Controllers/new/Scooters/ScootersController.cs
+++ BikeWale.UI/Controllers/new/Scooters/ScootersController.cs
@@ -1,5 +1,8 @@
-﻿using Bikewale.Entities.BikeData.NewLaunched;
+﻿using Bikewale.Entities.BikeData;
+using Bikewale.Entities.BikeData.NewLaunched;
 using Bikewale.Interfaces.BikeData.NewLaunched;
+using Bikewale.Interfaces.BikeData.UpComing;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace Bikewale.Controllers.Desktop.Scooters
@@ -7,22 +10,43 @@ namespace Bikewale.Controllers.Desktop.Scooters
     public class ScootersController : Controller
     {
         private readonly INewBikeLaunchesBL _newLaunches = null;
-        public ScootersController(INewBikeLaunchesBL newLaunches)
+        private readonly IUpcoming _upcoming = null;
+        public ScootersController(INewBikeLaunchesBL newLaunches, IUpcoming upcoming)
         {
             _newLaunches = newLaunches;
+            _upcoming = upcoming;
         }
 
         [Route("scooters/")]
         public ActionResult Index()
         {
             PopulateNewlaunch();
+            Upcoming();
             return View("~/views/scooters/index.cshtml");
         }
         [Route("m/scooters/")]
         public ActionResult MIndex()
         {
             PopulateNewlaunch();
+            Upcoming();
             return View("~/views/m/scooters/index.cshtml");
+        }
+        /// <summary>
+        /// Created By :- Subodh Jain 09 March 2017
+        /// Summary :- Populate Upcoming scooters
+        /// </summary>
+        private void Upcoming()
+        {
+            var objFiltersUpcoming = new Bikewale.Entities.BikeData.UpcomingBikesListInputEntity()
+            {
+                EndIndex = 9,
+                StartIndex = 1,
+                BodyStyleId = 5
+            };
+            var sortBy = Bikewale.Entities.BikeData.EnumUpcomingBikesFilter.Default;
+            IEnumerable<UpcomingBikeEntity> objUpcomingBikes = _upcoming.GetModels(objFiltersUpcoming, sortBy);
+            ViewBag.UpcomingBikes = objUpcomingBikes;
+
         }
         /// <summary>
         /// Created By :- Subodh Jain 09 March 2017
