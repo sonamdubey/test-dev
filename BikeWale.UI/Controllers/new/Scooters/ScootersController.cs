a1d789477066038c0ad5284c744d0dc80eb00373
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-03-10 11:31:37 +0530

    Scooters - moved mobile controller code to desktop

--- BikeWale.UI/Controllers/new/Scooters/ScootersController.cs
+++ BikeWale.UI/Controllers/new/Scooters/ScootersController.cs
@@ -47,7 +47,33 @@ namespace Bikewale.Controllers.Desktop.Scooters
             return View("~/views/scooters/bikesbymake.cshtml");
         }
 
-        
+        // GET: Scooters
+        [Route("m/scooters/")]
+        public ActionResult MIndex()
+        {
+            MPopulatePopularScooters();
+            return View("~/views/m/scooters/index.cshtml");
+        }
+        /// <summary>
+        /// Created by : Aditi Srivastava on 9 Mar 2017
+        /// Summary    : get list of popular scooters
+        /// </summary>
+        private void MPopulatePopularScooters()
+        {
+            uint cityId = GlobalCityArea.GetGlobalCityArea().CityId;
+            uint topCount = 9;
+            PopularScootersList objScooters = new PopularScootersList();
+            objScooters.PopularScooters = _models.GetMostPopularScooters(topCount, cityId);
+
+            objScooters.PQSourceId = 87;
+            objScooters.PageCatId = 63;
+            ViewBag.popularScooters = objScooters;
+        }
+        [Route("m/scooters/make/")]
+        public ActionResult MBikesByMake()
+        {
+            return View("~/views/m/scooters/bikesbymake.cshtml");
+        }
 
     }
 }
