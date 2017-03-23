e34624eff1e805b26a11ef676921550150509cf8
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-03-23 14:41:27 +0530

    Saved changes related to dealer pricequote page - Saved changes related to dealer pricequote page
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

---
+++ BikeWale.UI/Controllers/PriceController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Bikewale.Controllers
+{
+    public class PriceController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Route("pricequote/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("pricequote/dealer/")]
+        public ActionResult DealerPriceQuoteIndex()
+        {
+            return View();
+        }
+    }
+}
