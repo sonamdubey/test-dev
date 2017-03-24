fcf0d00f6b8c4c5328e27913f9352c93f5fc69cd
cw-sangram <sangram.nandkhile@carwale.com>
2017-03-24 11:37:14 +0530

    Created Brand widget  and common view -- used bikedata enum

---
+++ BikeWale.UI/Models/BikeMakes/BrandWidgetModel.cs
@@ -0,0 +1,65 @@
+﻿using Bikewale.Entities.BikeData;
+using Bikewale.Interfaces.BikeData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bikewale.Models
+{
+    /// <summary>
+    /// Created by : Sangram Nandkhile on 10 Mar 2017
+    /// Summary: Model to holf scooter's brands- topBrands and remaining brands
+    /// </summary>
+    public class BrandWidgetModel
+    {
+        private IEnumerable<BikeMakeEntityBase> _brands = null;
+        public ushort TopCount { get; private set; }
+        private readonly IBikeMakes<BikeMakeEntity, int> _bikeMakes = null;
+        public BrandWidgetModel(ushort topCount, IBikeMakes<BikeMakeEntity, int> bikeMakes)
+        {
+            _bikeMakes = bikeMakes;
+            TopCount = topCount;
+        }
+        public BrandWidgetVM GetData(EnumBikeType page)
+        {
+
+            BrandWidgetVM objData = new BrandWidgetVM();
+
+            switch (page)
+            {
+                case EnumBikeType.New:
+                    _brands = _bikeMakes.GetMakesByType(page);
+                    foreach (var make in _brands)
+                    {
+                        make.Href = String.Format("/{0}-bikes/", make.MaskingName);
+                        make.Title = String.Format("{0} bikes", make.MakeName);
+                    }
+                    break;
+                case EnumBikeType.Used:
+                    break;
+                case EnumBikeType.Dealer:
+                    foreach (var make in _brands)
+                    {
+                        make.Href = String.Format("/{0}-dealer-showrooms-in-india/", make.MaskingName);
+                        make.Title = String.Format("{0} dealer showrooms in India/", make.MakeName);
+                    }
+                    break;
+                case EnumBikeType.ServiceCenter:
+                    break;
+                case EnumBikeType.Scooters:
+                    _brands = _bikeMakes.GetScooterMakes();
+                    foreach (var make in _brands)
+                    {
+                        make.Href = String.Format("/{0}-{1}/", make.MaskingName, !make.IsScooterOnly ? "scooters" : "bikes");
+                        make.Title = String.Format("{0} scooters", make.MakeName);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            objData.TopBrands = _brands.Take(TopCount);
+            objData.OtherBrands = _brands.Skip(TopCount);
+            return objData;
+        }
+    }
+}
---
+++ BikeWale.UI/Models/HomePageModel.cs
@@ -0,0 +1,45 @@
+﻿using Bikewale.Entities;
+using Bikewale.Entities.BikeData;
+using Bikewale.Interfaces.BikeData;
+
+namespace Bikewale.Models
+{
+    /// <summary>
+    /// Created by: Sangram Nandkhile on 24-Mar-2017
+    /// Summary:  Model for homepage
+    /// </summary>
+    public class HomePageModel
+    {
+        #region Variables for dependency injection
+        private readonly IBikeMakes<BikeMakeEntity, int> _bikeMakes = null;
+        #endregion
+
+        #region Page level variables
+        public ushort TopCount { get; private set; }
+        public string redirectUrl;
+        public StatusCodes status;
+        #endregion
+
+        public HomePageModel(ushort topCount, IBikeMakes<BikeMakeEntity, int> bikeMakes)
+        {
+            TopCount = topCount;
+            _bikeMakes = bikeMakes;
+        }
+
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <returns>
+        /// Created by : Sangram Nandkhile on 24-Mar-2017 
+        /// </returns>
+        public HomePageVM GetData()
+        {
+            HomePageVM objVM = new HomePageVM();
+            objVM.AdTags = new AdTags();
+            objVM.PageMetaTags = new PageMetaTags();
+            objVM.Brands = new BrandWidgetModel(TopCount, _bikeMakes).GetData(Entities.BikeData.EnumBikeType.New);
+            return objVM;
+        }
+    }
+}
