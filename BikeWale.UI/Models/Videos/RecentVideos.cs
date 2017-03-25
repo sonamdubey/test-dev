3edf18278dac7bbf13a96ab2f5daffa69f9fab28
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-03-25 16:58:11 +0530

    Created widgets for video by sub category

--- BikeWale.UI/Models/Videos/RecentVideos.cs
+++ BikeWale.UI/Models/Videos/RecentVideos.cs
@@ -29,32 +29,6 @@ namespace Bikewale.Models.Videos
         /// Created by : Aditi Srivastava on 23 Mar 2017
         /// Summary    : To get list of news articles
         /// </summary>
-        //public RecentNewsVM GetData(uint totalRecords, uint makeId, uint modelId, string makeName, string makeMasking, string modelName, string modelMasking)
-        //{
-        //    RecentNewsVM recentNews = new RecentNewsVM();
-        //    try
-        //    {
-        //        recentNews.ArticlesList = _articles.GetMostRecentArticlesByIdList(Convert.ToString((int)EnumCMSContentType.News), totalRecords, makeId, modelId);
-        //        if (makeId > 0)
-        //        {
-        //            recentNews.MakeName = makeName;
-        //            recentNews.MakeMasking = makeMasking;
-        //        }
-
-        //        if (modelId > 0)
-        //        {
-        //            recentNews.ModelName = modelName;
-        //            recentNews.ModelMasking = modelMasking;
-        //        }
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        ErrorClass objErr = new ErrorClass(ex, string.Format("Bikewale.Models.News.RecentNews.GetData: TotalRecords {0},MakeId {1}, ModelId {2}", totalRecords, makeId, modelId));
-        //    }
-        //    return recentNews;
-        //}
-
          public RecentVideosVM GetData(ushort pageNo, ushort pageSize, uint makeId, string makeName,string makeMasking, uint modelId, string modelName,string modelMasking)
         {
              RecentVideosVM recentVideos=new RecentVideosVM();
--- Bikewale.Models/ExpertReviews/RecentExpertReviewsVM.cs
+++ Bikewale.Models/ExpertReviews/RecentExpertReviewsVM.cs
@@ -1,9 +1,5 @@
 ﻿using Bikewale.Entities.CMS.Articles;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bikewale.Models.ExpertReviews
 {
