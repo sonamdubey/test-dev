5e363b89bd3a2290b7fc2cfae21c116214c7efea
Sajal Gupta <sajal.gupta@aepl.com>
2017-03-25 12:55:35 +0530

    Added bike comparison widget

---
+++ BikeWale.UI/Models/CompareBikes/ComparisonMinWidget.cs
@@ -0,0 +1,45 @@
+﻿using Bikewale.Common;
+using Bikewale.Entities.Compare;
+using Bikewale.Interfaces.Compare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bikewale.Models.CompareBikes
+{
+    public class ComparisonMinWidget
+    {
+        private readonly IBikeCompareCacheRepository _objCompareCache;
+        private uint _topCount;
+        private bool _showComparisonButton;
+
+        public ComparisonMinWidget(IBikeCompareCacheRepository objCompareCache, uint topCount, bool showComparisonButton)
+        {
+            _topCount = topCount;
+            _objCompareCache = objCompareCache;
+            _showComparisonButton = showComparisonButton;
+        }
+
+        public ComparisonMinWidgetVM GetData()
+        {
+            ComparisonMinWidgetVM objComparison = null;
+            try
+            {
+                IEnumerable<TopBikeCompareBase> topBikeCompares = _objCompareCache.CompareList(_topCount);
+
+                if (topBikeCompares != null && topBikeCompares.Count() > 0)
+                {
+                    objComparison = new ComparisonMinWidgetVM();
+                    objComparison.TopComparisonRecord = topBikeCompares.First();
+                    objComparison.RemainingCompareList = topBikeCompares.Skip(1);
+                    objComparison.ShowComparisonButton = _showComparisonButton;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorClass objErr = new ErrorClass(ex, "Bikewale.Models.CompareBikes.ComparisonMinWidget.GetData()");
+            }
+            return objComparison;
+        }
+    }
+}
---
+++ Bikewale.Models/CompareBikes/ComparisonMinWidgetVM.cs
@@ -0,0 +1,12 @@
+﻿
+using Bikewale.Entities.Compare;
+using System.Collections.Generic;
+namespace Bikewale.Models.CompareBikes
+{
+    public class ComparisonMinWidgetVM
+    {
+        public TopBikeCompareBase TopComparisonRecord { get; set; }
+        public IEnumerable<TopBikeCompareBase> RemainingCompareList { get; set; }
+        public bool ShowComparisonButton { get; set; }
+    }
+}
