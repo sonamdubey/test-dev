c0aec917ebf77a42c288d8784b2621d8c9c901ae
Sajal Gupta <sajal.gupta@aepl.com>
2017-03-25 17:20:41 +0530

    added similar bike comparisons for mobile and desktop

---
+++ BikeWale.UI/Models/CompareBikes/PopularModelCompareWidget.cs
@@ -0,0 +1,39 @@
+﻿using Bikewale.Common;
+using Bikewale.Entities.BikeData;
+using Bikewale.Interfaces.Compare;
+using System;
+using System.Collections.Generic;
+
+namespace Bikewale.Models.CompareBikes
+{
+    public class PopularModelCompareWidget
+    {
+        private string _versionList;
+        private uint _topCount, _cityId;
+        private readonly IBikeCompareCacheRepository _objCompare;
+
+        public PopularModelCompareWidget(IBikeCompareCacheRepository objCompare, uint topCount, uint cityId, string versionList)
+        {
+            _versionList = versionList;
+            _topCount = topCount;
+            _cityId = cityId;
+            _objCompare = objCompare;
+        }
+
+        public ICollection<SimilarCompareBikeEntity> GetData()
+        {
+            ICollection<SimilarCompareBikeEntity> objSimilarBikes = null;
+
+            try
+            {
+                objSimilarBikes = _objCompare.GetSimilarCompareBikes(_versionList, (ushort)_topCount, (int)_cityId);
+            }
+            catch (Exception ex)
+            {
+                ErrorClass objErr = new ErrorClass(ex, "Bikewale.Models.CompareBikes.PopularModelCompareWidget.GetData()");
+            }
+
+            return objSimilarBikes;
+        }
+    }
+}
