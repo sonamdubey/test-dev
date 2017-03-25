3721e3d8a9f9ae5fa934512c9557ce9815e58258
Sajal Gupta <sajal.gupta@aepl.com>
2017-03-25 18:04:02 +0530

    Fixed build errors

--- BikeWale.UI/Controllers/NewsController.cs
+++ BikeWale.UI/Controllers/NewsController.cs
@@ -1,12 +1,5 @@
-﻿using Bikewale.Entities.Videos;
-using Bikewale.Interfaces.CMS;
+﻿using Bikewale.Interfaces.CMS;
 using Bikewale.Interfaces.Videos;
-using Bikewale.Models;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Bikewale.Controllers
@@ -45,34 +38,32 @@ namespace Bikewale.Controllers
             return View();
         }
 
-        /// <summary>
-        /// This is a test controller
-        /// </summary>
-        /// <returns></returns>
-        [Route("testnews/")]
-        public ActionResult TestNews()
-        {
-            ICollection<string> videoCatList = new Collection<string>();
-                videoCatList.Add(((int)EnumVideosCategory.BikeReview).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.Motorsports).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.Misc).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.FirstRide).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.LaunchAlert).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.TopMusic).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.Specials).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.FirstLook).ToString());
-                videoCatList.Add(((int)EnumVideosCategory.Blockbuster).ToString());
+        ///// <summary>
+        ///// This is a test controller
+        ///// </summary>
+        ///// <returns></returns>
+        //[Route("testnews/")]
+        //public ActionResult TestNews()
+        //{
+        //    ICollection<string> videoCatList = new Collection<string>();
+        //    videoCatList.Add(((int)EnumVideosCategory.BikeReview).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.Motorsports).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.Misc).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.FirstRide).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.LaunchAlert).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.TopMusic).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.Specials).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.FirstLook).ToString());
+        //    videoCatList.Add(((int)EnumVideosCategory.Blockbuster).ToString());
 
-            uint totalRec = 10, makeid = 1, modelid = 411;
-            string makeName = "Bajaj", makemasking = "bajaj";
-            string modelName = "Pulsar RS200", modelmasking = "pulsar";
-            testPageModel obj = new testPageModel(_articles, _videos);
-            TestVM objData = new TestVM();
-            objData = obj.PopulatePageModel(totalRec, 0, 0, null, null, null, null, 1, 5,videoCatList,VideosSortOrder.FeaturedAndLatest, EnumVideosCategory.FeaturedAndLatest);
-            // objData = obj.PopulatePageModel(totalRec, makeid, modelid, makeName, makemasking, modelName, modelmasking);
-            return View(objData);
+        //    uint totalRec = 10, makeid = 1, modelid = 411;
+        //    string makeName = "Bajaj", makemasking = "bajaj";
+        //    string modelName = "Pulsar RS200", modelmasking = "pulsar";
+        //    objData = obj.PopulatePageModel(totalRec, 0, 0, null, null, null, null, 1, 5, videoCatList, VideosSortOrder.FeaturedAndLatest, EnumVideosCategory.FeaturedAndLatest);
+        //    // objData = obj.PopulatePageModel(totalRec, makeid, modelid, makeName, makemasking, modelName, modelmasking);
+        //    return View(objData);
 
-        }
+        //}
 
 
     }
