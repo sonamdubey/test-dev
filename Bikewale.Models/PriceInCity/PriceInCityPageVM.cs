f3a89b8e7692d63bbd4cf136d67083e3f3cecaf6
cw-sumitkate <sumit.kate@carwale.com>
2017-03-27 14:17:54 +0530

    Change Location Popup - Intergrate new popup with dealer in city page
    Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

---
+++ Bikewale.Models/PriceInCity/PriceInCityPageVM.cs
@@ -0,0 +1,14 @@
+﻿
+using Bikewale.Entities.DealerLocator;
+using Bikewale.Models.ServiceCenters;
+namespace Bikewale.Models
+{
+    public class PriceInCityPageVM
+    {
+        public PriceInCity.PriceInTopCitiesWidgetVM TopCities { get; private set; }
+        public DealersEntity Dealers { get; private set; }
+        public SimilarBikesWidgetVM AlternateBikes { get; private set; }
+        public ServiceCentersNearByCityWidgetVM ServiceCenters { get; private set; }
+        public BikeInfoVM BikeInfo { get; private set; }
+    }
+}
