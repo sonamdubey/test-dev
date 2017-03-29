0ca862bdaeee9ed1295c213c76cc931ec4025ae6
cw-sumitkate <sumit.kate@carwale.com>
2017-03-30 02:10:23 +0530

    New Launches landing Page code restructure - Add View model and model for new launches landing page - Bind the new view model on new launches landing page
    Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

---
+++ Bikewale.Models/NewLaunches/NewLaunchedIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+namespace Bikewale.Models
+{
+    public class NewLaunchedIndexVM : ModelBase
+    {
+        public NewLaunchesBikesVM NewLaunched { get; set; }
+        public UpcomingBikesWidgetVM Upcoming { get; set; }
+        public BrandWidgetVM Brands { get; set; }
+        public uint CityId { get; set; }
+        public bool HasUpcoming { get { return Upcoming != null && Upcoming.UpcomingBikes != null && Upcoming.UpcomingBikes.Count() > 0; } }
+        public bool HasBrands { get { return Brands != null && Brands.TopBrands != null && Brands.TopBrands.Count() > 0; } }
+    }
+}
