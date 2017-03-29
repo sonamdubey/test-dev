0021d10cab2ccc4bc14d8019b4dd4c8a046324fd
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-03-29 15:33:36 +0530

    Add upcoming bikes by make page mobile - Add upcoming bikes by make page mobile

--- BikeWale.UI/Controllers/UpcomingBikesController.cs
+++ BikeWale.UI/Controllers/UpcomingBikesController.cs
@@ -16,5 +16,13 @@ namespace Bikewale.Controllers
             ModelBase m = new ModelBase();
             return View(m);
         }
+
+        // GET: UpcomingBikes by Make
+        [Route("m/upcomingbikes/make/")]
+        public ActionResult UpcomingBikesByMake_Mobile()
+        {
+            ModelBase m = new ModelBase();
+            return View(m);
+        }
     }
 }
