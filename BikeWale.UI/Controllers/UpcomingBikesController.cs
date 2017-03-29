8e718d4467082d3914211962ecbe75eeb72af589
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-03-29 12:59:35 +0530

    Add mobile upcoming bikes landing page - Add mobile upcoming bikes landing page

---
+++ BikeWale.UI/Controllers/UpcomingBikesController.cs
@@ -0,0 +1,20 @@
+﻿using Bikewale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers
+{
+    public class UpcomingBikesController : Controller
+    {
+        // GET: UpcomingBikes
+        [Route("m/upcomingbikes/")]
+        public ActionResult Index_Mobile()
+        {
+            ModelBase m = new ModelBase();
+            return View(m);
+        }
+    }
+}
---
+++ BikeWale.UI/m/src/upcoming-bikes.js
@@ -0,0 +1,82 @@
+﻿var directionRight = { direction: 'right' };
+
+// brand
+$('#brand-filter-selection').on('click', function () {
+    slideInDrawer.open($('#brand-slideIn-drawer'));
+    appendState('brand-drawer');
+});
+
+$('#close-brand-slideIn-drawer').on('click', function () {
+    slideInDrawer.close($('#brand-slideIn-drawer'));
+    history.back();
+});
+
+// year
+$('#year-filter-selection').on('click', function () {
+    slideInDrawer.open($('#year-slideIn-drawer'));
+    appendState('year-drawer');
+});
+
+$('#close-year-slideIn-drawer').on('click', function () {
+    slideInDrawer.close($('#year-slideIn-drawer'));
+    history.back();
+});
+
+var objMakesList = $("#brand-slideIn-drawer ul li").slice(1), makeInputEle = $("#brand-slideIn-drawer input[type='text']");
+
+var slideInDrawer = {
+    open: function (container) {
+
+        if (container) {
+            container.show(effect, directionRight, duration, function () {
+                container.addClass('fix-header-input');
+            });
+            windowScreen.lock();
+        }
+    },
+
+    close: function (container) {
+        if (container) {
+            container.hide(effect, directionRight, duration, function () { });
+            container.removeClass('fix-header-input');
+            windowScreen.unlock();
+        }
+    }
+};
+
+var windowScreen = {
+    htmlElement: $('html'),
+
+    bodyElement: $('body'),
+
+    lock: function () {
+        if ($(document).height() > $(window).height()) {
+            var windowScrollTop = windowScreen.htmlElement.scrollTop() ? windowScreen.htmlElement.scrollTop() : windowScreen.bodyElement.scrollTop();
+            if (windowScrollTop < 0) {
+                windowScrollTop = 0;
+            }
+            windowScreen.htmlElement.addClass('lock-browser-scroll').css('top', -windowScrollTop);
+        }
+    },
+
+    unlock: function () {
+        var windowScrollTop = parseInt(windowScreen.htmlElement.css('top'));
+
+        windowScreen.htmlElement.removeClass('lock-browser-scroll');
+        $('html, body').scrollTop(-windowScrollTop);
+    }
+};
+
+/* popup state */
+var appendState = function (state) {
+    window.history.pushState(state, '', '');
+};
+
+$(window).on('popstate', function (event) {
+    if ($('#brand-slideIn-drawer').is(':visible')) {
+        slideInDrawer.close($('#brand-slideIn-drawer'));
+    }
+    if ($('#year-slideIn-drawer').is(':visible')) {
+        slideInDrawer.close($('#year-slideIn-drawer'));
+    }
+});
