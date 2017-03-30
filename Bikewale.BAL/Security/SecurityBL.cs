dc3bc8a6e07ab70f1ec6a3680e04fa1f27af477f
cw-sumitkate <sumit.kate@carwale.com>
2017-03-30 10:16:45 +0530

    Image Upload API changes - Add AWS region changes - Image upload js changes in bikewale and opr Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

--- Bikewale.BAL/Security/SecurityBL.cs
+++ Bikewale.BAL/Security/SecurityBL.cs
@@ -13,12 +13,15 @@ namespace Bikewale.BAL.Security
     /// </summary>
     public class SecurityBL : ISecurity
     {
+        protected string signature;
+        protected string policyJSONBase64;
         private readonly string awsAccessKeyId = Utility.BWConfiguration.Instance.AWSAccessKey;
         private readonly string bucketName = Utility.BWConfiguration.Instance.AWSBucketName;
         private readonly string secretKey = Utility.BWConfiguration.Instance.AWSSecretKey;
         private const string SEED = "8ik3wa13ghi8jkl7mno6pqr5st4u3v2w1xyz";
         private const int baseCount = 30;
         private readonly int HashLength = Utility.BWConfiguration.Instance.SecurityHashLength;
+        private readonly string s3region = Utility.BWConfiguration.Instance.AWSS3Region;
 
         /// <summary>
         /// Created by  :   Sumit Kate on 09 Nov 2016
@@ -32,19 +35,34 @@ namespace Bikewale.BAL.Security
             {
                 string[] tokens = new string[2];
                 string signature = "", policyJSONBase64 = "", awsURI = "";
+                DateTime now = DateTime.Now;
+
+                string datetimeiso = now.ToString("yyyyMMdd");
+                string datetimeisolong = now.ToString("yyyyMMddTHHmmssZ");
+
                 awsURI = String.Format("https://{0}.s3.amazonaws.com/", bucketName);
-                string jsonPolicy = String.Format("{{'expiration':'{0}','conditions': [{{'bucket':'{1}'}},['starts-with', '$key', ''],['starts-with', '$Content-Type', ''],{{'acl': 'public-read'}},{{'success_action_status': '201'}}]}}", DateTime.Now.AddHours(1).ToString("yyyy-MM-ddTHH:mm:sss.fffZ"), bucketName);
+                string jsonPolicy = String.Format("{{'expiration':'{0}','conditions': [{{'bucket':'{1}'}},['starts-with', '$key', ''],['starts-with', '$Content-Type', ''],{{'acl': 'public-read'}},{{'success_action_status': '201'}},{{'x-amz-algorithm': 'AWS4-HMAC-SHA256'}},{{'x-amz-credential':'{2}/{3}/{4}/s3/aws4_request'}},{{'x-amz-date': '{5}'}}]}}", DateTime.Now.AddHours(1).ToString("yyyy-MM-ddTHH:mm:sss.fffZ"), bucketName, awsAccessKeyId, datetimeiso, s3region, datetimeisolong);
 
                 policyJSONBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(jsonPolicy));
 
-                var encoding = new ASCIIEncoding();
-                byte[] secretKeyBytes = encoding.GetBytes(secretKey);
-                byte[] base64PolicyBytes = encoding.GetBytes(policyJSONBase64);
-                var hmacsha1 = new HMACSHA1(secretKeyBytes);
-                byte[] signatureBytes = hmacsha1.ComputeHash(base64PolicyBytes);
-                signature = Convert.ToBase64String(signatureBytes);
+                string secret = secretKey;
+                byte[] kSecret = Encoding.UTF8.GetBytes(("AWS4" + secret).ToCharArray());
+                byte[] kDate = HmacSHA256(datetimeiso, kSecret);
+                byte[] kRegion = HmacSHA256(s3region, kDate);
+                byte[] kService = HmacSHA256("s3", kRegion);
+                byte[] kSigning = HmacSHA256("aws4_request", kService);
+                byte[] signatureBytes = HmacSHA256(policyJSONBase64, kSigning);
+                signature = BitConverter.ToString(signatureBytes).Replace("-", "").ToLower();
 
-                objAwsToken = new Token() { Policy = policyJSONBase64, Signature = signature, AccessKeyId = awsAccessKeyId, URI = awsURI };
+                objAwsToken = new Token()
+                {
+                    Policy = policyJSONBase64,
+                    Signature = signature,
+                    AccessKeyId = awsAccessKeyId,
+                    URI = awsURI,
+                    DatetTmeISO = datetimeiso,
+                    DateTimeISOLong = datetimeisolong
+                };
             }
             catch (Exception ex)
             {
@@ -52,6 +70,12 @@ namespace Bikewale.BAL.Security
                 objErr.SendMail();
             }
             return objAwsToken;
+        }
+
+        static byte[] HmacSHA256(String data, byte[] key)
+        {
+            var hash = new HMACSHA256(key);
+            return hash.ComputeHash(Encoding.UTF8.GetBytes(data));
         }
 
         /// <summary>
