0d0f0426b066bfc44eee7c27b5e1f780393a0421
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-03-30 14:57:54 +0530

    Add desktop upcoming bikes by make page - Add desktop upcoming bikes by make page

--- BikeWale.UI/Controllers/UpcomingBikesController.cs
+++ BikeWale.UI/Controllers/UpcomingBikesController.cs
@@ -26,6 +26,14 @@ namespace Bikewale.Controllers
         }
 
         // GET: UpcomingBikes by Make
+        [Route("upcomingbikes/make/")]
+        public ActionResult UpcomingBikesByMake()
+        {
+            ModelBase m = new ModelBase();
+            return View(m);
+        }
+
+        // GET: UpcomingBikes by Make
         [Route("m/upcomingbikes/make/")]
         public ActionResult UpcomingBikesByMake_Mobile()
         {
