c6fcd6e02199ca129c36ea3cb1f4319528d54cd2
Sajal Gupta <sajal.gupta@aepl.com>
2017-04-04 19:51:12 +0530

    Called bikewale api from category wise videos page

--- BikeWale.UI/src/Videos/videoByCategory.js
+++ BikeWale.UI/src/Videos/videoByCategory.js
@@ -1,4 +1,4 @@
-﻿var pageNo = 1, isNextPage = true, cwHostUrl, catId, maxPage, apiURL, cacheKey, objVideos;
+﻿var pageNo = 1, isNextPage = true, catId, maxPage, apiURL, cacheKey, objVideos;
 
 
 docReady(function () {
@@ -7,8 +7,7 @@ docReady(function () {
     window.location.hash = "";
 
     var div = $("#js-variable");
-
-    cwHostUrl = div.data('cwhosturl');
+    
     catId = div.data('catid');
     maxPage = div.data('maxpage');
     apiURL = div.data('apiurl');
@@ -114,15 +113,15 @@ docReady(function () {
             $('#loading').hide();
         }
         else {
-            var catURL = cwHostUrl + apiURL + catId + "/?appId=2&pageNo=" + pageNo + "&pageSize=9&sortCategory=3";
+            var catURL = apiURL + catId + "/pn/" + pageNo + "/ps/9/";
             $.ajax({
                 type: 'GET',
                 url: catURL,
                 dataType: 'json',
                 success: function (response) {
-                    if (response && response.Videos.length > 0) {
-                        if (typeof response.Videos == 'undefined') {
-                            objVideos = { 'Videos': response };
+                    if (response && response.videos && response.videos.length > 0) {
+                        if (typeof response.videos == 'undefined') {
+                            objVideos = { 'Videos': response.videos };
                         }
                         else {
                             objVideos = response;
@@ -146,10 +145,10 @@ docReady(function () {
 
     $.bindVideos = function (reponseVideos) {        
         var koHtml = '<div class="miscWrapper container">'
-                             + '<ul id="listVideos' + pageNo + '"  data-bind="template: { name: \'templetVideos\', foreach: Videos }">'
+                             + '<ul id="listVideos' + pageNo + '"  data-bind="template: { name: \'templetVideos\', foreach: videos }">'
                              + '</ul>'
                          + '<div class="clear"></div></div>';
-        $('#listVideos' + (pageNo - 1)).parent().after(koHtml);
+        $('#listVideos' + (pageNo - 1)).parent().after(koHtml);        
         ko.applyBindings(new VideoViewModel(reponseVideos), $("#listVideos" + pageNo)[0]);
     };
 
