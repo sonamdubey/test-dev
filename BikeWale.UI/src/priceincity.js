eda75e5f2fef7a0510b24780a15ac44d909df6fe
dutiyesh <dutiyesh.salunkhe@carwale.com>
2017-04-04 15:55:26 +0530

    Add top card tabs - Add top card tabs

--- BikeWale.UI/src/priceincity.js
+++ BikeWale.UI/src/priceincity.js
@@ -1,9 +1,148 @@
 ﻿var bikeName;
+var selectDropdownBox;
 
 docReady(function () {
 
+    // version dropdown
+    $('.chosen-select').chosen();
+
+    selectDropdownBox = $('.select-box-no-input');
+    selectDropdownBox.each(function () {
+        var text = $(this).find('.chosen-select').attr('data-title'),
+            searchBox = $(this).find('.chosen-search')
+
+        searchBox.empty().append('<p class="no-input-label">' + text + '</p>');
+    });
+
     // add divider between version prices table and prices in nearby cities
     addDivider($('#version-prices-grid'), $('#nearby-prices-grid'));
+
+    var bikeVersionPrice = 93750;
+
+    // emi calculator
+    ko.bindingHandlers.slider = {
+        init: function (element, valueAccessor, allBindingsAccessor, bindingContext) {
+            var options = allBindingsAccessor().sliderOptions || {};
+            $("#" + element.id).slider(options);
+            ko.utils.registerEventHandler("#" + element.id, "slide", function (event, ui) {
+                var observable = valueAccessor();
+                observable(ui.value);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, bindingContext) {
+            var options = allBindingsAccessor().sliderOptions || {};
+            $("#" + element.id).slider(options);
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            if (isNaN(value)) value = 0;
+            $("#" + element.id).slider("value", value);
+        }
+    };
+
+    var BikeEMI = function () {
+        var self = this;
+        self.breakPoints = ko.observable(5);
+        self.bikePrice = ko.observable(bikeVersionPrice);
+        self.minDnPay = ko.observable(10 * bikeVersionPrice/100);
+        self.maxDnPay = ko.observable(40 * bikeVersionPrice/100);
+        self.minTenure = ko.observable(12);
+        self.maxTenure = ko.observable(48);
+        self.minROI = ko.observable(10);
+        self.maxROI = ko.observable(15);
+
+        self.processingFees = ko.observable(0);
+        self.exshowroomprice = ko.observable(bikeVersionPrice);
+        self.loan = ko.observable();
+
+        self.tenure = ko.observable((self.maxTenure() - self.minTenure())/2 + self.minTenure());
+        self.rateofinterest = ko.observable((self.maxROI() - self.minROI())/2 + self.minROI());
+        self.downPayment = ko.pureComputed({
+            read: function () {
+                if (self.loan() == undefined || isNaN(self.loan()) || self.loan() == null)
+                    self.loan($.LoanAmount(self.exshowroomprice(), 70));
+                return (($.LoanAmount(self.exshowroomprice(), 100)) - self.loan());
+            },
+            write: function (value) {
+                self.loan((($.LoanAmount(self.exshowroomprice(), 100))) - value);
+            },
+            owner: this
+        });
+
+        self.monthlyEMI = ko.pureComputed({
+            read: function () {
+                return $.calculateEMI(self.loan(), self.tenure(), self.rateofinterest(),self.processingFees());
+            },
+            owner: this
+        });
+
+        self.totalPayable = ko.pureComputed({
+            read: function () {
+                return (self.downPayment() + (self.monthlyEMI() * self.tenure()));
+            },
+            owner: this
+        });
+    };          
+
+
+    $.calculateEMI = function (loanAmount, tenure, rateOfInterest,proFees) {
+        var interest, totalRepay, finalEmi;
+        try {
+            interest = (loanAmount * tenure * rateOfInterest) / (12 * 100);
+            totalRepay = loanAmount + interest + proFees;
+            finalEmi = Math.ceil((totalRepay / tenure));
+        }
+        catch (e) {
+        }
+        return finalEmi;
+    };
+
+    $.createSliderPoints = function(index,min,max,breaks,sliderType)
+    {   var svar = "";
+        try {
+            switch(sliderType)
+            {
+                case 1: 
+                    svar =  $.valueFormatter(Math.round(min + (index * (max - min)/breaks)));
+                    break;
+                case 2:
+                    svar =  Math.round(min + (index * (max - min)/breaks));
+                    break;
+                default:
+                    svar =  (min + (index * (max - min)/breaks)).toFixed(2);
+                    break;
+            } 
+        } catch (e) {
+    
+        }
+        return svar;
+    };
+
+    $.LoanAmount = function (onRoadPrice, percentage) {
+        var price;
+        try {
+            price = (onRoadPrice * percentage) / 100;
+            price = Math.ceil(price / 100.0) * 100;
+        }
+        catch (e) {
+        }
+        return price;
+    };
+
+    $.valueFormatter = function (num) {
+        if(isNaN(num))
+        {
+            if (num >= 100000) {
+                return (num / 100000).toFixed(1).replace(/\.0$/, '') + 'L';
+            }
+            if (num >= 1000) {
+                return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+            }
+        }
+              
+        return num;
+    };
+
+    var EMIviewModel = new BikeEMI;
+    ko.applyBindings(EMIviewModel, $("#emiContent")[0]);
 
     var $dvPgVar = $("#dvPgVar");
     bikeName = $dvPgVar.data("bikename");
