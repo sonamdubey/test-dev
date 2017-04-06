91ef46681d9bae557b5814d7451b0dcc8067c30b
cw-sumitkate <sumit.kate@carwale.com>
2017-04-06 18:42:56 +0530

    Bikewale Image IPC consumer - This consumer supports image upload from Opr
    Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

---
+++ Bikewale.ImageIPC/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Consumer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+namespace Bikewale.ImageIPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RabbitMqImageConsumer rabbitMqImageConsumer = new RabbitMqImageConsumer();
+            Random random = new Random();
+            string hostName = ((List<string>)CreateConnection.nodes)[random.Next(((List<string>)CreateConnection.nodes).Count)];
+            rabbitMqImageConsumer.RabbitMQExecution("RabbitMq-" + ConfigurationManager.AppSettings["QueueName"].ToUpper() + "-Queue", hostName);
+        }
+    }
+}
