7e19f317a1e5e22a7f35ad8cbd85a4d75a7706fe
cw-sangram <sangram.nandkhile@carwale.com>
2017-04-07 18:37:50 +0530

    Upcoming bikes branch created

---
+++ BikeWale.UI/Models/Upcoming/UpcomingPageModel.cs
@@ -0,0 +1,61 @@
+﻿using Bikewale.Entities.BikeData;
+using Bikewale.Interfaces.BikeData.UpComing;
+using Bikewale.Notifications;
+using System;
+
+namespace Bikewale.Models
+{
+    /// <summary>
+    /// Created by: Sangram Nandkhile on 07-Apr-2017
+    /// summary: Upcoming bikes page model
+    /// </summary>
+    public class UpcomingPageModel
+    {
+        #region Private variables
+
+        private IUpcoming _upcoming = null;
+        private EnumUpcomingBikesFilter filter = EnumUpcomingBikesFilter.Default;
+
+        #endregion
+
+        #region Constructor
+
+        public UpcomingPageModel(IUpcoming upcoming)
+        {
+            _upcoming = upcoming;
+        }
+        #endregion
+
+        #region Public members
+
+        public UpcomingBikesListInputEntity Filters { get; set; }
+        public EnumUpcomingBikesFilter SortBy { get; set; }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <returns>
+        /// Created by : Sangram Nandkhile on 07-Apr-2017 
+        /// </returns>
+        public UpcomingBikesWidgetVM GetData()
+        {
+            UpcomingBikesWidgetVM objUpcoming = new UpcomingBikesWidgetVM();
+            try
+            {
+                objUpcoming.UpcomingBikes = _upcoming.GetModels(Filters, SortBy);
+            }
+            catch (Exception ex)
+            {
+
+                ErrorClass objErr = new ErrorClass(ex, "Bikewale.Models.UpcomingBikesWidget.GetData");
+            }
+            return objUpcoming;
+        }
+
+        #endregion
+    }
+}
---
+++ Bikewale.Models/Upcoming/UpcomingPageVM.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Bikewale.Models
+{
+    public class UpcomingPageVM
+    {
+        public BrandWidgetVM Brands { get; set; }
+    }
+}
