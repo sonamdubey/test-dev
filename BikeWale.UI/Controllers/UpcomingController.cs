232f050e2ae3df9b79e026970cd6ef7ea3be242a
cw-sangram <sangram.nandkhile@carwale.com>
2017-04-10 18:18:28 +0530

    Pusshed merged branch

--- BikeWale.UI/Controllers/UpcomingController.cs
+++ BikeWale.UI/Controllers/UpcomingController.cs
@@ -49,6 +49,14 @@ namespace Bikewale.Controllers
         }
 
         // GET: UpcomingBikes by Make
+        [Route("/upcomingbikes/make/")]
+        public ActionResult UpcomingBikesByMake_Mobile()
+        {
+            ModelBase m = new ModelBase();
+            return View(m);
+        }
+
+        // GET: UpcomingBikes by Make
         [Route("m/upcomingbikes/make/")]
         public ActionResult UpcomingBikesByMake_Mobile()
         {
