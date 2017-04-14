eb218200dbda8cc556ab796abf1f88446cd3d37d
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-04-14 17:35:01 +0530

    Removed unused files - Removed unused files
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- BikeWale.UI/test.aspx.cs
+++ BikeWale.UI/test.aspx.cs
@@ -10,18 +10,9 @@ namespace Bikewale
         protected Bikewale.Controls.DealerCard deskDealer;
         protected void Page_Load(object sender, EventArgs e)
         {
-            ctrlTopCityPrices.ModelId = Convert.ToUInt32(99);
-            ctrlTopCityPrices.CityId = 1;
-            ctrlTopCityPrices.TopCount = 8;
 
-            mobDealer.makeMaskingName = "honda";
-            mobDealer.makeName = "Honda";
-            mobDealer.MakeId = 7;
-            mobDealer.CityId = 1;
-            deskDealer.makeMaskingName = "suzuki";
-            deskDealer.makeName = "Suzuki";
-            deskDealer.MakeId = 12;
-            deskDealer.CityId = 1;
         }
+
+
     }
 }
