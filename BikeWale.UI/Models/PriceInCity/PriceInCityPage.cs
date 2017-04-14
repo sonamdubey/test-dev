a88703947dfe0241c0070f61dbea9aef59a6520f
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-04-14 17:32:18 +0530

    discontinued bikes fixes price in city

--- BikeWale.UI/Models/PriceInCity/PriceInCityPage.cs
+++ BikeWale.UI/Models/PriceInCity/PriceInCityPage.cs
@@ -498,7 +498,7 @@ namespace Bikewale.Models
                     if (dealerCount > 0)
                         newBikeDescription = string.Format("{0} {1} is sold by {2} dealership{3} in {4}.", newBikeDescription, firstVersion.ModelName, dealerCount, multiDealer, firstVersion.City);
 
-                    newBikeDescription = string.Format("{0} All the colour options and versions of {1} might not be available at all the dealerships in {2}. Click on a {1} version name to know on-road price in {2}.", newBikeDescription, firstVersion.ModelName, firstVersion.City);
+                    newBikeDescription = string.Format("{0} All the colour options and versions of {1} might not be available at all the dealerships in {2}.", newBikeDescription, firstVersion.ModelName, firstVersion.City);
 
                     string discontinuedDescription = string.Format("The last known ex-showroom price of {0} {1} in {2} was Rs. {3} onwards. This bike has now been discontinued. It was available in {4} version{5}{6} Click on a {1} version name to know the last known ex-showroom price in {2}.", firstVersion.MakeName, firstVersion.ModelName, firstVersion.City, CommonOpn.FormatPrice(firstVersion.OnRoadPrice.ToString()), versionCount, multiVersion, multiColour);
 
