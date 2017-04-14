46627a3bb3d6c8e5a906bee6875d7f1fcc15a7a3
subodh jain <subodh.jain@carwale.com>
2017-04-14 11:40:24 +0530

    User Review Classes
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

---
+++ Bikewale.Entities/UserReviews/UserReviewOverallRating.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Bikewale.Entities.UserReviews
+{
+
+    public class UserReviewOverallRating
+    {
+        public uint Id { get; set; }
+        public ushort Value { get; set; }
+        public string Description { get; set; }
+        public string Heading { get; set; }
+    }
+
+    public enum UserReviewQuestionType
+    {
+        Rating = 1,
+        Review = 2
+    }
+
+}
