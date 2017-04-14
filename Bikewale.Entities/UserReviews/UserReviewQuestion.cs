dbc8650f017536dcfca3c3f487288c3e9d775754
subodh jain <subodh.jain@carwale.com>
2017-04-14 11:28:21 +0530

    User Review Classes structure
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

---
+++ Bikewale.Entities/UserReviews/UserReviewQuestion.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections.Generic;
+namespace Bikewale.Entities.UserReviews
+{
+    public class UserReviewQuestion
+    {
+        public uint Id { get; set; }
+        public UserReviewQuestionType Type { get; set; }
+        public string Heading { get; set; }
+        public string Description { get; set; }
+        public uint SelectedRatingId { get; set; }
+        public UserReviewQuestionDisplayType DisplayType { get; set; }
+        public IEnumerable<UserReviewrating> Rating { get; set; }
+        public ushort Order { get; set; }
+
+    }
+
+    public enum UserReviewQuestionDisplayType
+    {
+        Star = 1,
+        Text = 2
+    }
+
+    public class UserReviewrating
+    {
+        public uint Id { get; set; }
+        public string Value { get; set; }
+        public string Text { get; set; }
+    }
+
+
+}
