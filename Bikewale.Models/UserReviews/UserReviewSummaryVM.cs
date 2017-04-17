dd833d2b625f78255800d1e89ad07e118860dcd6
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-04-17 21:01:19 +0530

    partial commit

---
+++ Bikewale.Models/UserReviews/UserReviewSummaryVM.cs
@@ -0,0 +1,13 @@
+﻿using Bikewale.Entities.BikeData;
+using Bikewale.Entities.UserReviews;
+
+namespace Bikewale.Models
+{
+    public class UserReviewSummaryVM : ModelBase
+    {
+        public UserReviewSummary Summary { get; set; }
+        public BikeMakeEntityBase Make { get; set; }
+        public BikeModelEntityBase Model { get; set; }
+    }
+
+}
