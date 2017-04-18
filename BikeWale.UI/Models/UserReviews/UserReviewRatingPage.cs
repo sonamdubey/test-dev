7fac2695df75d59801cca9178f20c998c28dbe0a
subodh jain <subodh.jain@carwale.com>
2017-04-18 15:30:15 +0530

    User review Changes
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- BikeWale.UI/Models/UserReviews/UserReviewRatingPage.cs
+++ BikeWale.UI/Models/UserReviews/UserReviewRatingPage.cs
@@ -29,6 +29,12 @@ namespace Bikewale.Models
             _userReviews = userReviews;
             _objModel = objModel;
             _reviewId = reviewId ?? 0;
+            ProcessQuery();
+
+        }
+        private void ProcessQuery()
+        {
+
 
         }
         /// <summary>
@@ -45,15 +51,24 @@ namespace Bikewale.Models
             GetUserRatings(objUserVM);
 
             BindMetas(objUserVM);
+
             return objUserVM;
         }
 
         private void BindMetas(UserReviewRatingVM objUserVM)
         {
 
-            objUserVM.PageMetaTags.Title = "Rate Your Bike| Write a Review - BikeWale";
-            objUserVM.PageMetaTags.Description = string.Format("Rate your {0} {1} on BikeWale. Write a detailed review about {0} {1} and help others in making a right buying decision.", objUserVM.objModelEntity.MakeBase.MakeName, objUserVM.objModelEntity.ModelName);
+            try
+            {
+                objUserVM.PageMetaTags.Title = "Rate Your Bike| Write a Review - BikeWale";
+                objUserVM.PageMetaTags.Description = string.Format("Rate your {0} {1} on BikeWale. Write a detailed review about {0} {1} and help others in making a right buying decision.", objUserVM.objModelEntity.MakeBase.MakeName, objUserVM.objModelEntity.ModelName);
 
+            }
+            catch (System.Exception ex)
+            {
+
+                throw;
+            }
         }
 
         /// <summary>
@@ -64,7 +79,15 @@ namespace Bikewale.Models
         /// <param name="objUserVM"></param>
         private void GetBikeData(UserReviewRatingVM objUserVM)
         {
-            objUserVM.objModelEntity = _objModel.GetById((int)_modelId);
+            try
+            {
+                objUserVM.objModelEntity = _objModel.GetById((int)_modelId);
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
         /// <summary>
         /// Created By : Sushil Kumar on 17th April 2017
@@ -98,10 +121,13 @@ namespace Bikewale.Models
                             {
                                 objQuestions.FirstOrDefault(x => x.Id == 2).SubQuestionId = 3;
                                 objQuestions.FirstOrDefault(x => x.Id == 3).Visibility = false;
+                                objQuestions.FirstOrDefault(x => x.Id == 3).IsRequired = false;
                                 objUserVM.RatingQuestion = Newtonsoft.Json.JsonConvert.SerializeObject(objQuestions);
                             }
                         }
                     }
+
+
                 }
                 else
                 {
@@ -114,6 +140,8 @@ namespace Bikewale.Models
                     {
                         objUserVM.ReviewsOverAllrating = Newtonsoft.Json.JsonConvert.SerializeObject(objUserReviewDataReview.OverallRatingId);
                         objUserVM.RatingQuestion = Newtonsoft.Json.JsonConvert.SerializeObject(objUserReviewDataReview.Questions);
+                        objUserVM.CustomerEmail = objUserReviewDataReview.CustomerEmail;
+                        objUserVM.CustomerName = objUserReviewDataReview.CustomerName;
                     }
 
 
