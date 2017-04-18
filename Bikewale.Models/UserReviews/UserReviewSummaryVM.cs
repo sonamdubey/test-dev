099e4b6137dca38032345041205e663a3c28e0c0
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-04-18 12:03:24 +0530

    Added model and make data for user summary - Added model and make data for user summary
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- Bikewale.Models/UserReviews/UserReviewSummaryVM.cs
+++ Bikewale.Models/UserReviews/UserReviewSummaryVM.cs
@@ -1,13 +1,10 @@
-﻿using Bikewale.Entities.BikeData;
-using Bikewale.Entities.UserReviews;
+﻿using Bikewale.Entities.UserReviews;
 
 namespace Bikewale.Models
 {
     public class UserReviewSummaryVM : ModelBase
     {
         public UserReviewSummary Summary { get; set; }
-        public BikeMakeEntityBase Make { get; set; }
-        public BikeModelEntityBase Model { get; set; }
     }
 
 }
