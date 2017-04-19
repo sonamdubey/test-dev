e3c67bd1969df1d4ec46e06f3057b284c991d9ca
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-04-19 13:42:22 +0530

    Review Id check for user review summary

--- BikeWale.UI/Controllers/UserReviewController.cs
+++ BikeWale.UI/Controllers/UserReviewController.cs
@@ -127,15 +127,26 @@ namespace Bikewale.Controllers
             return View(objData);
         }
 
+        /// <summary>
+        /// Created by : Aditi Srivastava on 19 Apr 2017
+        /// Summary    : To fetch review summary page
+        /// </summary>
         [Route("m/user-reviews/review-summary/{reviewid}/")]
-        public ActionResult ReviewSummary_Mobile(uint reviewid,string q)
+        public ActionResult ReviewSummary_Mobile(uint reviewid, string q)
         {
-            UserReviewSummaryPage objData = new UserReviewSummaryPage(_userReviews, reviewid,q);
-            UserReviewSummaryVM objVM = objData.GetData();
-            if (objData != null && objData.status == Entities.StatusCodes.ContentFound)
-                return View(objVM);
+            if (reviewid > 0)
+            {
+                UserReviewSummaryPage objData = new UserReviewSummaryPage(_userReviews, reviewid, q);
+                UserReviewSummaryVM objVM = objData.GetData();
+                if (objData != null && objData.status == Entities.StatusCodes.ContentFound)
+                    return View(objVM);
+                else
+                    return Redirect("/m/pageNotFound.aspx");
+            }
             else
+            {
                 return Redirect("/m/pageNotFound.aspx");
+            }
         }
 
 
