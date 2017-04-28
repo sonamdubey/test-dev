eef9afedf2ccc59d2aa4478f1c1ac0321455ae91
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-04-28 17:12:42 +0530

    Select bike pop up on user review landing mobile Fixed return urls for user review summary for emails for redirection from summary for both mobile and desktop

--- Bikewale.Utility/UserReviews.cs
+++ Bikewale.Utility/UserReviews.cs
@@ -11,37 +11,28 @@ namespace Bikewale.Utility
         /// <summary>
         /// Summary : Format previous page url 
         /// </summary>
-        public static string FormatPreviousPageUrl(UserReviewPageSourceEnum pageSource, string makeMasking, string modelMasking, bool isDesktop)
+        public static string FormatPreviousPageUrl(UserReviewPageSourceEnum pageSource, string makeMasking, string modelMasking)
         {
             string url = string.Empty;
             switch (pageSource)
             {
                 case UserReviewPageSourceEnum.Mobile_ModelPage:
-                    url = string.Format("/m/{0}-bikes/{1}/", makeMasking, modelMasking);
-                    break;
-                case UserReviewPageSourceEnum.Mobile_UserReview_Listing:
-                    url = string.Format("/m/{0}-bikes/{1}/user-reviews/", makeMasking, modelMasking);
-                    break;
                 case UserReviewPageSourceEnum.Desktop_ModelPage:
                     url = string.Format("/{0}-bikes/{1}/", makeMasking, modelMasking);
                     break;
+                case UserReviewPageSourceEnum.Mobile_UserReview_Listing:
                 case UserReviewPageSourceEnum.Desktop_UserReview_Listing:
                     url = string.Format("/{0}-bikes/{1}/user-reviews/", makeMasking, modelMasking);
                     break;
                 case UserReviewPageSourceEnum.Desktop_UserReview_Landing:
+                case UserReviewPageSourceEnum.Mobile_UserReview_Landing:
                     url = string.Format("/user-reviews/");
                     break;
                 case UserReviewPageSourceEnum.Desktop_UserReview_Details:
                     url = string.Format("/{0}-bikes/{1}/user-reviews/", makeMasking, modelMasking);
                     break;
-                case UserReviewPageSourceEnum.Mobile_UserReview_Landing:
-                    url = "/m/user-reviews/";
-                    break;
                 default:
-                    if (isDesktop)
-                        url = string.Format("/{0}-bikes/{1}/", makeMasking, modelMasking);
-                    else
-                        url = string.Format("/m/{0}-bikes/{1}/", makeMasking, modelMasking);
+                    url = string.Format("/{0}-bikes/{1}/", makeMasking, modelMasking);
                     break;
             }
             return url;
