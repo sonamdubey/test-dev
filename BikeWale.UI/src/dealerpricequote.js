81f32297b96ced1edec2b3c81ef0d99d2c03f974
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-04-28 20:06:35 +0530

    EMI issue fixed - EMI issue fixed
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- BikeWale.UI/src/dealerpricequote.js
+++ BikeWale.UI/src/dealerpricequote.js
@@ -263,7 +263,7 @@ docReady(function () {
         try {
             interest = (loanAmount * tenure * rateOfInterest) / (12 * 100);
             totalRepay = loanAmount + interest + proFees;
-            finalEmi = Math.ceil((totalRepay / tenure));
+            finalEmi = Math.round((totalRepay / tenure));
         }
         catch (e) {
         }
@@ -294,7 +294,7 @@ docReady(function () {
         var price;
         try {
             price = (onRoadPrice * percentage) / 100;
-            price = Math.ceil(price / 100.0) * 100;
+            price = Math.round(price / 100.0) * 100;
         }
         catch (e) {
         }
