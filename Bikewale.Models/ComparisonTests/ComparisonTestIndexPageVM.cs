504d2a1abde4de0ebd972562ccc9be083202d6c7
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-05-08 14:17:20 +0530

    Created comparison test landing page - Mobile & desktop

---
+++ Bikewale.Models/ComparisonTests/ComparisonTestIndexPageVM.cs
@@ -0,0 +1,22 @@
+﻿using Bikewale.Entities.BikeData;
+using Bikewale.Entities.CMS.Articles;
+using Bikewale.Entities.Pager;
+
+namespace Bikewale.Models
+{
+    /// <summary>
+    /// Created by : Aditi Srivastava on 8 May 2017
+    /// Summary :   View model for comparison test landing page
+    /// </summary>
+    public class ComparisonTestsIndexPageVM : ModelBase
+    {
+        public CMSContent Articles { get; set; }
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
+        public PagerEntity PagerEntity { get; set; }
+        public string PageH1 { get; set; }
+        public string PageH2 { get; set; }
+        public MostPopularBikeWidgetVM MostPopularBikes { get; set; }
+        public UpcomingBikesWidgetVM UpcomingBikes { get; set; }   
+    }
+}
