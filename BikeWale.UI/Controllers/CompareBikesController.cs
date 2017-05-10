e04f4d33b1d3422bcec3a040315fa6936a4736bf
subodh jain <subodh.jain@carwale.com>
2017-05-10 18:49:44 +0530

    Compare bike Landing page Desktop Signed-off-by: subodh jain <subodh.jain@carwale.com>

--- BikeWale.UI/Controllers/CompareBikesController.cs
+++ BikeWale.UI/Controllers/CompareBikesController.cs
@@ -1,20 +1,45 @@
-﻿using Bikewale.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Bikewale.CoreDAL;
+using Bikewale.Interfaces.CMS;
+using Bikewale.Interfaces.Compare;
+using Bikewale.Models;
 using System.Web.Mvc;
 
 namespace Bikewale.Controllers
 {
+    /// <summary>
+    /// Created By :- Subodh Jain 10 May 2017
+    /// Summary :- Compare Bike controller
+    /// </summary>
     public class CompareBikesController : Controller
     {
+
+        private readonly IBikeCompareCacheRepository _cachedCompare = null;
+        private readonly ICMSCacheContent _compareTest = null;
+
+        public CompareBikesController(IBikeCompareCacheRepository cachedCompare, ICMSCacheContent compareTest)
+        {
+            _cachedCompare = cachedCompare;
+            _compareTest = compareTest;
+
+        }
+
         // GET: CompareBikes
         [Route("compare/")]
         public ActionResult Index()
         {
-            ModelBase m = new ModelBase();
-            return View(m);
+            CompareIndex objCompare = new CompareIndex(_cachedCompare, _compareTest);
+
+            if (objCompare != null)
+            {
+                CompareVM CompareVM = null;
+
+                CompareVM = objCompare.GetData();
+                return View(CompareVM);
+            }
+            else
+            {
+                return Redirect(CommonOpn.AppPath + "pageNotFound.aspx");
+            }
         }
 
         // GET: CompareBikes Details
--- BikeWale.UI/src/compare/landing.js
+++ BikeWale.UI/src/compare/landing.js
@@ -1,14 +1,16 @@
 ﻿var bike1 = {
-    makeName: 'Honda',
-    modelName: 'CB Shine',
-    versionName: 'Kick/Drum/Spokes',
-    hostUrl: 'https://imgd.aeplcdn.com/',
-    originalImagePath: '/bw/models/honda-shine.jpg',
-    price: '70,147'
+    makeName: '',
+    modelName: '',
+    versionName: '',
+    hostUrl: '',
+    originalImagePath: '',
+    price: ''
 }
-
+var isSameVersion = false;
+var bikeDetails = new Array();
 docReady(function () {
-
+    bwcache.removeAll(true);
+    bwcache.setScope('CLPage');
     var bikeComparisonBox = document.getElementById('bike-comparison-box');
 
     $('.comparison-type-carousel .jcarousel-control-prev').jcarouselControl({
@@ -21,18 +23,38 @@ docReady(function () {
 
     var bikeTemplate = function (data) {
         var self = this;
-
         self.modelData = data;
     };
 
     $('.compare-box-list').on('change', '.select-box select', function () {
         var elementValue = $(this).val();
 
-        if(elementValue > 0) {
+        if (elementValue > 0) {
             var selectBox = $(this).closest('.select-box');
-
             compareBox.dropdownChange(selectBox, elementValue);
         }
+    });
+    $('#btnCompare').on('click', function () {
+        bikeDetails.sort(function (a, b) {
+            return a.modelId - b.modelId;
+        });
+       
+        $.each(bikeDetails, function (i, val) {
+            if(i!=bikeDetails.length-1)
+                result += (val.makemasking + "-" + val.modelmasking + "-vs-");
+            else
+                result += (val.makemasking + "-" + val.modelmasking +"/?");
+
+        });
+        var querystring = "";
+        $.each(bikeDetails, function (i, val) {
+            if (i != bikeDetails.length - 1)
+                querystring += ("bike" + (i + 1) + "=" + val.versionId + "&");
+            else
+                querystring += ("bike" + (i + 1) + "=" + val.versionId);
+
+        });
+        window.location.href = window.location.domain + result + querystring;
     });
 
     var defaultActiveBoxCount = 2;
@@ -44,8 +66,8 @@ docReady(function () {
 
         var validateClick = false;
 
-        if(listItemIndex >= defaultActiveBoxCount) {
-            if(listItemIndex - selectionCount == 0) {
+        if (listItemIndex >= defaultActiveBoxCount) {
+            if (listItemIndex - selectionCount == 0) {
                 validateClick = true;
             }
             else {
@@ -56,7 +78,7 @@ docReady(function () {
             validateClick = true;
         }
 
-        if(validateClick) {
+        if (validateClick) {
             $(listItem).find('.box-placeholder').hide();
             compareBox.setDropdown(listItem[0]);
         }
@@ -65,7 +87,8 @@ docReady(function () {
     $('.compare-box-list').on('click', '.cancel-selected-item', function () {
         var listItem = $(this).closest('.list-item'),
             list = listItem.closest('.compare-box-list')[0];
-
+        if (listItem[0].getAttribute('data-value'))
+        bikeDetails.splice(listItem[0].getAttribute('data-value'), 1);
         listItem.remove();
         compareBox.setPlaceholderBox(list);
     });
@@ -83,7 +106,7 @@ docReady(function () {
         classPrefix: 'select-type-',
         dropdownArr: ['brand', 'model', 'version'],
 
-        placeholderTemplate: '<div class="box-placeholder"><span class="box-label"><span class="add-icon"></span><br />Add bike <span class="label-count"></span></span></div>',
+        placeholderTemplate: '<div class="box-placeholder"><span class="box-label"><span class="add-icon"></span><br />Add bike <span class="label-count"></span></span></div> <span class="error-text"></span>',
 
         setPlaceholderBox: function (list) {
             var li = document.createElement("li");
@@ -102,7 +125,7 @@ docReady(function () {
                 selectionCount = Number(list.getAttribute('data-selection-count')),
                 elementLength = labelElement.length;
 
-            for(var i = selectionCount; i < elementLength; i++) {
+            for (var i = selectionCount; i < elementLength; i++) {
                 labelElement[i].innerHTML = i + 1;
             }
         },
@@ -114,43 +137,50 @@ docReady(function () {
             var divTemplate = "",
                 dropdownLength = compareBox.dropdownArr.length;
 
-            for(var i = 0; i < dropdownLength; i++) {
+            for (var i = 0; i < dropdownLength; i++) {
                 var dropdownLabel = compareBox.dropdownArr[i];
 
                 divTemplate += '<div class="select-box select-box-size-13 select-' + dropdownLabel + '">';
-                divTemplate += '<p class="select-label">'+ dropdownLabel +'</p>';
-                if(i != 0) {
-                    divTemplate += '<select class="chosen-select ' + compareBox.classPrefix + dropdownLabel + '" data-title="Select ' +  dropdownLabel + '" disabled></select>';
+                divTemplate += '<p class="select-label">' + dropdownLabel + '</p>';
+                if (i != 0) {
+                    divTemplate += '<select class="chosen-select ' + compareBox.classPrefix + dropdownLabel + '" data-title="Select ' + dropdownLabel + '" disabled></select>';
                 }
                 else {
-                    divTemplate += '<select class="chosen-select ' + compareBox.classPrefix + dropdownLabel + '" data-title="Select ' +  dropdownLabel + '"></select>';
+                    divTemplate += '<select class="chosen-select ' + compareBox.classPrefix + dropdownLabel + '" data-title="Select ' + dropdownLabel + '"></select>';
                 }
                 divTemplate += '</div>';
             }
 
             div.innerHTML = divTemplate;
             listItem.appendChild(div);
-            
+
             compareBox.getMakes(listItem);
         },
 
         dropdownChange: function (selectBox, elementValue) {
             selectBox.addClass('done');
 
-            if($(selectBox).hasClass('select-brand')) {
+            if ($(selectBox).hasClass('select-brand')) {
                 compareBox.resetDropdownSelection(selectBox);
                 compareBox.getModels(elementValue, selectBox.next()[0]);
             }
-            else if($(selectBox).hasClass('select-model')) {
+            else if ($(selectBox).hasClass('select-model')) {
                 compareBox.resetDropdownSelection(selectBox);
                 compareBox.getVersions(elementValue, selectBox.next()[0]);
             }
-            else if($(selectBox).hasClass('select-version')) {
+            else if ($(selectBox).hasClass('select-version')) {
                 var listItem = selectBox.closest('.list-item'),
                     list = listItem.closest('.compare-box-list')[0];
-
-                setBikeDetails(listItem, bike1);
-                compareBox.setBikeCount(list, true); // true flag, to increment count
+                compareBox.getVersionData(elementValue, listItem);
+               
+                if (!isSameVersion )
+                {
+                    bike1.versionName = $(selectBox).find("option:selected").text();
+                    setBikeDetails(listItem, bike1);
+                    compareBox.setBikeCount(list, true);
+                }
+                
+                // true flag, to increment count
             }
         },
 
@@ -158,30 +188,32 @@ docReady(function () {
             var selectionCount = Number(list.getAttribute('data-selection-count')),
                 listItem = list.getElementsByClassName('list-item');
 
-            if(!incrementFlag) {
+            if (!incrementFlag) {
                 selectionCount--;
             }
             else {
                 selectionCount++;
             }
-            
+
             list.className = "compare-box-list selection-start";
             list.setAttribute('data-selection-count', selectionCount);
 
             // if selection count is 0, remove selection start class and increment it by 1 to enable 0th - 1st list item from array in below for loop
-            if(!selectionCount) {
+            if (!selectionCount) {
                 selectionCount++;
                 list.className = "compare-box-list";
             }
 
             var listItemLength = listItem.length;
 
-            for(var i = 0; i < listItemLength; i++) {
-                if(i <= selectionCount) {
+            for (var i = 0; i < listItemLength; i++) {
+                if (i <= selectionCount) {
                     listItem[i].className = "list-item";
+                    listItem[i].setAttribute('data-value',i);
                 }
                 else {
                     listItem[i].className = "list-item cur-disabled";
+                    listItem[i].setAttribute('data-value', i);
                 }
             }
 
@@ -190,55 +222,173 @@ docReady(function () {
 
         resetDropdownSelection: function (selectBox) {
             var selectBoxSiblings = $(selectBox).nextAll('.select-box');
-            
+
             selectBoxSiblings.removeClass('done');
             selectBoxSiblings.find('.chosen-select').empty().prop('disabled', true).trigger('chosen:updated');
         },
 
         getMakes: function (listItem) {
-            $.ajax({
-                type: "Get",
-                async: false,
-                url: "/api/MakeList/?requesttype=7",
-                contentType: "application/json",
-                dataType: 'json',
-                success: function (response) {
-                    compareBox.setMakesDropdown(response.makes, listItem);
-                }
-            });
-        },
+            var key = "compare_makes";
+            if (!compareBox.checkCache(key)) {
+                $.ajax({
+                    type: "Get",
+                    async: false,
+                    url: "/api/MakeList/?requesttype=2",
+                    contentType: "application/json",
+                    dataType: 'json',
+                    success: function (response) {
+                        bwcache.set(key, response.makes, 30);
+                        compareBox.setMakesDropdown(response.makes, listItem);
 
+                    },
+                    complete: function (xhr) {
+                        if (xhr.status == 404 || xhr.status == 204) {
+                            bwcache.set(key, null, 30);
+                            compareBox.resetDropdownSelection(selectBox);
+                        }
+                    }
+                });
+            }
+            else {
+                data = bwcache.get(key);
+                compareBox.setMakesDropdown(data, listItem);
+            }
+
+
+
+        },
+        checkCache: function (bKey) {
+            if (bwcache.get(bKey)) return true;
+            else return false;
+        },
         getModels: function (makeId, selectBox) {
-            $.ajax({
-                type: "Get",
-                async: false,
-                url: "/api/modellist/?requestType=3&makeId=" + makeId,
-                contentType: "application/json",
-                dataType: 'json',
-                success: function (response) {
-                    compareBox.setModelDropdown(response.modelList, selectBox);
-                }
-            });
+            var key = "compare_make_" + makeId;
+
+            if (!compareBox.checkCache(key)) {
+                $.ajax({
+                    type: "Get",
+                    async: false,
+                    url: "/api/modellist/?requestType=2&makeId=" + makeId,
+                    contentType: "application/json",
+                    dataType: 'json',
+                    success: function (response) {
+                        bwcache.set(key, response.modelList, 30);
+                        compareBox.setModelDropdown(response.modelList, selectBox);
+                    },
+                    complete: function (xhr) {
+                        if (xhr.status == 404 || xhr.status == 204) {
+                            bwcache.set(key, null, 30);
+                            compareBox.resetDropdownSelection(selectBox);
+                        }
+                    }
+                });
+            }
+            else {
+                data = bwcache.get(key);
+                compareBox.setModelDropdown(data, selectBox);
+            }
+           
         },
 
         getVersions: function (modelId, selectBox) {
-            $.ajax({
-                type: "Get",
-                async: false,
-                url: "/api/versionList/?requestType=2&modelId=" + modelId,
-                contentType: "application/json",
-                dataType: 'json',
-                success: function (response) {                    
-                    compareBox.setVersionDropdown(response.Version, selectBox);
+            var key = "compare_versions_" + modelId;
+           
+            if (!compareBox.checkCache(key)) {
+                $.ajax({
+                    type: "Get",
+                    async: false,
+                    url: "/api/versionList/?requestType=2&modelId=" + modelId,
+                    contentType: "application/json",
+                    dataType: 'json',
+                    success: function (response) {
+                        bwcache.set(key, response.Version, 30);
+                        compareBox.setVersionDropdown(response.Version, selectBox);
+                    },
+                    complete: function (xhr) {
+                    if (xhr.status == 404 || xhr.status == 204) {
+                        bwcache.set(key , null, 30);
+                        compareBox.resetDropdownSelection(selectBox);
+                    }
                 }
-            });
+                });
+            }
+            else {
+                data = bwcache.get(key);
+                compareBox.setVersionDropdown(data, selectBox);
+            }
         },
+        getVersionData: function (versionId, selectBox) {
+            var key = "compare_versiondetails_" + versionId;
+             isSameVersion = false;
+            $.each(bikeDetails, function (i, val) {
+                if (val.versionId == versionId)
+                    isSameVersion = true;
+            });
+            if (!isSameVersion) {
+                if (!compareBox.checkCache(key)) {
+                    $.ajax({
+                        type: "Get",
+                        async: false,
+                        url: "/api/v2/Version/?versionId=" + versionId,
+                        contentType: "application/json",
+                        dataType: 'json',
+                        success: function (response) {
+                            bike1.makeName = response.makeDetails.makeName;
+                            bike1.modelName = response.modelDetails.modelName;
+                            bike1.hostUrl = response.hostUrl;
+                            bike1.originalImagePath = response.originalImagePath;
+                            bike1.price = compareBox.FormatPrice(response.price);
+                            var data = {};
+                            data.makemasking = response.makeDetails.maskingName;
+                            data.modelmasking = response.modelDetails.maskingName;
+                            data.modelId = response.modelDetails.modelId;
+                            data.versionId = versionId;
+                            bikeDetails.push(data);
+                            selectBox.first().find('.error-text').hide();
+                            bwcache.set(key, response, 30);
+                        },
+                        complete: function (xhr) {
+                            if (xhr.status == 404 || xhr.status == 204) {
+                                bwcache.set(key, null, 30);
+                            }
+                        }
+                    });
+                }
+                else {
+                    response = bwcache.get(key);
+                    bike1.makeName = response.makeDetails.makeName;
+                    bike1.modelName = response.modelDetails.modelName;
+                    bike1.hostUrl = response.hostUrl;
+                    bike1.originalImagePath = response.originalImagePath;
+                    bike1.price = compareBox.FormatPrice(response.price);
+                    var data = {};
+                    data.makemasking = response.makeDetails.maskingName;
+                    data.modelmasking = response.modelDetails.maskingName;
+                    data.modelId = response.modelDetails.modelId;
+                    data.versionId = versionId;
+                    bikeDetails.push(data);
 
+                }
+            }
+            else {
+                selectBox.first().find('.error-text').text("Please choose different bikes for comparison.");
+            }
+
+        },
+        FormatPrice: function (price) {
+            price = price.toString();
+            var lastThree = price.substring(price.length - 3);
+            var otherNumbers = price.substring(0, price.length - 3);
+            if (otherNumbers != '')
+                lastThree = ',' + lastThree;
+            var price = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+            return price;
+        },
         setMakesDropdown: function (response, listItem) {
             var dropdownOptions = "<option value></option>",
                 responseLength = response.length;
 
-            for(var i = 0; i < responseLength; i++) {
+            for (var i = 0; i < responseLength; i++) {
                 dropdownOptions += '<option value="' + response[i].makeId + '">' + response[i].makeName + '</option>';
             }
 
@@ -252,7 +402,7 @@ docReady(function () {
             var dropdownOptions = "<option value></option>",
                 resoponseLength = response.length;
 
-            for(var i = 0; i < resoponseLength; i++) {
+            for (var i = 0; i < resoponseLength; i++) {
                 dropdownOptions += '<option value="' + response[i].modelId + '">' + response[i].modelName + '</option>';
             }
 
@@ -264,7 +414,7 @@ docReady(function () {
             var dropdownOptions = "<option value></option>",
                 resoponseLength = response.length;
 
-            for(var i = 0; i < resoponseLength; i++) {
+            for (var i = 0; i < resoponseLength; i++) {
                 dropdownOptions += '<option value="' + response[i].versionId + '">' + response[i].versionName + '</option>';
             }
 
@@ -283,12 +433,14 @@ docReady(function () {
         setSubmitButton: function (list) {
             var selectionCount = Number($(list).attr('data-selection-count'));
 
-            if(selectionCount > 1) {
-                $(bikeComparisonBox).find('.compare-now-btn').show();
-            }
-            else {
-                $(bikeComparisonBox).find('.compare-now-btn').hide();
-            }
+          
+                if (selectionCount > 1) {
+                    $(bikeComparisonBox).find('.compare-now-btn').show();
+                }
+                else {
+                    $(bikeComparisonBox).find('.compare-now-btn').hide();
+                }
+            
         }
     }
 });
