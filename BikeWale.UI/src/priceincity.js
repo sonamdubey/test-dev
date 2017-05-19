e1ed600f3fd2eeabaa9ee4630cdee8cb4ca6486d
cw-sumitkate <sumit.kate@carwale.com>
2017-05-19 10:32:28 +0530

    Add dealer dropdown related code for price in city js
    Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

--- BikeWale.UI/src/priceincity.js
+++ BikeWale.UI/src/priceincity.js
@@ -340,7 +340,38 @@ docReady(function () {
         }
 
     });
-
+    function generateDealerDropdown() {
+        $.ajax({
+            type: "GET",
+            url: "/api/ManufacturerCampaign/?city=" + cityId,
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (response) {
+                var obj = ko.toJS(response);
+                var count = obj.length;
+                if (count >= 1) {
+                    if (count == 1) {
+                        $("#ddlMfgDealers").append("<option value='0' data-id='" + obj[0].id + "' >" + obj[0].dealerName + "</option>");
+                        $("#ddlMfgDealers").val('0');
+                        $("#ddlMfgDealers").closest('.select-box').addClass('done');
+                        dleadvm.dealersRequired(false);
+                    } else {
+                        $("#ddlMfgDealers").html('');
+                        $("#ddlMfgDealers").append("<option value></option>");
+                        for (i = 0; i < count; i++) {
+                            var dt = obj[i];
+                            var areaName = '';
+                            if (dt.area != null) {
+                                areaName = ", " + dt.area;
+                            }
+                            $("#ddlMfgDealers").append("<option value=" + (i + 1) + " data-id='" + dt.id + "' >" + dt.dealerName + areaName + "</option>");
+                        }
+                    }
+                }
+                $("#ddlMfgDealers").trigger("chosen:updated");
+            },
+        });
+    };
 });
 
 // add divider
