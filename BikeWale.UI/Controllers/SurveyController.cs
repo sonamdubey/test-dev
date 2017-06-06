bc834678ea64163c08f977c36d57e329d45b4706
cw-sangram <sangram.nandkhile@carwale.com>
2017-06-06 20:12:10 +0530

    Controller for survey -- model and viewmodel

---
+++ BikeWale.UI/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using Bikewale.Models.Survey;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Survey
+        [Route("survey/bajaj/")]
+        public ActionResult BajajSurvey_Index()
+        {
+            BajajSurveyModel model = new BajajSurveyModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("survey/bajaj/SubmitReview/")]
+        public ActionResult SubmitBajajReview(BajajSurveyModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ BikeWale.UI/Models/Survey/BajajSurveyModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Models.Survey
+{
+    public class BajajSurveyModel : ModelBase
+    {
+        public string ModelName { get; set; }
+        public List<string> MultipleModel { get; set; }
+        public List<string> InternetOptions { get; set; }
+        public string OtherBike { get; set; }
+        public string IsLongerFormat { get; set; }
+
+        public string Age { get; set; }
+        public string Handset { get; set; }
+    }
+
+    public class SubQuestion
+    {
+        public short Id { get; set; }
+        public int Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Question : SubQuestion
+    { 
+
+        public IEnumerable<SubQuestion> SubQuestions { get; set; }
+    }
+}
