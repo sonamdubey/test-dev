835837df27ad2ea2d68096aba0a83080b7f4faad
cw-sangram <sangram.nandkhile@carwale.com>
2017-06-08 12:36:14 +0530

    BAL and DAL for Bajaj survey -- created SP and BAL functions

---
+++ BikeWale.UI/Models/Survey/SurveyBajajModel.cs
@@ -0,0 +1,25 @@
+﻿using Bikewale.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bikewale.Models.Survey
+{
+    public class SurveyBajajModel
+    {
+        private readonly ISurvey _survey;
+        private readonly BajajSurveyVM _response;
+
+        public SurveyBajajModel(BajajSurveyVM Response, ISurvey Survey)
+        {
+            _survey = Survey;
+            _response = Response;
+        }
+
+        public void GetData()
+        {
+            _survey.InsertBajajSurveyResponse(_response);
+        }
+    }
+}
