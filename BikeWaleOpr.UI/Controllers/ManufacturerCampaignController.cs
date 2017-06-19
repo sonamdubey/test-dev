fdfa57312a57eff1a61dabdab6ba3b0a8f1e439b
subodh jain <subodh.jain@carwale.com>
2017-06-19 18:19:45 +0530

    Manufacturer campaign changes
    Signed-off-by: subodh jain <subodh.jain@carwale.com>

---
+++ BikeWaleOpr.UI/Controllers/ManufacturerCampaignController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikewaleOpr.Controllers
+{
+    public class ManufacturerCampaignController : Controller
+    {
+        // GET: ManufacturerCampaign
+        public ActionResult SearchManufacturerCampaign()
+        {
+
+
+            return View();
+        }
+    }
+}
