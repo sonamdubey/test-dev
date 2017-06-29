d7a64071a28461393bbd56596669ed57e63e68b9
Aditi Srivastava <aditi.srivastava@aepl.com>
2017-06-29 15:21:27 +0530

    Functional changes in choosing location in manufacturer campaign rules

--- BikeWaleOpr.UI/src/ManufactureCampaign/rules.js
+++ BikeWaleOpr.UI/src/ManufactureCampaign/rules.js
@@ -1,18 +1,28 @@
-﻿if (msg != "") { Materialize.toast(msg, 5000); }
+﻿if (msg != "") { Materialize.toast(msg, 4000); }
 var ddlModels = $('#ddlModels');
 var ddlCities = $('#ddlCities');
 var cityIds = "", stateIds = "", modelIds = "";
-var getCitiesFromDropDown, setRulesData, validateInput;
+var getCitiesFromDropDown, setRulesData, validateInput, showAllIndiaAlert;
 var manufacturerRulesViewModel = function () {
     var self = this;
     self.onExShowroom = onExShowroom;
+    self.AllIndiaValue = ko.observable();
     self.isAllIndia = ko.observable(false);
     self.selectedMake = ko.observable();
     self.listModels = ko.observableArray([]);
     self.selectedState = ko.observable();
     self.listCities = ko.observableArray([]);
     self.listStates = ko.observableArray([]);
-    self.isMultipleStates = ko.observable(false);
+    self.isAllIndia.ForSubmit = ko.computed({
+        read: function () {
+            return this.isAllIndia().toString();
+        },
+        write: function (newValue) {
+            this.isAllIndia(newValue === "1");
+        },
+        owner: this
+    });
+
     self.selectMake = function () {
         self.selectedMake($('#ddlMakes option:selected').val());
         if (self.selectedMake() != undefined && self.selectedMake() > 0) {
@@ -31,51 +41,51 @@ var manufacturerRulesViewModel = function () {
                 }
             });
         }
+        else {
+            self.listModels([]);
+            ddlModels.material_select();
+        }
     };
-    self.selectState = function () {
+
+    self.selectStates = function () {
         self.listStates([]);
         var array = new Array();
-        $('#ddlStates :selected').each(function () {
+        $('#ddlStatesOnly :selected').each(function () {
             array.push($(this).val());
         });
         self.listStates(array)
-        if (self.listStates().length > 1) {
-            self.isMultipleStates(true);
+        if (self.listStates().length > 0) {
             stateIds = "";
             ko.utils.arrayForEach(self.listStates(), function (state) {
                 if (state)
                     stateIds = stateIds + "," + state;
             });
+        };
+    };
 
+    self.selectState = function () {
+        self.selectedState($('#ddlStates option:selected').val());
+        if (self.selectedState() != undefined && self.selectedState() > 0) {
+            $.ajax({
+                type: "GET",
+                url: bwHostUrl + "/api/campaigns/manufacturer/cities/stateId/" + self.selectedState() + "/",
+                datatype: "json",
+                success: function (response) {
+                    var cities = ko.toJS(response);
+                    if (cities) {
+                        self.listCities(cities);
+                    }
+                },
+                complete: function (xhr) {
+                    ddlCities.material_select();
+                }
+            });
         }
         else {
-            self.selectedState($('#ddlStates option:selected').val());
-            if (self.selectedState() != undefined && self.selectedState() > 0) {
-                stateIds = self.selectedState();
-                $.ajax({
-                    type: "GET",
-                    url: bwHostUrl + "/api/campaigns/manufacturer/cities/stateId/" + self.selectedState() + "/",
-                    datatype: "json",
-                    success: function (response) {
-                        var cities = ko.toJS(response);
-                        if (cities) {
-                            self.listCities(cities);
-                        }
-                    },
-                    complete: function (xhr) {
-                        ddlCities.material_select();
-                    }
-                });
-            }
+            self.listCities([]);
+            ddlCities.material_select();
         }
     };
-    
-    self.showCities = ko.computed(function () {
-        return (self.listStates() != null && self.listStates().length === 1);
-    });
-
-    if (self.onExShowroom)
-        self.isAllIndia(true);
 
 };
 
@@ -83,11 +93,33 @@ $(document).ready(function () {
 
     mfgVM = new manufacturerRulesViewModel;
     ko.applyBindings(mfgVM, $('#configRules')[0]);
+
+    if (mfgVM.onExShowroom) {
+        mfgVM.isAllIndia(true);
+        $('#rdoLocation1').prop("checked", true);
+    }
+
+    $("[name=location]").change(function () {
+        var rdo = $("[name=location]:checked").val();
+        if (mfgVM.onExShowroom && rdo != "1") {
+            $('#rdoLocation1').prop("checked", true);
+            showAllIndiaAlert();
+        }
+    });
+
     $('.chip .close').on('click', function () {
         var clicked = $(this);
         var el = clicked.attr('data-element');
-        if (el == "state")
+        if (el == "state") {
             clicked.parents('div.state').hide();
+            var childs = $("[class^=state]");
+            childs.click(function () {
+                $(this).hide();
+                if (!childs.filter(':visible').length) {
+                    clicked.parents('li').hide();
+                }
+            });
+        }
         if (el == "model")
             clicked.parents('li').hide();
         var modelId = clicked.attr('data-modelid');
@@ -126,15 +158,18 @@ $(document).ready(function () {
         $(this).material_select();
     });
 
+
+    showAllIndiaAlert = function () {
+        alert("This campaign is configured on ex-Showroom prices. You can run it for all India only.");
+    };
+
     getCitiesFromDropDown = function () {
-        if (!mfgVM.isMultipleStates()) {
-            $('#ddlCities :selected').each(function () {
-                if ($(this).val())
-                    cityIds = cityIds + $(this).val() + ',';
-            });
-        }
-        else
-            cityIds = null;
+        $('#ddlCities :selected').each(function () {
+            if ($(this).val())
+                cityIds = cityIds + $(this).val() + ',';
+        });
+        if (cityIds == "")
+            stateIds = mfgVM.selectedState();
     };
 
     validateInput = function () {
@@ -143,27 +178,33 @@ $(document).ready(function () {
             isValid = false;
             Materialize.toast("Please select one or more models", 6000);
         }
-        if (!mfgVM.isAllIndia()) {
-            var rdoLoc = $("[name=location]:checked").val();
-            switch (rdoLoc) {
-                case "1":
-                    cityIds = $("#txtCities").val();
-                    break;
-                case "2":
-                    getCitiesFromDropDown();
-                    break;
-            }
-            if (cityIds == "" && stateIds == "") {
-                isValid = false;
-                Materialize.toast("Please select location", 6000);
-            }
+        var rdoLoc = $("[name=location]:checked").val();
+        switch (rdoLoc) {
+            case "1":
+                cityIds = "0";
+                stateIds = null;
+                break;
+            case "2":
+                cityIds = null;
+                break;
+            case "3":
+                stateIds = null;
+                getCitiesFromDropDown();
+                break;
+            case "4":
+                cityIds = $("#txtCities").val();
+                var pat = /^[\d\s,]*$/;
+                if (!pat.test(cityIds)) {
+                    validate.inputField.showError($("#txtCities"));
+                    isValid = false;
+                }
+                break;
         }
-        else {
-            cityIds = "0";
-            stateIds = null;
+        if (cityIds == "" && stateIds == "") {
+            isValid = false;
+            Materialize.toast("Please select location", 6000);
         }
         return isValid;
-
     };
 
     $("#btnAddRules").click(function () {
@@ -184,7 +225,7 @@ $(document).ready(function () {
         $('#hdnIsAllIndia').val(mfgVM.isAllIndia());
     };
 
-    $('.m9 .collapsible .collapsible-header').on('click', function (event) {
+    $('.m10 .collapsible .collapsible-header').on('click', function (event) {
         var target = $(this);
         setTimeout(function () {
             if (target.length) {
