09aaadfbb3ab8e2551d769bf1a797f21b6518e27
sajal.gupta <sajal.gupta@USER-CW-197>
2017-07-17 12:39:51 +0530

    Fixed code review changes

--- Bikewale.Entities/UserReviews/QuestionsRatingValueByModel.cs
+++ Bikewale.Entities/UserReviews/QuestionsRatingValueByModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace Bikewale.Entities.UserReviews
     /// created by sajal gupta on 14-07-2017
     /// description : Entity to hold question rating value of particular model.
     /// </summary>
+    [Serializable, DataContract]
     public class QuestionsRatingValueByModel
     {
         public uint ModelId { get; set; }
