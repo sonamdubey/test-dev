bb025cd63335a357457306bed307010f85952da6
sajal.gupta <sajal.gupta@USER-CW-197>
2017-07-18 16:41:43 +0530

    Added html

---
+++ BikeWaleOpr.UI/src/Banner.js
@@ -0,0 +1,16 @@
+﻿var $dateInput = $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month   
+    closeOnSelect: true,
+    onClose: function () { if (dateValue != $("#reviewDateEle").val()) $("#reviewDate").val($("#reviewDateEle").val()); },
+    onOpen: function () { dateValue = $("#reviewDateEle").val() },
+    onSet: function (ele) { if (ele.select) { this.close(); } }
+});
+
+$(document).ready(function () {
+
+    if ($(".stepper"))
+    {
+        $('.stepper').activateStepper();
+    }
+
+});
