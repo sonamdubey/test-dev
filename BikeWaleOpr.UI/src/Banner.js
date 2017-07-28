94e885239a9f9f077a0d0b96a29e8b8f82ceda5d
sajal.gupta <sajal.gupta@USER-CW-197>
2017-07-28 14:33:11 +0530

    Fixed banner opr section js

--- BikeWaleOpr.UI/src/Banner.js
+++ BikeWaleOpr.UI/src/Banner.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
                     "startdate": $('#startDateEle').val() + ' ' + $('#startTimeEle').val(),
                     "enddate": $('#endDateEle').val() + ' ' + $('#endTimeEle').val(),
                     "bannerdescription": $('#textareaBannerDesc').val(),
-                    "campaignid": queries.id
+                    "campaignid": queries.id,
                     "userId": userId
                 }
                 $.ajax({
