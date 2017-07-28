0d90d35991f857baf2d247b6926c99272df3b07d
ashu82492 <ashutosh.sharma@carwale.com>
2017-07-28 12:11:21 +0530

    changed comments
    Signed-off-by: ashu82492 <ashutosh.sharma@carwale.com>

--- BikewaleOpr.Interface/BikeData/IBikeModelsRepository.cs
+++ BikewaleOpr.Interface/BikeData/IBikeModelsRepository.cs
@@ -13,9 +13,9 @@ namespace BikewaleOpr.Interface.BikeData
     ///  Modified by : Sajal Gupta on 03-03-2017
     /// Description : Added FetchPhotoId, DeleteUsedBikeModelImage, GetUsedBikeModelImageByMake, GetPendingUsedBikesWithoutModelImage
     /// Modified by : Aditi Srivastava on 23 May 2017
+    /// Summary     : added function to get model ingo by makeId
     /// Modified By: Ashutosh Sharma on 27-07-2017
     /// Discription: Added UpdateAsSoldInquiry
-    /// Summary     : added function to get model ingo by makeId
     /// </summary>
     public interface IBikeModelsRepository
     {
