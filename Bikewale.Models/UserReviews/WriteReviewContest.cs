789ee9f7466e5ad7217d94d7dd64bca3c06eb2f7
Vivek Singh Tomar <vivek.tomar@aepl.com>
2017-08-12 15:03:45 +0530

    show winners on review page and cache clear when new winner added

--- Bikewale.Models/UserReviews/WriteReviewContest.cs
+++ Bikewale.Models/UserReviews/WriteReviewContest.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Bikewale.Entities.BikeData;
+using Bikewale.Entities.UserReviews;
 
 namespace Bikewale.Models
 {
     /// <summary>
     /// Summary: View model for write Review
     /// Created by: Sangram Nandkhile on 05 June 2017
-    /// Modified by:
+    /// Modified by: Vivek Singh Tomar On 12th Aug 2017
+    /// Summary: Added property to hold list of user reviews contest winners
     /// </summary>
     public class WriteReviewContestVM : ModelBase
     {
         public IEnumerable<BikeMakeEntityBase> Makes { get; set; }
         public string QueryString { get; set; }
+        public IEnumerable<RecentReviewsWidget> UserReviewsWinners { get; set; }
     }
 }
