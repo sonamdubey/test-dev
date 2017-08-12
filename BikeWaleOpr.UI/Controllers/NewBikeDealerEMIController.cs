61558fe22372b2cd668f820164c0947e1956f6a0
ashu82492 <ashutosh.sharma@carwale.com>
2017-08-12 14:59:58 +0530

    testing

--- BikeWaleOpr.UI/Controllers/NewBikeDealerEMIController.cs
+++ BikeWaleOpr.UI/Controllers/NewBikeDealerEMIController.cs
@@ -11,8 +11,8 @@ using System.Web.Mvc;
 namespace BikewaleOpr.Controllers
 {
     /// <summary>
-    /// Created by : Ashutosh Sharma on 05/08/2017
-    /// Discription : Controller for Manage 
+    /// Created by : Ashutosh Sharma on 05-Aug-2017
+    /// Description : Controller for Manage EMI page.
     /// </summary>
     public class NewBikeDealerEMIController : Controller
     {
@@ -21,27 +21,40 @@ namespace BikewaleOpr.Controllers
         {
             _dealer = dealer;
         }
-        
-        [Route("newbikebooking/ManageDealerLoanAmounts/")]
-        public ActionResult Index(uint? dealerId)
+
+        /// <summary>
+        /// Created by : Ashutosh Sharma on 05-Aug-2017
+        /// Description : Action method for default view of Manage EMI page.
+        /// </summary>
+        /// <param name="dealerId"></param>
+        /// <returns></returns>
+        [Route("newbikebooking/ManageDealerLoanAmounts/{dealerId}")]
+        public ActionResult Index(uint dealerId)
         {
             EMI loanAmount = null;
             try
             {
                 loanAmount = new EMI();
-                loanAmount = _dealer.GetDealerLoanAmounts(dealerId??0);
+                loanAmount = _dealer.GetDealerLoanAmounts(dealerId);
             }
             catch (Exception ex)
             {
-                ErrorClass objErr = new ErrorClass(ex, string.Format("BikewaleOpr.Controllers.NewBikeDealerEMIController_{0}",dealerId));
+                ErrorClass objErr = new ErrorClass(ex, string.Format("NewBikeDealerEMIController_{0}",dealerId));
             }
             return View(loanAmount);
         }
 
+        /// <summary>
+        /// Created by : Ashutosh Sharma on 05/08/2017
+        /// Description : Action method to submit Manage EMI form.
+        /// </summary>
+        /// <param name="dealerId"></param>
+        /// <param name="emi"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult Submit(uint? dealerId, EMI emi)
+        public ActionResult Submit(uint dealerId, EMI emi)
         {
-            _dealer.SaveDealerEMI(dealerId??0, emi.MinDownPayment, emi.MaxDownPayment, emi.MinTenure, emi.MaxTenure
+            _dealer.SaveDealerEMI(dealerId, emi.MinDownPayment, emi.MaxDownPayment, emi.MinTenure, emi.MaxTenure
                             , emi.MinRateOfInterest, emi.MaxRateOfInterest, emi.MinLoanToValue, emi.MaxLoanToValue
                             , emi.LoanProvider, emi.ProcessingFee, emi.Id, Convert.ToUInt32(CurrentUser.Id));
             return RedirectToAction("Index", new { dealerId = dealerId });
---
+++ BikeWaleOpr.UI/src/content/newbikedealerEMI.js
@@ -0,0 +1,66 @@
+﻿function NewBikeDealerEMIViewModel() {
+
+    var self = this;
+
+    self.hdnLoanAmountId = ko.observable($("#hdnLoanAmountId").data('value'));
+    self.txtMinPayment = ko.observable($("#txtMinPayment").data('value'));
+    self.txtMaxPayment = ko.observable($("#txtMaxPayment").data('value'));
+    self.txtMinTenure = ko.observable($("#txtMinTenure").data('value'));
+    self.txtMaxTenure = ko.observable($("#txtMaxTenure").data('value'));
+    self.txtMinROI = ko.observable($("#txtMinROI").data('value'));
+    self.txtMaxROI = ko.observable($("#txtMaxROI").data('value'));
+    self.txtMinLtv = ko.observable($("#txtMinLtv").data('value'));
+    self.txtMaxLtv = ko.observable($("#txtMaxLtv").data('value'));
+    self.textLoanProvider = ko.observable($("#textLoanProvider").data('value'));
+    self.txtFees = ko.observable($("#txtFees").data('value'));
+
+    self.Reset = function () {
+        $("#txtMaxPayment").val("");
+        self.txtMinPayment = ko.observable("");
+        self.txtMaxPayment = ko.observable(null);
+        self.txtMinTenure = ko.observable(null);
+        self.txtMaxTenure = ko.observable(null);
+        self.txtMinROI = ko.observable(null);
+        self.txtMaxROI = ko.observable(null);
+        self.txtMinLtv = ko.observable(null);
+        self.txtMaxLtv = ko.observable(null);
+        self.textLoanProvider = ko.observable(null);
+        self.txtFees = ko.observable(null);
+    };
+
+    self.SaveEMI_Validate = function () {
+        if (self.txtMinPayment()) {
+            console.log("12asdfafd");
+        }
+    };
+
+    self.Delete = function () {
+
+        $.ajax({
+            type: "GET",
+            url: "/api/Dealers/DeleteDealerEMI/?id=" + self.hdnLoanAmountId(),
+            success: function (response) {
+                self.Reset();
+                self.hdnLoanAmountId('');
+                $("#btnSaveEMI").html('Add EMI');
+                $('#btnDelete').hide();
+                Materialize.toast('Data has been deleted.', 4000);
+            }
+        });
+
+    };
+}
+
+var vmNewBikeDealerEMI = new NewBikeDealerEMIViewModel();
+$(document).ready(function () {
+
+    try {
+        ko.applyBindings(vmNewBikeDealerEMI);
+
+        if (vmNewBikeDealerEMI.hdnLoanAmountId() != "") {
+            $("#btnSaveEMI").html('Update EMI');
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+});
