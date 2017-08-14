997f3e79e8d31e796dd3dbd4632cb2e9f6048cc3
sajal.gupta <sajal.gupta@USER-CW-197>
2017-08-14 19:22:41 +0530

    Fixed code review changes

--- BikeWaleOpr.UI/src/ConfigurePAgeMetas.js
+++ BikeWaleOpr.UI/src/ConfigurePAgeMetas.js
@@ -8,6 +8,27 @@ var ConfigurePageMetas = function () {
     self.pageList = ko.observableArray();
     self.selectedPage = ko.observable();        
 
+    self.validateData = function () {
+        var isValid = true;
+
+        if (!self.selectedMakeId()) {
+            Materialize.toast("Please select Make", 3000);
+            isValid = false;
+        }
+
+        if (!self.selectedPage()) {
+            Materialize.toast("Please select Page", 3000);
+            isValid = false;
+        }
+
+        if ((self.selectedPage() == '3' || self.selectedPage() == '4') && !self.selectedModel()) {
+            Materialize.toast("Please select Model", 3000);
+            isValid = false;
+        }
+
+        return isValid;
+    };
+
     self.changeMake = function () {       
         var makeId = $('#selectMake').find(":selected").val();
         if (makeId) {
@@ -42,12 +63,36 @@ var ConfigurePageMetas = function () {
 
     };
 
+    self.selectPlatform = function () {
+        if ($("input[name='platformId']:checked").val() == "1")
+            self.pageList(JSON.parse($('#dektopPagesList').val()));
+        else
+            self.pageList(JSON.parse($('#mobilePagesList').val()));
+
+        $('select[name="pageId"]').material_select();
+    };
+    
+    self.init = function () {
+        if ($('#pageMetaId').val() > 0) {        
+        if ($("input[name='platformId']:checked").val() == "1")
+            self.pageList(JSON.parse($('#dektopPagesList').val()));
+        else
+            self.pageList(JSON.parse($('#mobilePagesList').val()));
+
+
+        if ($('#selectPage') && $('#selectPage').attr('data-selectedPage') > 0)
+            self.selectedPage($('#selectPage').attr('data-selectedPage'));       
+
+        self.changeMake();
+    }
+
+    $('select[name="modelId"]').material_select();
+    $('select[name="pageId"]').material_select();   
+    };
 }
 
 var vmConfigurePageMetas = new ConfigurePageMetas;
 $(document).ready(function () {
-
-    
 
     if ($(".stepper")) {
         $('.stepper').activateStepper({ autoFocusInput: false });
@@ -58,55 +103,7 @@ $(document).ready(function () {
 
     if (pageMeta) {
         ko.applyBindings(vmConfigurePageMetas, pageMeta[0]);
-    }
-
-    $(document).on("change", "input[type=radio][name='platformId']", function () {
-        if ($("input[name='platformId']:checked").val() == "1")
-            vmConfigurePageMetas.pageList(JSON.parse($('#dektopPagesList').val()));
-        else
-            vmConfigurePageMetas.pageList(JSON.parse($('#mobilePagesList').val()));
-
-        $('select[name="pageId"]').material_select();
-    });
-
-
-    if ($('#pageMetaId').val() > 0) {        
-        if ($("input[name='platformId']:checked").val() == "1")
-            vmConfigurePageMetas.pageList(JSON.parse($('#dektopPagesList').val()));
-        else
-            vmConfigurePageMetas.pageList(JSON.parse($('#mobilePagesList').val()));
-
-
-        if ($('#selectPage') && $('#selectPage').attr('data-selectedPage') > 0)
-            vmConfigurePageMetas.selectedPage($('#selectPage').attr('data-selectedPage'));       
-
-        vmConfigurePageMetas.changeMake();
-    }
-
-    $('select[name="modelId"]').material_select();
-    $('select[name="pageId"]').material_select();
-
-
-    $('#btnConfigurePageMeta').click(function () {
-        var isValid = true;
-
-        if (!($('#selectMake').val())) {
-            Materialize.toast("Please select Make", 3000);
-            isValid = false;
-        }
-
-        if (!($('#selectPage').val())) {
-            Materialize.toast("Please select Page", 3000);
-            isValid = false;
-        }
-
-        if ((vmConfigurePageMetas.selectedPage() == '3' || vmConfigurePageMetas.selectedPage() == '4') && !($('#selectModel').val()))
-        {
-            Materialize.toast("Please select Model", 3000);
-            isValid = false;
-        }
-
-        return isValid;
-    });
+        vmConfigurePageMetas.init();
+    }        
 
 });
