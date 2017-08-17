10989094dddb827da5e2ae2f7ec01492e53c3783
ashu82492 <ashutosh.sharma@carwale.com>
2017-08-17 21:30:49 +0530

    bakend and frontend done.

---
+++ BikeWale.UI/Models/Videos/ScooterVideos.cs
@@ -0,0 +1,53 @@
+﻿using Bikewale.Entities;
+using Bikewale.Interfaces.Videos;
+using Bikewale.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bikewale.Models.Videos
+{
+    /// <summary>
+    /// Created by : Ashutosh Sharma on 17-Aug-2017
+    /// Description : Provide method to get list of scooter videos.
+    /// </summary>
+    public class ScooterVideos
+    {
+        private readonly IVideos _videos = null;
+
+        
+
+        public ScooterVideos(IVideos videos)
+        {
+            _videos = videos;
+        }
+
+        /// <summary>
+        /// Created by : Ashutosh Sharma on 17-Aug-2017
+        /// Description : Method to get list of scooter videos.
+        /// </summary>
+        /// <returns>List of scooter videos.</returns>
+        public ScooterVideosVM GetData()
+        {
+            ScooterVideosVM objVideosList = null;
+
+            try
+            {
+                string bodyStyleId = "5";
+
+                objVideosList = new ScooterVideosVM();
+                objVideosList.VideosList = _videos.GetVideosByMakeModel(0, 0, bodyStyleId, 0, 0);
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                ErrorClass objErr = new ErrorClass(ex, string.Format("Bikewale.Models.Videos.ScooterVideos.GetData"));
+                
+            }
+            return objVideosList;
+        }
+    }
+}
---
+++ Bikewale.Models/Videos/ScooterVideosVM.cs
@@ -0,0 +1,14 @@
+﻿using Bikewale.Entities.Videos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.Models.Videos
+{
+    public class ScooterVideosVM : ModelBase
+    {
+        public IEnumerable<BikeVideoEntity> VideosList { get; set; }
+    }
+}
