e800813f93cb25a39dff39f89bae0386e2fc84ac
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-08-27 16:40:26 +0530

    Fixed model page getpricequotebyid call - Fixed model page getpricequotebyid call
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- BikeWale.UI/Models/BikeModels/ModelPage.cs
+++ BikeWale.UI/Models/BikeModels/ModelPage.cs
@@ -714,7 +714,10 @@ namespace Bikewale.Models.BikeModels
                                 {
                                     var selectVersion = _pqOnRoad.DPQOutput.Varients.Where(m => m.objVersion.VersionId == version.VersionId).FirstOrDefault();
                                     if (selectVersion != null)
-                                        version.Price = selectVersion.OnRoadPrice;
+                                    {
+                                        version.Price = selectVersion.OnRoadPrice; break;
+                                    }
+
                                 }
 
                                 ///Choose the min price version of dealer
@@ -731,6 +734,7 @@ namespace Bikewale.Models.BikeModels
                                     if (selected != null)
                                     {
                                         version.Price = !_objData.ShowOnRoadButton ? selected.OnRoadPrice : selected.Price;
+                                        break;
                                     }
                                 }
                                 ///Choose the min price version of city level pricing
@@ -1152,7 +1156,8 @@ namespace Bikewale.Models.BikeModels
                     if (objPQOutput != null && objPQOutput.PQId > 0)
                     {
                         _objData.PQId = (uint)objPQOutput.PQId;
-                        bpqOutput = _objPQ.GetPriceQuoteById(objPQOutput.PQId, LeadSource);
+                        //bpqOutput = _objPQ.GetPriceQuoteById(objPQOutput.PQId, LeadSource);
+                        bpqOutput = new BikeQuotationEntity();
                         bpqOutput.Varients = _objPQCache.GetOtherVersionsPrices(_modelId, _cityId);
                         if (bpqOutput != null)
                         {
