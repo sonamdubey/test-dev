7f08d6ce00fa5d1d36582d7551726a7e98795db7
subodh jain <subodh.jain@carwale.com>
2017-09-01 13:04:44 +0530

    Signed-off-by: subodh jain <subodh.jain@carwale.com>

---
+++ BikeWale.UI/Controllers/PopUpController.cs
@@ -0,0 +1,21 @@
+﻿using Bikewale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers
+{
+    public class PopUpController : Controller
+    {
+        // GET: PopUp
+        [Route("m/popup/AMP/")]
+        public ActionResult Index(string queryString)
+        {
+            PoupCityAreaVM objPopupCityAreaVM = new PoupCityAreaVM();
+
+            return View("~/Views/m/Shared/_PopupWidget.cshtml");
+        }
+    }
+}
