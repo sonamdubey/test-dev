58bc3f1346a809cf492df63ea9e124bb24fd6f69
rahul.pote <rahul.pote@USER793>
2017-09-09 14:48:57 +0530

    add authors page

---
+++ BikeWale.UI/Controllers/AuthorsController.cs
@@ -0,0 +1,60 @@
+﻿using Bikewale.Entities;
+using Bikewale.Entities.BikeData;
+using Bikewale.Interfaces.BikeBooking;
+using Bikewale.Interfaces.BikeData;
+using Bikewale.Interfaces.BikeData.UpComing;
+using Bikewale.Interfaces.CMS;
+using Bikewale.Interfaces.Compare;
+using Bikewale.Interfaces.Dealer;
+using Bikewale.Interfaces.Location;
+using Bikewale.Interfaces.PriceQuote;
+using Bikewale.Interfaces.ServiceCenter;
+using Bikewale.Interfaces.Used;
+using Bikewale.Interfaces.UsedBikes;
+using Bikewale.Interfaces.UserReviews;
+using Bikewale.Interfaces.UserReviews.Search;
+using Bikewale.Interfaces.Videos;
+using Bikewale.ManufacturerCampaign.Entities;
+using Bikewale.ManufacturerCampaign.Interface;
+using Bikewale.Models.BikeModels;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers
+{
+    public class AuthorController : Controller
+    {
+      
+
+        // GET: Models
+        [Route("m/authors/listing/"), Filters.DeviceDetection]
+        public ActionResult Listing_Mobile()
+        {
+            ModelPageVM obj = new ModelPageVM();
+            return View(obj);
+        }
+
+        // GET: Models
+        [Route("m/authors/details/"), Filters.DeviceDetection]
+        public ActionResult Details_Mobile()
+        {
+            ModelPageVM obj = new ModelPageVM();
+            return View(obj);
+        }
+
+        // GET: Models
+        [Route("authors/listing/"), Filters.DeviceDetection]
+        public ActionResult Listing_Desktop()
+        {
+            ModelPageVM obj = new ModelPageVM();
+            return View(obj);
+        }
+        // GET: Models
+        [Route("authors/details/"), Filters.DeviceDetection]
+        public ActionResult Details_Desktop()
+        {
+            ModelPageVM obj = new ModelPageVM();
+            return View(obj);
+        }
+
+    }   // class
+}   // namespace
---
+++ BikeWale.UI/src/authors.js
@@ -0,0 +1,32 @@
+﻿(function ($) {
+    $(function () {
+        var jcarousel = $('#author__caraousel .jcarousel');
+
+        $('#author__carousel .jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=2'
+            });
+
+        $('#author__carousel .jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=2'
+            });
+
+        $('#author__carousel .jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function () {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function () {
+                $(this).removeClass('active');
+            })
+            .on('click', function (e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function (page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+            });
+    });
+})(jQuery);
