08a43a52e167e216c885f339620fc0549026a54b
cw-sumitkate <sumit.kate@carwale.com>
2017-09-11 19:16:20 +0530

    Capital First voucher processing - Create Consumer for capital first voucher processing - Pending: Email and SMS integration
    Signed-off-by: cw-sumitkate <sumit.kate@carwale.com>

---
+++ Bikewale.RabbitMq.CapitalFirstLeadConsumer/CapitalFirstLeadEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bikewale.RabbitMq.CapitalFirstLeadConsumer
+{
+    internal class CapitalFirstLeadEntity
+    {
+        public string CTLeadId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNo { get; set; }
+        public string EmailId { get; set; }
+        public string BikeName { get; set; }
+        public uint Exshowroom { get; set; }
+        public uint RTO { get; set; }
+        public uint Insurance { get; set; }
+        public string VoucherNumber { get; set; }
+        public DateTime VoucherExpiryDate { get; set; }
+        public string AgentName { get; set; }
+        public string AgentNumber { get; set; }
+    }
+}
