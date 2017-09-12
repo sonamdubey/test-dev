a542f9295cc4f20ba8754b52c051a5d593cbc1dd
cw-sushilkumar <kanojia.sushil@gmail.com>
2017-09-12 15:16:01 +0530

    Fixed breadcrum for layout - Fixed breadcrum for layout
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- Bikewale.Entities/Schema/BreadcrumbListItem.cs
+++ Bikewale.Entities/Schema/BreadcrumbListItem.cs
@@ -1,13 +1,15 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Bikewale.Entities.Schema
 {
     public class BreadcrumbListItem : ListItem
     {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
         [JsonProperty("item", NullValueHandling = NullValueHandling.Ignore)]
-        public BreadcrumbItem Item { get; set; }
+        public BreadcrumbItem Item { get; set; } 
     }
 }
