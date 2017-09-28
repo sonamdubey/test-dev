d3ed90dab0b59cfff504feb7c2d908c818de1868
Snehal Niranjan Dange <snehal.dange@carwale.com>
2017-09-28 16:56:56 +0530

    Change "Rate this Bike" to "Rate & Win" -Added makeDetails and ModelsDetail in writereviewcontest model -Added makEiD,ModelId and related prop in view model of makemodelpopup -encoded the url string in all pages which linked to review-contest-page ---ExpertReview,News,Scooters,Features,HomePage , Reviews,[Desktop & Mobile] ---Index_detailscard , _index_new_mobile, upcoming
    Signed-off-by: Snehal Niranjan Dange <snehal.dange@carwale.com>

--- Bikewale.Models/Make/MakeModelPopupVM.cs
+++ Bikewale.Models/Make/MakeModelPopupVM.cs
@@ -6,9 +6,15 @@ namespace Bikewale.Models.Make
     /// <summary>
     /// Created By  :   Vishnu Teja Yalakuntla on 22nd Aug 2017
     /// Description :   View Model for the popup which containes Makes and Models. 
+    /// Modified By :   Snehal Dange on 25th Sep 2017
+    /// Descrption By:  Added makeId and modelId  
     /// </summary>
     public class UserReviewPopupVM
     {
         public IEnumerable<BikeMakeEntityBase> Makes { get; set; }
+        public uint? MakeId { get; set; }
+        public uint? ModelId { get; set; }
+
+        public string ModelName { get; set; }
     }
 }
