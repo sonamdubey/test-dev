5801c0d419d433b670bbb3dc3b9c608d6786c8af
ashu82492 <ashutosh.sharma@carwale.com>
2017-10-27 12:29:10 +0530

    sp changed from getmodeldetails_new_12102017 to getmodeldetails_new_23102017.

--- Bikewale.DAL/BikeData/BikeModelsRespository.cs
+++ Bikewale.DAL/BikeData/BikeModelsRespository.cs
@@ -367,6 +367,8 @@ namespace Bikewale.DAL.BikeData
         /// Description : Changed SP from 'getmodeldetails_new_28062017' to 'getmodeldetails_new_30082017', removed IsGstPrice flag
         /// Modified By :Snehal Dange on 12 Oct 2017
         /// Description : Changed Sp from 'getmodeldetails_new_30082017' to 'getmodeldetails_new_12102017' added IsScooterOnly flag
+        /// Modified by : Ashutosh Sharma on 23 Oct 2017 
+        /// Description : Changed SP from 'getmodeldetails_new_12102017' to 'getmodeldetails_new_23102017'
         /// </summary>
         /// <param name="id">Model Id should be a positive number.</param>
         /// <returns>Returns object containing the particular model's all details.</returns>
@@ -375,7 +377,7 @@ namespace Bikewale.DAL.BikeData
             T t = default(T);
             try
             {
-                using (DbCommand cmd = DbFactory.GetDBCommand("getmodeldetails_new_12102017"))
+                using (DbCommand cmd = DbFactory.GetDBCommand("getmodeldetails_new_23102017"))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.Add(DbFactory.GetDbParam("par_modelid", DbType.Int32, id));
