bfa7aabcae5b24beda87915c39013fa0cd87b234
ashu82492 <ashutosh.sharma@carwale.com>
2017-10-30 18:33:56 +0530

    Ad slot on-off options - OPR done

---
+++ BikewaleOpr.Interface/AdSlot/IAdSlotRepository.cs
@@ -0,0 +1,19 @@
+﻿using BikewaleOpr.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikewaleOpr.Interface.AdSlot
+{
+    /// <summary>
+    /// Created by : Ashutosh Sharma on 30 Oct 2017
+    /// Description : Provide methods to get data for ad slots.
+    /// </summary>
+    public interface IAdSlotRepository
+    {
+        IEnumerable<AdSlotEntity> GetAdSlots();
+        bool ChangeStatus(uint AdId, uint UserId);
+    }
+}
---
+++ BikewaleOpr.Service/Controllers/AdSlotController.cs
@@ -0,0 +1,47 @@
+﻿using Bikewale.Notifications;
+using BikewaleOpr.Interface.AdSlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BikewaleOpr.Service.Controllers
+{
+    /// <summary>
+    /// Created by : Ashutosh Sharma on 30 Oct 2017
+    /// Description : Api controller for ad slots.
+    /// </summary>
+    public class AdSlotController : ApiController
+    {
+
+        private readonly IAdSlotRepository _AdSlotRepository = null;
+        public AdSlotController(IAdSlotRepository AdSlotRepository)
+        {
+            _AdSlotRepository = AdSlotRepository;
+        }
+
+        /// <summary>
+        /// Created by : Ashutosh Sharma on 30 Oct 2017
+        /// Description : Api method to change status of ad slot.
+        /// </summary>
+        /// <param name="AdId">Id of Ad slot that need to be changed.</param>
+        /// <param name="UserId">Id of User who is making changes.</param>
+        /// <returns></returns>
+        [HttpPost, Route("api/adslot/change/{AdId}")]
+        public IHttpActionResult ChangeStatusAdSlot(uint AdId, uint UserId)
+        {
+            try
+            {
+                bool IsChanged = _AdSlotRepository.ChangeStatus(AdId, UserId);
+                return Ok(IsChanged);
+            }
+            catch (Exception ex)
+            {
+                ErrorClass objErr = new ErrorClass(ex, String.Format("AdSlotController.ChangeStatusAdSlot({0},{1})", AdId, UserId));
+                return InternalServerError();
+            }
+        }
+    }
+}
