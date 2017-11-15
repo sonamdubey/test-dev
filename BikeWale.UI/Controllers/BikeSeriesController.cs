0d85263accfeb997577bb5da0cd9df4ba1d0e3c1
ashu82492 <ashutosh.sharma@carwale.com>
2017-11-15 14:39:29 +0530

    series page controller and vm created.

--- BikeWale.UI/Controllers/BikeSeriesController.cs
+++ BikeWale.UI/Controllers/BikeSeriesController.cs
@@ -4,17 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Bikewale.Models.BikeModels;
+using Bikewale.Interfaces.BikeData;
+using Bikewale.Models.BikeSeries;
 
 namespace Bikewale.Controllers
 {
+	/// <summary>
+	/// Created by : Ashutosh Sharma on 15 Nov 2017
+	/// Description : UI controller for bike series page.
+	/// </summary>
     public class BikeSeriesController : Controller
     {
-        // GET: BikeSeries
-        [Route("m/model/series/"), Filters.DeviceDetection]
+		private readonly IBikeSeriesCacheRepository _seriesCache;
+		public BikeSeriesController(IBikeSeriesCacheRepository seriesCache)
+		{
+			_seriesCache = seriesCache;
+		}
+		/// <summary>
+		/// Created by : Ashutosh Sharma on 15 Nov 2017
+		/// Description : Action method for desktop.
+		/// </summary>
+		/// <returns></returns>
+		[Route("model/series/"), Filters.DeviceDetection]
+		public ActionResult Index()
+		{
+			SeriesPageVM obj;
+			SeriesPage seriesPage = new SeriesPage(_seriesCache);
+			obj = seriesPage.GetData();
+			return View(obj);
+		}
+
+		/// <summary>
+		/// Created by : Ashutosh Sharma on 15 Nov 2017
+		/// Description : Action method for mobile.
+		/// </summary>
+		/// <returns></returns>
+		[Route("m/model/series/"), Filters.DeviceDetection]
         public ActionResult Index_List_Mobile()
         {
-            ModelPageVM obj = new ModelPageVM();
-            return View(obj);
+			SeriesPageVM obj;
+			SeriesPage seriesPage = new SeriesPage(_seriesCache);
+			obj = seriesPage.GetData();
+			return View(obj);
         }
     }
 }
---
+++ BikeWale.UI/Models/BikeSeries/SeriesPage.cs
@@ -0,0 +1,33 @@
+﻿using Bikewale.Interfaces.BikeData;
+using Bikewale.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bikewale.Models.BikeSeries
+{
+	public class SeriesPage
+	{
+		private readonly IBikeSeriesCacheRepository _seriesCache;
+		public SeriesPage(IBikeSeriesCacheRepository seriesCache)
+		{
+			_seriesCache = seriesCache;
+		}
+
+		public SeriesPageVM GetData()
+		{
+			SeriesPageVM objSeriesPage = null;
+			try
+			{
+				objSeriesPage = new SeriesPageVM();
+				objSeriesPage.SeriesModels =  _seriesCache.GetModelsListBySeriesId(2);
+			}
+			catch (Exception ex)
+			{
+				ErrorClass objErr = new ErrorClass(ex, "Bikewale.Models.BikeSeries.SeriesPage.GetData");
+			}
+			return objSeriesPage;
+		}
+	}
+}
