cc54fa8a85b833a43c07f19b6cfebeaa57829adf
Ashish Kamble <ashish.kamble@carwale.com>
2017-11-22 11:43:28 +0530

    Model Series page url redirection - Add Masking Name processing for Series and model. If given masking name is not series masking name then redirect to model page - Added Model Page view path in Model Controller action methods - Rewrite rule for model page is changed to seriescontroller - Removed commented GetModelsListBySeriesId method from Series repo
    Signed-off-by: Ashish Kamble <ashish.kamble@carwale.com>

--- Bikewale.DAL/BikeData/BikeSeriesRespository.cs
+++ Bikewale.DAL/BikeData/BikeSeriesRespository.cs
@@ -198,56 +198,7 @@ namespace Bikewale.DAL.BikeData
             }
             return objModelsList;
         }
-
-
-        //public BikeSeriesModels GetModelsListBySeriesId(uint seriesId)
-        //      {
-        //          BikeSeriesModels objBikeSeriesModels = null;
-        //          try
-        //          {
-        //              using (IDbConnection connection = DatabaseHelper.GetReadonlyConnection())
-        //              {
-        //                  DynamicParameters param = new DynamicParameters();
-        //                  param.Add("par_seriesId", seriesId);
-        //                  var reader = connection.QueryMultiple("getmodelsbyseriesid", param: param, commandType: CommandType.StoredProcedure);
-        //                  if (reader != null)
-        //                  {
-        //                      objBikeSeriesModels = new BikeSeriesModels();
-        //                      objBikeSeriesModels.NewBikes = reader.Read<BikeMakeBase, BikeModelEntityBase, ImageEntityBase, MinSpecsEntity,  NewBikeEntityBase>(
-        //                          (bikeMakeBase, bikeModelEntityBase, imageEntityBase, minSpecsEntity) => 
-        //                          {
-        //                              NewBikeEntityBase newBikeEntityBase = new NewBikeEntityBase()
-        //                              {
-        //                                  BikeMake = bikeMakeBase,
-        //                                  BikeModel = bikeModelEntityBase,
-        //                                  BikeImage = imageEntityBase,
-        //                                  MinSpecs =  minSpecsEntity
-        //                              };
-        //                              return newBikeEntityBase;
-        //                          }, splitOn: "ModelId, HostURL, Displacement"
-        //                          );
-        //                      objBikeSeriesModels.UpcomingBikes = reader.Read<UpcomingBikeEntityBase, BikeMakeBase, BikeModelEntityBase, ImageEntityBase, UpcomingBikeEntityBase>(
-        //                              (upcomingBikeEntityBase, bikeMakeBase, bikeModelEntityBase, imageEntityBase) => 
-        //                              {
-        //                                  upcomingBikeEntityBase.BikeMake = bikeMakeBase;
-        //                                  upcomingBikeEntityBase.BikeModel = bikeModelEntityBase;
-        //                                  upcomingBikeEntityBase.BikeImage = imageEntityBase;
-        //                                  return upcomingBikeEntityBase;
-        //                              }, splitOn: "MakeId, ModelId, HostURL"
-        //                          );
-        //                  }
-        //                  if (connection.State == ConnectionState.Open)
-        //                  {
-        //                      connection.Close();
-        //                  }
-        //              }
-        //          }
-        //          catch (Exception ex)
-        //          {
-        //              ErrorClass objErr = new ErrorClass(ex, string.Format("DAL.BikeData.BikeSeriesRepository.GetModelsListBySeriesId SeriesId = {0}", seriesId));
-        //          }
-        //          return objBikeSeriesModels;
-        //      }   // end of GetModelsListBySeriesId
+        
 
         public BikeDescriptionEntity GetSynopsis(uint seriesId)
         {
