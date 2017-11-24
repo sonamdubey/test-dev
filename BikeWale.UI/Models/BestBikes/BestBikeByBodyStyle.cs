c1a9f8049c9dd38db8271caaef54ba4d53c7d457
snehal.dange <snehal.dange@carwale.com>
2017-11-24 19:49:28 +0530

    Replace last card in best bikes widget with creative card as on deler locator page - Added bodystyle property in  BestBikeList.cs - Replaced 9th card in _BestBikeByBodyStyleCarousel.cshtml [Mob & Desktop] - Implemented on home page and new bike page - Swaped userreviews and best bike widget in NewPage/iNDEX [desktop and Mobile]
    Signed-off-by: snehal.dange <snehal.dange@carwale.com>

--- BikeWale.UI/Models/BestBikes/BestBikeByBodyStyle.cs
+++ BikeWale.UI/Models/BestBikes/BestBikeByBodyStyle.cs
@@ -4,38 +4,55 @@ using Bikewale.Notifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace Bikewale.Models
 {
     public class BestBikeByBodyStyle
     {
         private readonly IBikeModelsCacheRepository<int> _objBestBikes = null;
-        
+
         public EnumBikeBodyStyles BodyStyleType;
 
         public uint topCount { get; set; }
-        public BestBikeByBodyStyle( IBikeModelsCacheRepository<int> objBestBikes)
+        public BestBikeByBodyStyle(IBikeModelsCacheRepository<int> objBestBikes)
         {
-           
+
             _objBestBikes = objBestBikes;
         }
 
+        /// <summary>
+        /// Modified By : Snehal Dange on 24th Nov 2017
+        /// Description: Added EnumBikeBodyStyles  for each category
+        /// </summary>
+        /// <returns></returns>
         public BestBikeByCategoryVM GetData()
         {
             BestBikeByCategoryVM objData = null;
             try
             {
                 objData = new BestBikeByCategoryVM();
-               
+
                 objData.objBestScootersList = FetchBestBikesList(EnumBikeBodyStyles.Scooter);
-                
                 objData.objBestSportsBikeList = FetchBestBikesList(EnumBikeBodyStyles.Sports);
                 objData.objBestCruiserBikesList = FetchBestBikesList(EnumBikeBodyStyles.Cruiser);
                 objData.objBestMileageBikesList = FetchBestBikesList(EnumBikeBodyStyles.Mileage);
-                
 
-
+                if (objData.objBestScootersList != null && objData.objBestScootersList.Any())
+                {
+                    objData.objBestScootersList.First().CurrentPage = EnumBikeBodyStyles.Scooter;
+                }
+                if (objData.objBestSportsBikeList != null && objData.objBestSportsBikeList.Any())
+                {
+                    objData.objBestSportsBikeList.First().CurrentPage = EnumBikeBodyStyles.Sports;
+                }
+                if (objData.objBestCruiserBikesList != null && objData.objBestCruiserBikesList.Any())
+                {
+                    objData.objBestCruiserBikesList.First().CurrentPage = EnumBikeBodyStyles.Cruiser;
+                }
+                if (objData.objBestMileageBikesList != null && objData.objBestMileageBikesList.Any())
+                {
+                    objData.objBestMileageBikesList.First().CurrentPage = EnumBikeBodyStyles.Mileage;
+                }
             }
             catch (Exception ex)
             {
@@ -55,16 +72,16 @@ namespace Bikewale.Models
             try
             {
 
-                
-                 objBikesList = _objBestBikes.GetBestBikesByCategory(BodyStyleType);
+
+                objBikesList = _objBestBikes.GetBestBikesByCategory(BodyStyleType);
 
                 if (objBikesList != null)
                 {
                     objBikesList = objBikesList.Reverse();
-                   objBikesList = objBikesList.Take((int)topCount);
+                    objBikesList = objBikesList.Take((int)topCount);
                 }
 
-             
+
             }
             catch (Exception ex)
             {
