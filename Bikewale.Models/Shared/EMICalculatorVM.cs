b84ffa91605f7dd0e23a231eb804d232827540c2
cw-viveksingh <vivek.singh@carwale.com>
2017-12-01 15:38:20 +0530

    with required changes

--- Bikewale.Models/Shared/EMICalculatorVM.cs
+++ Bikewale.Models/Shared/EMICalculatorVM.cs
@@ -6,6 +6,8 @@ namespace Bikewale.Models
     /// <summary>
     /// Created by  :   Sumit Kate on 30 Nov 2017
     /// Description :   EMI Calculator View Model for EMICalculator View
+    /// Modified by :   Vivek Singh Tomar on 01st Nov 2017
+    /// Description :   Added LeadSourceEnum to capture dealer lead source id
     /// </summary>
     public class EMICalculatorVM
     {
@@ -15,9 +17,9 @@ namespace Bikewale.Models
         public bool IsMobile { get; set; }
         public ManufactureCampaignEMIEntity ESEMICampaign { get; set; }
         public string BikeName { get; set; }
-        public string BikeModelName { get; set; }
         public uint PQId { get; set; }
         public bool IsPremiumDealer { get; set; }
         public NewBikeDealers DealerDetails { get; set; }
+        public LeadSourceEnum PremiumDealerLeadSourceId { get; set; }
     }
 }
