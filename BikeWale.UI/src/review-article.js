8d20f7cd72aa4c4dcb1087b8202f21043009d60d
rahul.pote <rahul.pote@USER793>
2017-12-05 19:37:17 +0530

    d model expert review card added

---
+++ BikeWale.UI/src/review-article.js
@@ -0,0 +1,7 @@
+﻿//$('.expert-article__carousel').jcarousel();
+//$('.expert-article__carousel .jcarousel-control-prev').jcarouselControl({
+//    target: '-=1'
+//});
+//$('.expert-article__carousel .jcarousel-control-next').jcarouselControl({
+//    target: '+=1'
+//});
