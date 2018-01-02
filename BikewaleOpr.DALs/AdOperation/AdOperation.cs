458b15a3039f5127e177d6d4ffde72d423f23bcf
snehal.dange <snehal.dange@carwale.com>
2018-01-02 12:12:36 +0530

    Add latest bike in opr skeleton - Created entity : PromotedBike ,AdOperationEnum - Created controller : AdOperation  in bwopr - Created respective dal and model
    Signed-off-by: snehal.dange <snehal.dange@carwale.com>

---
+++ BikewaleOpr.DALs/AdOperation/AdOperation.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BikewaleOpr.DALs.AdOperation
+{
+    /// <summary>
+    /// Created by : Snehal Dange on 2nd Jan 2018
+    /// Descripion: AdOperations repository
+    /// </summary>
+    public class AdOperation
+    {
+    }
+}
