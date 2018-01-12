2c2f928e8c150316d937fe3f59f684171369cc19
Rajan Chauhan <rajan.chauhan@carwale.com>
2018-01-12 12:29:16 +0530

    Changed key name in BikewaleMakesCacheRepository

--- Bikewale.Models/Photos/v1/PhotosPageVM.cs
+++ Bikewale.Models/Photos/v1/PhotosPageVM.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Created By  : Rajan Chauhan on 09 Jan 2017
+/// Description : Added OtherPopularMakes
+/// </summary>
 namespace Bikewale.Models.Photos.v1
 {
     public class PhotosPageVM : ModelBase
