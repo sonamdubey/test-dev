2493b97fda7355325a926595b004eed8d1de0a15
Rajan Chauhan <rajan.chauhan@carwale.com>
2018-01-15 11:15:54 +0530

    Fix image landing redirection
    Signed-off-by: Rajan Chauhan <rajan.chauhan@carwale.com>

--- BikeWale.UI/m/src/loadPhotos.js
+++ BikeWale.UI/m/src/loadPhotos.js
@@ -3,14 +3,14 @@
 function NextClicked() {
     currentIndex++;
     fullUrl = $("#divPhotos .thumbDiv").eq(currentIndex).find("img").attr("src").toString().replace("/t/", "/l/");
-    $("#divPhotosOverlay").attr("style", "position:absolute;top:0;left:0;width:100%;height:" + (parseInt($("#divLargeImgContainer").height()) + 20) + "px;z-index;1000;background-color:black;filter:alpha(opacity=80);opacity:0.80;background-image: url('/m/images/loader.gif');background-position:center center;background-repeat:no-repeat;");
+    $("#divPhotosOverlay").attr("style", "position:absolute;top:0;left:0;width:100%;height:" + (parseInt($("#divLargeImgContainer").height()) + 20) + "px;z-index;1000;background-color:black;filter:alpha(opacity=80);opacity:0.80;background-image: url('/m/image/loader.gif');background-position:center center;background-repeat:no-repeat;");
     LoadLargePhoto();
 }
 
 function PrevClicked() {
     currentIndex--;
     fullUrl = $("#divPhotos .thumbDiv").eq(currentIndex).find("img").attr("src").toString().replace("/t/", "/l/");
-    $("#divPhotosOverlay").attr("style", "position:absolute;top:0;left:0;width:100%;height:" + (parseInt($("#divLargeImgContainer").height()) + 20) + "px;z-index;1000;background-color:black;filter:alpha(opacity=80);opacity:0.80;background-image: url('/m/images/loader.gif');background-position:center center;background-repeat:no-repeat;");
+    $("#divPhotosOverlay").attr("style", "position:absolute;top:0;left:0;width:100%;height:" + (parseInt($("#divLargeImgContainer").height()) + 20) + "px;z-index;1000;background-color:black;filter:alpha(opacity=80);opacity:0.80;background-image: url('/m/image/loader.gif');background-position:center center;background-repeat:no-repeat;");
     LoadLargePhoto();
 }
 
@@ -30,6 +30,6 @@ function ShowLargePhotos(_thumbDiv) {
             i++;
     });
 
-    $("#divPhotosOverlay").attr("style", "position:absolute;top:0;left:0;width:100%;height:" + (parseInt($("#divPhotos").height()) + 10) + "px;z-index;1000;background-color:black;filter:alpha(opacity=80);opacity:0.80;background-image: url('/m/images/loader.gif');background-position:center center;background-repeat:no-repeat;");
+    $("#divPhotosOverlay").attr("style", "position:absolute;top:0;left:0;width:100%;height:" + (parseInt($("#divPhotos").height()) + 10) + "px;z-index;1000;background-color:black;filter:alpha(opacity=80);opacity:0.80;background-image: url('/m/image/loader.gif');background-position:center center;background-repeat:no-repeat;");
     LoadLargePhoto();
 }
