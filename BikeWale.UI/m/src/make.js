79aa72e696fcee4d68b0e37a0e6da7e9fd9ddd39
dutiyesh <dutiyesh.salunkhe@carwale.com>
2018-01-15 17:08:09 +0530

    Add pros and cons list - Add bike pros and cons list - Add bike list

--- BikeWale.UI/m/src/make.js
+++ BikeWale.UI/m/src/make.js
@@ -1,1 +1,6 @@
-﻿
+﻿docReady(function() {
+	$('.model-card__pros-cons').on('click', '.pros-cons__more-btn', function(event) {
+		$(this).hide();
+		$(this).closest('.pros-cons__content').find('li').show();
+	});
+});
