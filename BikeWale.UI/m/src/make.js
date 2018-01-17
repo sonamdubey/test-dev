6765713ff4d5cc9c6e326f88f9dc4dc76369cf3f
dutiyesh <dutiyesh.salunkhe@carwale.com>
2018-01-17 15:21:24 +0530

    Add collapse pros-cons list feature - Replace color codes with variables

--- BikeWale.UI/m/src/make.js
+++ BikeWale.UI/m/src/make.js
@@ -6,7 +6,36 @@
 
 	// popular bikes carousel
 	$('.carousel__popular-bikes').on('click', '.view-pros-cons__target', function() {
-		$(this).closest('.model__card').addClass('card--active');
+		var modelCard = $(this).closest('.model__card');
+
+		$(this).hide();
+
+		if (!modelCard.hasClass('card--active')) {
+			modelCard.addClass('card--active');
+		}
+		else {
+			modelCard.removeClass('card--active');
+		}
+	});
+
+	$('.carousel__popular-bikes').on('webkitTransitionEnd transitionend', '.model-card__detail', function() {
+		var modelCard = $(this).closest('.model__card');		
+		var collpaseTargetElement = modelCard.find('.view-pros-cons__target');
+
+		var collapseCurrentText = collpaseTargetElement.html(),
+			collapseNextText = collpaseTargetElement.attr('data-text');
+
+		if (!collpaseTargetElement.is(':visible')) {
+			if (!modelCard.hasClass('card--active')) {
+				modelCard.removeClass('collapse-btn--active');
+			}
+			else {
+				modelCard.addClass('collapse-btn--active');
+			}
+			
+			collpaseTargetElement.attr('data-text', collapseCurrentText);
+			collpaseTargetElement.html(collapseNextText).fadeIn();
+		}
 	});
 
 });
