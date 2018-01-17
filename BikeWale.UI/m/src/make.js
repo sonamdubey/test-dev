91c5c02be07b9f6148f3dade34768914cf6e2e9c
dutiyesh <dutiyesh.salunkhe@carwale.com>
2018-01-17 11:02:01 +0530

    Add popular bikes carousel - Add popular bikes carousel - Truncate model name in card

--- BikeWale.UI/m/src/make.js
+++ BikeWale.UI/m/src/make.js
@@ -3,4 +3,10 @@
 		$(this).hide();
 		$(this).closest('.pros-cons__content').find('li').show();
 	});
+
+	// popular bikes carousel
+	$('.carousel__popular-bikes').on('click', '.view-pros-cons__target', function() {
+		$(this).closest('.model__card').addClass('card--active');
+	});
+
 });
