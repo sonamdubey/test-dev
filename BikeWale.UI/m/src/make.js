4f928e863ddec11dd4472c9d6df9a3d928fea25b
rahul.pote <rahul.pote@USER793>
2018-01-23 13:53:23 +0530

    floating nav functionality added

--- BikeWale.UI/m/src/make.js
+++ BikeWale.UI/m/src/make.js
@@ -6,7 +6,111 @@
 
     //interesting fact popup
 	interestingFactPopup.registerEvents();
+
+    //floating navbar
+	floatingNav.registerEvents();
 });
+var floatingNav = (function () {
+    var overallTabsContainer, overallContainer;
+    function _setSelectores() {
+        overallTabsContainer = $('.overall-tabs__content');
+        overallContainer = $('#overallContainer');
+    }
+    function registerEvents() {
+        _setSelectores();
+        $(window).scroll(function () {
+                var windowScrollTop = $(window).scrollTop(),
+                    specsTabsOffsetTop = $('.overall-tabs__placeholder').offset().top,
+                    overallContainerHeight = overallContainer.outerHeight(),
+                    topNavBarHeight = overallTabsContainer.height();
+
+                var currentActiveTab;
+
+                if (windowScrollTop > specsTabsOffsetTop) {
+                    overallTabsContainer.addClass('fixed-tab-nav');
+                }
+
+                else if (windowScrollTop < specsTabsOffsetTop) {
+                    overallTabsContainer.removeClass('fixed-tab-nav');
+                }
+
+                if (overallTabsContainer.hasClass('fixed-tab-nav')) {
+                    if (windowScrollTop > Math.ceil(overallContainerHeight) - (topNavBarHeight)) {
+                        overallTabsContainer.removeClass('fixed-tab-nav');
+                    }
+                }
+		
+                $('#overallContainer .overall-tabs-data').each(function () {
+                    var top = $(this).offset().top - topNavBarHeight,
+                        bottom = top + $(this).outerHeight();
+                    if (windowScrollTop >= top && windowScrollTop <= bottom) {
+                        overallTabsContainer.find('li').removeClass('tab--active');
+                        $('#overallContainer .overall-tabs-data').removeClass('tab--active');
+
+                        $(this).addClass('tab--active');
+
+                        currentActiveTab = overallTabsContainer.find('li[data-tabs="' + $(this).attr('data-id') + '"]');
+                        overallTabsContainer.find(currentActiveTab).addClass('tab--active');
+                        //centerItVariableWidth(overallTabsContainer.find('li[data-tabs="' + $(this).attr('data-id') + '"]'), '.overall-tabs__content');
+                        
+
+                    }
+                });
+
+            var tabElementThird = overallContainer.find('.overall-tabs-data:eq(3)'),
+                tabElementSixth = overallContainer.find('.overall-tabs-data:eq(6)'),
+                tabElementNinth = overallContainer.find('.overall-tabs-data:eq(9)'),
+                tabElementTwelve = overallContainer.find('.overall-tabs-data:eq(13)');
+
+                if (tabElementThird.length != 0) {
+                    focusFloatingTab(tabElementThird, 300, 0);
+                }
+
+                if (tabElementSixth.length != 0) {
+                    focusFloatingTab(tabElementSixth, 600, 300);
+                }
+
+                if (tabElementNinth.length != 0) {
+                    focusFloatingTab(tabElementNinth, 900, 600);
+                }
+
+                if (tabElementTwelve.length != 0) {
+                    focusFloatingTab(tabElementTwelve, 1200, 900);
+                }
+
+        });
+
+        $('.overall-tabs__list li').on('click', function () {
+            var target = $(this).attr('data-tabs'),
+                topNavBarHeight = $('.overall-tabs__content').height();
+            $('html, body').animate({ scrollTop: Math.ceil($(".overall-tabs-data[data-id=" + target+"]").offset().top) - topNavBarHeight }, 1000);
+            centerItVariableWidth($(this), '.overall-tabs__content');
+        });
+
+    }
+    function focusFloatingTab(element, startPosition, endPosition) {
+        var windowScrollTop = $(window).scrollTop();
+        if (windowScrollTop > element.offset().top - 45) {
+            if (!overallTabsContainer.hasClass('scrolled-left-' + startPosition)) {
+                overallTabsContainer.addClass('scrolled-left-' + startPosition);
+                scrollHorizontal(startPosition);
+            }
+        }
+
+        else if (windowScrollTop < element.offset().top) {
+            if (overallTabsContainer.hasClass('scrolled-left-' + startPosition)) {
+                overallTabsContainer.removeClass('scrolled-left-' + startPosition);
+                scrollHorizontal(endPosition);
+            }
+        }
+    };
+    function scrollHorizontal(pos) {
+        $('.overall-tabs__content').animate({ scrollLeft: pos - 15 + 'px' }, 500);
+    }
+    return {
+        registerEvents: registerEvents
+    }
+})();
 var interestingFactPopup = (function () {
     var fixedPoupContainer, container, readMoreBtn, closeBtn;
 
@@ -103,3 +207,4 @@ var bodyBackground = {
         $('html, body').scrollTop(-windowScrollTop);
     }
 };
+ 
