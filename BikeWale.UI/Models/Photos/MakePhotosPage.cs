fd7a323da1b403938c4b14b126da4f3fcc5ab608
Rajan Chauhan <rajan.chauhan@carwale.com>
2018-01-29 18:08:45 +0530

    make photos viewmodel and modelpage created

---
+++ BikeWale.UI/Models/Photos/MakePhotosPage.cs
@@ -0,0 +1,96 @@
+﻿using Bikewale.Entities;
+using Bikewale.Entities.BikeData;
+using Bikewale.Entities.CMS;
+using Bikewale.Entities.CMS.Photos;
+using Bikewale.Entities.GenericBikes;
+using Bikewale.Entities.Schema;
+using Bikewale.Models.Photos;
+using Bikewale.Interfaces.BikeData;
+using Bikewale.Notifications;
+using Bikewale.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bikewale.Models.Photos
+{
+    /// <summary>
+    /// Created By  : Rajan Chauhan on 29 Jan 2018
+    /// Description : To bind Model Photos Page 
+    /// </summary>
+    public class MakePhotosPage
+    {
+        private readonly IBikeMakesCacheRepository _objMakeCache = null;
+        private readonly IBikeModels<BikeModelEntity, int> _objModelEntity = null;
+        public StatusCodes Status { get; set; }
+        public MakeMaskingResponse objResponse { get; set; }
+        public string RedirectUrl { get; set; }
+        public uint _makeId;
+        public bool IsMobile { get; set; }
+        
+
+        public MakePhotosPage(bool isMobile, string makeMaskingName, IBikeModels<BikeModelEntity, int> objModelEntity, IBikeMakesCacheRepository objMakeCache)
+        {
+            IsMobile = isMobile;
+            _objModelEntity = objModelEntity;
+            _objMakeCache = objMakeCache;
+            ProcessQuery(makeMaskingName);
+        }
+
+        public MakePhotosPageVM GetData()
+        {
+            MakePhotosPageVM _objData = null;
+            try
+            {
+                _objData = new MakePhotosPageVM();
+                BindModelPhotos(_objData);
+            }
+            catch (Exception ex)
+            {
+                ErrorClass.LogError(ex, "Bikewale.Models.Photos.MakePhotosPage : GetData");
+            }
+            return _objData;
+            
+        }
+
+        private void BindModelPhotos()
+        {
+
+        }
+
+        private void ProcessQuery(string makeMaskingName)
+        {
+            try
+            {
+                objResponse = _objMakeCache.GetMakeMaskingResponse(makeMaskingName);
+                if (objResponse != null)
+                {
+                    Status = (StatusCodes)objResponse.StatusCode;
+                    if (objResponse.StatusCode == 200)
+                    {
+                        _makeId = objResponse.MakeId;
+                    }
+                    else if (objResponse.StatusCode == 301)
+                    {
+                        RedirectUrl = HttpContext.Current.Request.RawUrl.Replace(makeMaskingName, objResponse.MaskingName);
+                        Status = StatusCodes.RedirectPermanent;
+                    }
+                    else
+                    {
+                        Status = StatusCodes.ContentNotFound;
+                    }
+                }
+                else
+                {
+                    Status = StatusCodes.ContentNotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorClass.LogError(ex, string.Format("MakePhotosPage.ProcessQuery() makeMaskingName : {0}", makeMaskingName));
+            }
+        }
+
+    }
+}
---
+++ Bikewale.Models/Photos/MakePhotosPageVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bikewale.Entities.CMS.Photos;
+using Bikewale.Entities.Pager;
+using Bikewale.Entities.GenericBikes;
+
+namespace Bikewale.Models.Photos
+{
+    public class MakePhotosPageVM : ModelBase
+    {
+        public IEnumerable<ModelImages> BikeModelsPhotos { get; set; }
+        public Dictionary<EnumBikeBodyStyles, IEnumerable<int>> ModelBodyStyleArray { get; set; }
+        public string ImagesSynopsis { get; set; }
+    }
+}
