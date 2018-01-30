8a3c56fb209288625a0518f1a5b59e2fcd0ce2e9
cw-sushilkumar <kanojia.sushil@gmail.com>
2018-01-30 22:00:37 +0530

    Distinguish GA pages based in PWA - Distinguish GA pages based in PWA
    Signed-off-by: cw-sushilkumar <kanojia.sushil@gmail.com>

--- Bikewale.PWA.UI/src/utils/constants.js
+++ Bikewale.PWA.UI/src/utils/constants.js
@@ -15,5 +15,6 @@ module.exports = {
 	AD_PATH_NEWS_MOBILE_MIDDLE_320_50 : '/1017752/BikeWale_Mobile_News_Middle_320x50',
 	AD_DIV_REVIEWS_TOP_320_50 : 'div-gpt-ad-1395986297721-0',
 	AD_DIV_REVIEWS_BOTTOM_320_50 : 'div-gpt-ad-1395986297721-1',
-	AD_DIV_REVIEWS_MIDDLE_320_50 : 'div-gpt-ad-1395986297721-5'
+	AD_DIV_REVIEWS_MIDDLE_320_50: 'div-gpt-ad-1395986297721-5',
+	GA_PAGE_MAPPING: [{ "ListPage": { "id": "37", "name": "Editorial_List_Page" } }, { "DetailsPage": { "id": "38", "name": "Editorial_Details_Page" } }, { "VideosPage": { "id": "33", "name": "Videos_Landing_Page" } }, { "VideoDetailsPage": { "id": "36", "name": "Videos_Details_Page" } }, { "VideoCategoryPage": { "id": "45", "name": "Videos_Category_Page" } }]
 }
