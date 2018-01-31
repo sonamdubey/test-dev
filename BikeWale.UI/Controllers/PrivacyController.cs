6131faff5f6dff45def5107ceda2ff0576751d07
Pratibha Verma <pratibha.verma@aepl.com>
2018-01-31 12:11:34 +0530

    Added Controller

---
+++ BikeWale.UI/Controllers/PrivacyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bikewale.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        [HttpGet]
+        public ActionResult PrivacyPolicy()
+        {
+            return View("~/Views/Shared/_PrivacyPolicy.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult VisitorAgreement()
+        {
+            return View("~/Views/Shared/_VisitorAgreement.cshtml");
+        }
+    }
+}
