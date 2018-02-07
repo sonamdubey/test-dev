81a0dbda586a28961256d49978b5edb1acd96ec2
Dhruv Joshi <dhruv.joshi@aepl.com>
2018-02-07 12:44:04 +0530

    Added email body to ComposeBody() method in Bikewale.Notifications.MailTemplates

--- Bikewale.Notifications/MailTemplates/UpcomingBikesSubscription.cs
+++ Bikewale.Notifications/MailTemplates/UpcomingBikesSubscription.cs
@@ -23,7 +23,16 @@ namespace Bikewale.Notifications.MailTemplates
 
         public override string ComposeBody()
         {
-            throw new NotImplementedException();
+            StringBuilder message = new StringBuilder();
+            message.Append("Dear <UserName>,\n\n");
+            message.Append("Thank you for subscribing to BikeWale.\n\n");
+            message.AppendFormat("Glad to see that you have chosen BikeWale to research about your favorite bike. You will receive all the updates about {0} directly in your inbox.\n\n", bikeName);
+            message.Append("And if you like BikeWale, don't forget to download our Android App from google play store-  https://goo.gl/L9XSAu \n\n");
+            message.Append("Cheers!\n");
+            message.Append("Team BikeWale\n\n");
+
+            return message.ToString();
+
         }
    }
 }
