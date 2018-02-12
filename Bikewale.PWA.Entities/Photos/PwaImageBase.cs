7fdf22f68c9716769ac5cac1d2d467b20405a36e
ashu82492 <ashutosh.sharma@carwale.com>
2018-02-12 20:33:37 +0530

    API created for images of bike models for PWA.

---
+++ Bikewale.PWA.Entities/Photos/PwaImageBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.PWA.Entities.Photos
+{
+    /// <summary>
+    /// Created by : Ashutosh Sharma on 12 Feb 2017.
+    /// </summary>
+    [Serializable, DataContract]
+    public class PwaImageBase
+    {
+        [DataMember]
+        public string HostUrl { get; set; }
+        [DataMember]
+        public string OriginalImgPath { get; set; }
+    }
+}
