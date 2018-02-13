3f05a32d8306bcf1550888e8a00616d01df66c44
ashu82492 <ashutosh.sharma@carwale.com>
2018-02-13 10:00:07 +0530

    comments added

--- Bikewale.PWA.Entities/Photos/PwaImageBase.cs
+++ Bikewale.PWA.Entities/Photos/PwaImageBase.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Bikewale.PWA.Entities.Photos
 {
     /// <summary>
-    /// Created by : Ashutosh Sharma on 12 Feb 2017.
+    /// Created by : Ashutosh Sharma on 12 Feb 2018.
     /// </summary>
     [Serializable, DataContract]
     public class PwaImageBase
