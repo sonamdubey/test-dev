c34e0e9a583f0d617a69263d919fdd0901390628
Sanskar Gupta <sanskar.gupta@carwale.com>
2018-02-14 09:41:14 +0530

    - Cache Clear for Dealer Campaigns 	- Added a new class to hold all the functions related to Dealer Campaigns Cache functionality 	- Added ClearDealerBikes function to it. 	- Called that function from ManageDealers.aspx and DealerRules.aspx
    Signed-off-by: Sanskar Gupta <sanskar.gupta@carwale.com>

---
+++ BikewaleOpr.Cache/Campaigns/Dealers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikewaleOpr.Cache.Campaigns
+{
+    /// <summary>
+    /// Created by  : Sanskar Gupta on 13 Feb 2018
+    /// Description : Class which includes all the cache functionality related to Dealer Campaigns
+    /// </summary>
+    public class Dealers
+    {
+        public static bool ClearDealerBikes(int dealerId)
+        {
+            bool isDealerCacheCleared = true;
+            BwMemCache.ClearDealerBikes(Convert.ToUInt32(dealerId));
+            return isDealerCacheCleared;
+        }
+    }
+}
