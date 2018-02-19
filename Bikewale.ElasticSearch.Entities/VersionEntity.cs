cb7be9de01c7d779be37d205ae2ef071f83ba68f
Deepak Israni <deepak.israni@carwale.com>
2018-02-19 15:33:21 +0530

    Added common entities to elastic search project.

---
+++ Bikewale.ElasticSearch.Entities/VersionEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikewale.ElasticSearch.Entities
+{
+    /// <summary>
+    /// Created By : Deepak Israni on 19 Feb 2018
+    /// Description : Entity to store version information.
+    /// </summary>
+    public class VersionEntity
+    {
+        public uint VersionId { get; set; }
+        public string VersionName { get; set; }
+        public IEnumerable<SpecsEntity> Specs { get; set; }
+        public IEnumerable<PriceEntity> PriceList { get; set; }
+    }
+}
