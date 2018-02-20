54e7234fa240578e7a2dcf10aa375b9030dc0edf
Sanskar Gupta <sanskar.gupta@carwale.com>
2018-02-20 09:43:48 +0530

    - Consumer to consume RabbitMQ messages for ElasticSearch document Insertion/Updation.
    Signed-off-by: Sanskar Gupta <sanskar.gupta@carwale.com>

--- Bikewale.ElasticSearch.IndexUpdater/Program.cs
+++ Bikewale.ElasticSearch.IndexUpdater/Program.cs
@@ -1,10 +1,32 @@
-﻿
+﻿using Consumer;
+using System;
+using System.Configuration;
+
 namespace Bikewale.ElasticSearch.IndexUpdaterConsumer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string queueName = ConfigurationManager.AppSettings["QueueName"].ToUpper();
+            string consumerName = ConfigurationManager.AppSettings["ConsumerName"].ToString();
+            string retryCount = ConfigurationManager.AppSettings["RetryCount"];
+            string rabbitMsgTTL = ConfigurationManager.AppSettings["RabbitMsgTTL"];
+            log4net.Config.XmlConfigurator.Configure();
+            try
+            {
+                IndexUpdateConsumer consumer = new IndexUpdateConsumer(queueName, consumerName, retryCount, rabbitMsgTTL);
+                consumer.ProcessMessages();
+                Logs.WriteInfoLog("Started at : " + DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                Logs.WriteErrorLog("Exception : " + ex.Message);
+            }
+            finally
+            {
+                Logs.WriteInfoLog("End at : " + DateTime.Now);
+            }
         }
     }
 }
