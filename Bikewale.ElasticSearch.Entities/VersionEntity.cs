887841b306644004de0d51abc8c41e7ccb13a031
Dhruv Joshi <dhruv.joshi@aepl.com>
2018-02-21 11:16:20 +0530

    Specs Entity deleted instead minspecs stored as individual properties in top version entity

--- Bikewale.ElasticSearch.Entities/VersionEntity.cs
+++ Bikewale.ElasticSearch.Entities/VersionEntity.cs
@@ -12,13 +12,19 @@ namespace Bikewale.ElasticSearch.Entities
     /// Modified by: Dhruv Joshi
     /// Dated: 20th Feb 2018
     /// Description: Added flag for version status
+    /// Modified by: Dhruv Joshi
+    /// Dated: 21st Feb 2018
+    /// Description: Added minspecs as individual properties instead of an entity
     /// </summary>
     public class VersionEntity
     {
 
         public uint VersionId { get; set; }
         public string VersionName { get; set; }
-        public IEnumerable<SpecsEntity> Specs { get; set; }
+        public uint Mileage { get; set; }
+        public uint KerbWeight { get; set; }
+        public double Power { get; set; }
+        public double Displacement { get; set; }
         public IEnumerable<PriceEntity> PriceList { get; set; }
         public BikeStatus VersionStatus { get; set; }
 
