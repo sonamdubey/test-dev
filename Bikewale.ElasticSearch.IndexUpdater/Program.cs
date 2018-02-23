d6f56e763d99e51594e010d61ae91072c80a4a86
Sanskar Gupta <sanskar.gupta@carwale.com>
2018-02-23 14:42:50 +0530

    - Code Optimized and comments added.
    Signed-off-by: Sanskar Gupta <sanskar.gupta@carwale.com>

--- Bikewale.ElasticSearch.IndexUpdater/Program.cs
+++ Bikewale.ElasticSearch.IndexUpdater/Program.cs
@@ -18,13 +18,32 @@ namespace Bikewale.ElasticSearch.IndexUpdaterConsumer
     {
 
         private static ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().Name);
-        private static ElasticClient client = ElasticSearchInstance.GetInstance();
+
+        private const string ES_OPERATION_INSERT = "insert";
+        private const string ES_OPERATION_UPDATE = "update";
+        private const string ES_OPERATION_DELETE = "delete";
+
+        private static ElasticClient client;
+        private static int NO_OF_RETRIES;
+
+        static Program(){
+            try
+            {
+                client = ElasticSearchInstance.GetInstance();
+                NO_OF_RETRIES = Convert.ToInt32(ConfigurationManager.AppSettings["RetryCount"]);
+            }
+            catch (Exception e)
+            {
+                logger.Error("An Error Occured while trying to get ES Instance / fetching NO_OF_RETRIES from AppSettings", e);
+            }
+        }
+
         static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
             try
             {
-                Logs.WriteInfoLog("Queue Started at : " + DateTime.Now);
+                Logs.WriteInfoLog("Consumer Started at : " + DateTime.Now);
 
                 RabbitMqPublishing.RabbitMqPublish _RabbitMQPublishing = new RabbitMqPublishing.RabbitMqPublish();
 
@@ -37,13 +56,13 @@ namespace Bikewale.ElasticSearch.IndexUpdaterConsumer
             }
             finally
             {
-                logger.Info("Queue Ended at : " + DateTime.Now);
+                logger.Info("Consumer Ended at : " + DateTime.Now);
             }
         }
 
         /// <summary>
         /// Function responsible for receiving the message from queue.
-        /// This function should return `false` for the message to be published to DeadLetterQueue and `true` otherwise.
+        /// This function should return `false` for the message to be published to DeadLetterQueue and `true` when the the message should be acknowledged/rejected.
         /// </summary>
         /// <param name="nvc"></param>
         /// <returns></returns>
@@ -85,63 +104,73 @@ namespace Bikewale.ElasticSearch.IndexUpdaterConsumer
         /// <returns></returns>
         private static bool InsertOrUpdateDocumentInIndex(NameValueCollection queueMessage, int c = 0)
         {
-            if (c >= 3) {
+            if (c >= NO_OF_RETRIES) {
                 return true;
             }
 
             string indexName = queueMessage["indexName"];
             string documentType = queueMessage["documentType"];
             string documentJson = null;
-
-            if (!string.Equals(queueMessage["operationType"], "delete", StringComparison.OrdinalIgnoreCase))
-            {
-                documentJson = queueMessage["documentJson"];
-            }
-
             string documentId = queueMessage["documentId"];
-
             string operationType = queueMessage["operationType"];
-
-
             bool isOperationSuccessful = false;
+            string esResponse = string.Empty;
 
-            string esResponse = "System didn't interact with ES.";
+            logger.Info(string.Format("IndexName : {0}, DocumentId = {1}, DocumentType : {2}, OperationType : {3}", indexName, documentId, documentType, operationType));
+
+            if (!string.Equals(operationType, ES_OPERATION_DELETE, StringComparison.OrdinalIgnoreCase))
+            {
+                if (queueMessage["documentJson"] != null)
+                {
+                    documentJson = queueMessage["documentJson"];
+                }
+                else
+                {
+                    logger.Error(string.Format("ERROR : Document JSON Not Passed, OperationType = {0}", operationType));
+                    return true;
+                }
+            }
 
             try
             {
 
                 switch (operationType)
                 {
-                    case ("delete"):
+                    case ES_OPERATION_DELETE:
                         bool doesDocumentExist = client.DocumentExists(new Nest.DocumentExistsRequest(indexName, documentType, documentId)).Exists;
                         if (!doesDocumentExist)
                         {
-                            logger.Error(string.Format("IndexName : {0}, DocumentId = {1}, OperationType = {2}, Document Does Not Exist", indexName, documentId, operationType));
+                            logger.Error(string.Format("ERROR : Document Does Not Exist, OperationType = {0}", operationType));
                             isOperationSuccessful = true;
                         }
                         else
                         {
                             var esDeleteResponse = client.Delete(new DeleteRequest(indexName, documentType, documentId));
-                            isOperationSuccessful = esDeleteResponse.IsValid;
-                            esResponse = esDeleteResponse.DebugInformation;
+                            if (esDeleteResponse != null) { 
+                                isOperationSuccessful = esDeleteResponse.IsValid;
+                                esResponse = esDeleteResponse.DebugInformation;
+                            }
                         }
                         break;
 
-                    case ("update"):
+                    case ES_OPERATION_UPDATE:
 
-                    case ("insert"):
+                    case ES_OPERATION_INSERT:
                         JObject documentJObject = JObject.Parse(documentJson);
                         var esIndexResponse = client.Index(documentJObject, i => i
                                       .Index(indexName)
                                       .Type(documentType)
                                       .Id(documentId)
                                       );
-                        isOperationSuccessful = esIndexResponse.IsValid;
-                        esResponse = esIndexResponse.DebugInformation;
+                        if (esIndexResponse != null)
+                        {
+                            isOperationSuccessful = esIndexResponse.IsValid;
+                            esResponse = esIndexResponse.DebugInformation;
+                        }
                         break;
 
                     default:
-                        logger.Info(string.Format("IndexName : {0}, DocumentId = {1}, Message : Unsupported operationType", indexName, documentId));
+                        logger.Error(string.Format("ERROR. Message : Unsupported operationType_{0}", operationType));
                         break;
                 }
 
@@ -149,17 +178,16 @@ namespace Bikewale.ElasticSearch.IndexUpdaterConsumer
             catch (Exception e)
             {
                 logger.Error("Exception occured while updating the document", e);
-                isOperationSuccessful = InsertOrUpdateDocumentInIndex(queueMessage, ++c);
             }
             finally
             {
                 if (isOperationSuccessful)
                 {
-                    logger.Info(string.Format("IndexName : {0}, DocumentId = {1}, OperationType : {2}, EsResponse : {3}", indexName, documentId, operationType, esResponse));
+                    logger.Info(string.Format("ES Operation Successfully processed, Response : {0}", esResponse));
                 }
                 else
                 {
-                    logger.Error(string.Format("IndexName : {0}, DocumentId = {1}, OperationType : {2}, EsResponse : {3}", indexName, documentId, operationType, esResponse));
+                    logger.Error(string.Format("Error! ES Operation can't be processed, Response : {0}", esResponse));
                     isOperationSuccessful = InsertOrUpdateDocumentInIndex(queueMessage, ++c);
                 }
             }
