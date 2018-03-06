bf80be79a2f38f64e02e6cd5d856d826075cbc79
Pratibha Verma <pratibha.verma@aepl.com>
2018-03-06 19:22:17 +0530

    Reversed AutoExpo Category on News

--- Bikewale.PWA.UI/src/components/News/NewsCommon.js
+++ Bikewale.PWA.UI/src/components/News/NewsCommon.js
@@ -45,7 +45,7 @@ function extractPageCategoryFromURL() {
 }
 
 function isReactCategory(categoryName) {
-	var allowedCategoriesInReact = ["NEWS", "EXPERT REVIEWS"];
+	var allowedCategoriesInReact = ["NEWS", "EXPERT REVIEWS", "AutoExpo 2018"];
 	if (allowedCategoriesInReact.indexOf(categoryName) !== -1) {
 		return true;
 	}
