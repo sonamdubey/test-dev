b5bcdaede7ca519f349c3a330f93d688d9f2e40d
Ashish Kamble <ashish.kamble@carwale.com>
2018-03-13 17:59:04 +0530

    New AdTest page added to bikewale projetct
    - Created new controller for ad test page
    - Created desktop adtest page
    
    Signed-off-by: Ashish Kamble <ashish.kamble@carwale.com>

---
+++ BikeWale.UI/Controllers/AdTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bikewale.Models;
+
+namespace Bikewale.Controllers
+{
+    public class AdTestController : Controller
+    {
+        // GET: AdTest
+        public ActionResult Index()
+        {
+			ModelBase objBase = new ModelBase();			
+
+			return View(objBase);
+        }
+    }
+}
