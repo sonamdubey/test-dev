5ce961f454bb7bddfc52c2812d4a328301911c08
Pratibha Verma <pratibha.verma@aepl.com>
2018-03-27 20:20:33 +0530

    commom method created to bind MinSpecs

--- BikeWale.UI/Models/BestBikes/BestBikeByBodyStyle.cs
+++ BikeWale.UI/Models/BestBikes/BestBikeByBodyStyle.cs
@@ -37,45 +37,13 @@ namespace Bikewale.Models
                 objData = new BestBikeByCategoryVM();
 
                 objData.objBestScootersList = FetchBestBikesList(EnumBikeBodyStyles.Scooter);
-                if (objData.objBestScootersList != null && objData.objBestScootersList.Any())
-                {
-                    IEnumerable<VersionMinSpecsEntity> scooterVersionMinSpecs = SpecsFeaturesServiceGateway.GetVersionsMinSpecs(objData.objBestScootersList.Select(m => m.VersionId));
-                    foreach (var bestScooter in objData.objBestScootersList)
-                    {
-                        VersionMinSpecsEntity scooterMinSpecs = scooterVersionMinSpecs.FirstOrDefault(x => x.VersionId.Equals(bestScooter.VersionId));
-                        bestScooter.MinSpecsList = scooterMinSpecs != null ? scooterMinSpecs.MinSpecsList : null;
-                    }
-                }
+                BindMinSpecs(objData.objBestScootersList);
                 objData.objBestSportsBikeList = FetchBestBikesList(EnumBikeBodyStyles.Sports);
-                if (objData.objBestSportsBikeList != null && objData.objBestSportsBikeList.Any())
-                {
-                    IEnumerable<VersionMinSpecsEntity> sportsVersionMinSpecs = SpecsFeaturesServiceGateway.GetVersionsMinSpecs(objData.objBestSportsBikeList.Select(m => m.VersionId));
-                    foreach (var bestSports in objData.objBestSportsBikeList)
-                    {
-                        VersionMinSpecsEntity sportsMinSpecs = sportsVersionMinSpecs.FirstOrDefault(x => x.VersionId.Equals(bestSports.VersionId));
-                        bestSports.MinSpecsList = sportsMinSpecs != null ? sportsMinSpecs.MinSpecsList : null;
-                    }
-                }
+                BindMinSpecs(objData.objBestSportsBikeList);
                 objData.objBestCruiserBikesList = FetchBestBikesList(EnumBikeBodyStyles.Cruiser);
-                if (objData.objBestCruiserBikesList != null && objData.objBestCruiserBikesList.Any())
-                {
-                    IEnumerable<VersionMinSpecsEntity> cruiserVersionMinSpecs = SpecsFeaturesServiceGateway.GetVersionsMinSpecs(objData.objBestCruiserBikesList.Select(m => m.VersionId));
-                    foreach (var bestCruiser in objData.objBestCruiserBikesList)
-                    {
-                        VersionMinSpecsEntity cruiserMinSpecs = cruiserVersionMinSpecs.FirstOrDefault(x => x.VersionId.Equals(bestCruiser.VersionId));
-                        bestCruiser.MinSpecsList = cruiserMinSpecs != null ? cruiserMinSpecs.MinSpecsList : null;
-                    }
-                }
+                BindMinSpecs(objData.objBestCruiserBikesList);
                 objData.objBestMileageBikesList = FetchBestBikesList(EnumBikeBodyStyles.Mileage);
-                if (objData.objBestMileageBikesList != null && objData.objBestMileageBikesList.Any())
-                {
-                    IEnumerable<VersionMinSpecsEntity> mileageVersionMinSpecs = SpecsFeaturesServiceGateway.GetVersionsMinSpecs(objData.objBestMileageBikesList.Select(m => m.VersionId));
-                    foreach (var bestMileage in objData.objBestMileageBikesList)
-                    {
-                        VersionMinSpecsEntity mileageMinSpecs = mileageVersionMinSpecs.FirstOrDefault(x => x.VersionId.Equals(bestMileage.VersionId));
-                        bestMileage.MinSpecsList = mileageMinSpecs != null ? mileageMinSpecs.MinSpecsList : null;
-                    }
-                }
+                BindMinSpecs(objData.objBestMileageBikesList);
             }
             catch (Exception ex)
             {
@@ -84,7 +52,34 @@ namespace Bikewale.Models
             return objData;
         }
 
+        /// <summary>
+        /// Created By : Pratibha Verma on 27 Mar 2018
+        /// Summary : Bind MinSpecs to Generic Bike List
+        /// </summary>
+        private void BindMinSpecs(IEnumerable<BestBikeEntityBase> GenericBikeList)
+        {
+            try
+            {
+                if (GenericBikeList != null && GenericBikeList.Any())
+                {
+                    IEnumerable<VersionMinSpecsEntity> versionMinSpecs = SpecsFeaturesServiceGateway.GetVersionsMinSpecs(GenericBikeList.Select(m => m.VersionId));
+                    foreach (var genericBike in GenericBikeList)
+                    {
+                        VersionMinSpecsEntity minSpecs = versionMinSpecs.FirstOrDefault(x => x.VersionId.Equals(genericBike.VersionId));
+                        if (minSpecs != null)
+                        {
+                            genericBike.MinSpecsList = minSpecs.MinSpecsList;
+                        }
+                    }
+                }
 
+            }
+            catch (Exception ex)
+            {
+                ErrorClass.LogError(ex, string.Format("Bikewale.Models.BestBikeByBodyStyle.BindMinSpecs()"));
+            }
+
+        }
         /// <summary>
         /// Created By :- Subodh Jain 18 May 2017
         /// Summary :- Generic Bike Model FetchBestBikesList;
