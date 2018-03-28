86c9f00fbd29852fc35881908eea7525e4af711a
Deepak Israni <deepak.israni@carwale.com>
2018-03-28 13:49:41 +0530

    Added targeting for model, make and city on model images page.

--- BikeWale.UI/Models/Photos/PhotosPage.cs
+++ BikeWale.UI/Models/Photos/PhotosPage.cs
@@ -32,6 +32,7 @@ namespace Bikewale.Models.Photos
         private readonly IVideos _objVideos = null;
 
         private uint _modelId, _cityId;
+        private string _cityName;
         private PhotosPageVM _objData = null;
         private string _returnUrl;
         private uint _selectedColorImageId;
@@ -92,6 +93,7 @@ namespace Bikewale.Models.Photos
                 _objData.BodyStyle = 0;
                 GlobalCityAreaEntity currentCityArea = GlobalCityArea.GetGlobalCityArea();
                 _cityId = currentCityArea.CityId;
+                _cityName = currentCityArea.City;
                 ProcessQueryStringVariables(qstr);
                 _objData.Series = _objModelEntity.GetSeriesByModelId(_modelId);
                 BindPhotos();
@@ -287,6 +289,14 @@ namespace Bikewale.Models.Photos
 
                     SetBreadcrumList();
                     SetPageJSONLDSchema(_objData.PageMetaTags);
+
+                    _objData.AdTags.TargetedModel = _objData.Model.ModelName;
+                    _objData.AdTags.TargetedMakes = _objData.Make.MakeName;
+
+                    if (!String.IsNullOrEmpty(_cityName))
+                    {
+                        _objData.AdTags.TargetedCity = _cityName;
+                    }
 
                 }
             }
