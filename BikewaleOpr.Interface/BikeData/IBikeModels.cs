be42c2a823781aba950c63187e84409c3822016c
cw-ashutoshsharma <ashutosh.sharma@carwale.com>
2018-04-03 16:29:21 +0530

    API created to partially update es index when specs is updated.

--- BikewaleOpr.Interface/BikeData/IBikeModels.cs
+++ BikewaleOpr.Interface/BikeData/IBikeModels.cs
@@ -6,12 +6,15 @@ namespace BikewaleOpr.Interface.BikeData
 {
     /// <summary>
     /// Created by : Sajal Gupta on 09-03-2017
-    /// Description: Added GetPendingUsedBikesWithoutModelImage method;
+    /// Description: Added GetPendingUsedBikesWithoutModelImage method.
+    /// Modified by : Ashutosh Sharma on 02 Apr 2018.
+    /// Description : Added UpdateMinSpecsInESIndex to update verson min specs in elastic search index.
     /// </summary>
     public interface IBikeModels
     {
         UsedBikeImagesByMakeNotificationData GetPendingUsedBikesWithoutModelImage();
         IEnumerable<BikeModelsByMake> GetModelsWithMissingColorImage();
         void UpdateModelESIndex(string ids, string operation);
+        void UpdateMinSpecsInESIndex(int versionId, IEnumerable<SpecsItem> specItemList);
     }
 }
---
+++ BikewaleOpr.Service/Controllers/NewBikeSpecificationController.cs
@@ -0,0 +1,50 @@
+﻿using BikewaleOpr.Entity.BikeData;
+using BikewaleOpr.Interface.BikeData;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace BikewaleOpr.Service.Controllers
+{
+    /// <summary>
+    /// Created by : Ashutosh Sharma on 01 Ape 2018
+    /// Description : Provide APIs for bike specifications and features.
+    /// </summary>
+    public class NewBikeSpecificationController : ApiController
+    {
+        private readonly IBikeModels _IBikeModels;
+        public NewBikeSpecificationController(IBikeModels bikeModels)
+        {
+            _IBikeModels = bikeModels;
+        }
+        
+        /// <summary>
+        /// Created by : Ashutosh Sharma on 02 Apr 2018
+        /// Description : API to update bike min specs in elastic search index when specs are updated for a version.
+        /// </summary>
+        /// <param name="versionId">Version Id for which min specs to be updated in elastic index.</param>
+        /// <param name="specItemList">List of specs items with updated values which need to updated in ealstic index.</param>
+        /// <returns></returns>
+        [HttpPost, Route("api/bikespecs/versions/{versionId}/")]
+        public IHttpActionResult UpdateSpecsInESIndex(int versionId, [FromBody]IEnumerable<SpecsItem> specItemList)
+        {
+            try
+            {
+                if (versionId > 0 && specItemList != null)
+                {
+                    _IBikeModels.UpdateMinSpecsInESIndex(versionId, specItemList);
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
