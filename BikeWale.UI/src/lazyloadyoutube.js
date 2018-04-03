3f1b0de2d793ba299a805476b0ece032454f7404
Deepak Israni <deepak.israni@carwale.com>
2018-04-03 14:07:19 +0530

    Restructuring of lazyloadyoutube.js to follow object oriented standards.

--- BikeWale.UI/src/lazyloadyoutube.js
+++ BikeWale.UI/src/lazyloadyoutube.js
@@ -1,47 +1,76 @@
-﻿var youtubeVideos = document.querySelectorAll(".bw-youtube");
-var _videosLoaded = false;
-var loadVideosAt = 0.4;
-var body = document.body, html = document.documentElement;
-var pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+﻿var lazyloadYoutube = {
 
-for (var i = 0; i < youtubeVideos.length; i++) {
-    var ytVideoId = youtubeVideos[i].dataset.embed;
-    var source = "https://img.youtube.com/vi/" + ytVideoId + "/sddefault.jpg";
-
-    var image = new Image();
-    image.src = source;
-    image.addEventListener("load", function () {
-        youtubeVideos[i].appendChild(image);
-    }(i));
-}
+    _videosLoaded: false,
+    loadVideosAt: 0.4,
+    body: document.body,
+    html: document.documentElement,
+    imgSource: "https://img.youtube.com/vi/",
+    imgQuality: "/sddefault.jpg",
+    videoSource: "https://www.youtube.com/embed/",
 
 
-function getScrollPercent() {
-    var scrollTop = html.scrollTop;
-    var scrollPercentage = (scrollTop / pageHeight);
-    return scrollPercentage;
-}
+    getAllVideoSlots: function() {
+        return document.querySelectorAll(".bw-youtube");
+    },
 
+    getPageHeight: function() {
+        return Math.max(lazyloadYoutube.body.scrollHeight, lazyloadYoutube.body.offsetHeight, lazyloadYoutube.html.clientHeight, lazyloadYoutube.html.scrollHeight, lazyloadYoutube.html.offsetHeight);
+    },
 
-function loadYoutubeVideos() {
-    for (var i = 0; i < youtubeVideos.length; i++) {
-        var ytVideo = youtubeVideos[i];
-        var ytVideoId = ytVideo.dataset.embed;
+    setVideoPreviews: function () {
+        var youtubeVideos = lazyloadYoutube.getAllVideoSlots();
+        var noOfVideoSlots = youtubeVideos.length;
 
+        for (var i = 0; i < noOfVideoSlots; i++) {
+
+            var ytVideoId = youtubeVideos[i].dataset.embed;
+
+            var source = lazyloadYoutube.imgSource + ytVideoId + lazyloadYoutube.imgQuality;
+
+            var image = new Image();
+            image.src = source;
+            image.addEventListener("load", function () {
+                youtubeVideos[i].appendChild(image);
+            }(i));
+        }
+    },
+
+    getScrollPercent: function () {
+        var scrollTop = lazyloadYoutube.html.scrollTop;
+        var scrollPercentage = (scrollTop / lazyloadYoutube.getPageHeight());
+        return scrollPercentage;
+    },
+
+    generateIFrame: function (videoId) {
         var iframe = document.createElement("iframe");
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("allowfullscreen", "");
-        iframe.setAttribute("src", "https://www.youtube.com/embed/" + ytVideoId + "?rel=0&showinfo=0&autoplay=0");
-        ytVideo.innerHTML = "";
-        ytVideo.appendChild(iframe);
+        iframe.setAttribute("src", lazyloadYoutube.videoSource + videoId + "?rel=0&showinfo=0&autoplay=0");
+        return iframe;
+    },
+
+    loadYoutubeVideos: function () {
+
+        var youtubeVideos = lazyloadYoutube.getAllVideoSlots();
+
+        for (var i = 0; i < youtubeVideos.length; i++) {
+            var ytVideo = youtubeVideos[i];
+            var ytVideoId = ytVideo.dataset.embed;
+            var iframe = lazyloadYoutube.generateIFrame(ytVideoId);
+            //ytVideo.innerHTML = "";
+            ytVideo.appendChild(iframe);
+        }
+
+        lazyloadYoutube._videosLoaded = true;
     }
-    _videosLoaded = true;
 }
 
+docReady(lazyloadYoutube.setVideoPreviews());
+
 window.addEventListener("scroll", function (event) {
-    if (!_videosLoaded) {
-        if (getScrollPercent() > loadVideosAt) {
-            loadYoutubeVideos();
+    if (!lazyloadYoutube._videosLoaded) {
+        if (lazyloadYoutube.getScrollPercent() > lazyloadYoutube.loadVideosAt) {
+            lazyloadYoutube.loadYoutubeVideos();
         }
     }
 });
