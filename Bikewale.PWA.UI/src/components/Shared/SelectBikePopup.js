b5936405c4b4b114e85ead7ed89c49afcc7499c6
dutiyesh <dutiyesh.salunkhe@carwale.com>
2018-04-16 10:54:17 +0530

    Add autocomplete field - Add autocomplete field

--- Bikewale.PWA.UI/src/components/Shared/SelectBikePopup.js
+++ Bikewale.PWA.UI/src/components/Shared/SelectBikePopup.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import Autocomplete from '../Autocomplete';
 import Accordion from '../Shared/Accordion';
 
 class SelectBikePopup extends React.Component {
   constructor(props) {
     super(props);
-    
+
+    this.state = {
+      isFocus: false
+    }
+
     this.getList = this.getList.bind(this);
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.setReference = this.setReference.bind(this);
   }
 
   getList() {
@@ -42,6 +49,18 @@ class SelectBikePopup extends React.Component {
       {
         makeId: 8,
         makeName: "KTM"
+      },
+      {
+        makeId: 9,
+        makeName: "Yamaha"
+      },
+      {
+        makeId: 10,
+        makeName: "Suzuki"
+      },
+      {
+        makeId: 11,
+        makeName: "KTM"
       }
     ];
 
@@ -71,19 +90,53 @@ class SelectBikePopup extends React.Component {
     )
   }
 
+  handleInputFocus() {
+    let popupSearchBox = this.popupContent.querySelector('.select-bike__search-box');
+
+    this.popupContent.scrollTo(0, popupSearchBox.offsetTop);
+
+    this.setState({
+      isFocus: true
+    })
+  }
+
+  setReference(ref) {
+    this.popupContent = ref;
+  }
+
   render() {
-    let {
+    const {
       isActive
     } = this.props
+    
+    const {
+      isFocus
+    } = this.state
 
     const popupActiveClassName = isActive ? 'select-bike-popup--active' : ''
+    const fixedHeader = isFocus ? 'select-bike-head--fixed' : ''
+    const popupClasses = `select-bike-popup ${popupActiveClassName} ${fixedHeader}`
 
     return (
-      <div className={"select-bike-popup " + popupActiveClassName}>
-        <div className="select-bike-popup__content">
+      <div className={popupClasses}>
+        <div ref={this.setReference} className="select-bike-popup__content">
           <div className="select-bike__head">
             <div className="select-bike-head__content">
               <span className="select-bike__close"></span>
+              <div className="select-bike__search-box">
+                <p className="select-bike-search__title">Select Make and Model</p>
+                <div className="autocomplete-box">
+                  <div className="autocomplete-field">
+                    <Autocomplete
+                      inputProps={{
+                        className: "form-control",
+                        placeholder: "Type to select Make and Model"
+                      }}
+                      onClick={this.handleInputFocus}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="select-bike__body select-bike__accordion">
@@ -91,9 +144,9 @@ class SelectBikePopup extends React.Component {
               {this.getList()}
             </Accordion>
           </div>
-          <div className="select-bike__footer">
+          {/*<div className="select-bike__footer">
             <span className="select-bike__next">Next</span>
-          </div>
+          </div>*/}
         </div>
       </div>
     );
