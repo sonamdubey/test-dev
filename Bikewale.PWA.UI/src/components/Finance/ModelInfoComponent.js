26b05d34c9bdd7bf749a5a9038f5dac592611212
dutiyesh <dutiyesh.salunkhe@carwale.com>
2018-04-26 14:20:21 +0530

    Add version dropdown - Add version dropdown

--- Bikewale.PWA.UI/src/components/Finance/ModelInfoComponent.js
+++ Bikewale.PWA.UI/src/components/Finance/ModelInfoComponent.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Dropdown from 'react-dropdown';
 
 class ModelInfo extends React.Component {
   constructor(props) {
     super(props);
+  }
+  
+  handleVersionChange = (option) => {
+    // handle version change
   }
 
   render() {
@@ -31,9 +36,16 @@ class ModelInfo extends React.Component {
           <span onClick={openSelectBikePopup} className="model-info-card__edit"></span>
         </div>
         <div className="model-info-card__body">
-          <div className="model-info__col">
+          <div className="model-info__col model-info-col--dropdown">
             <p className="model-info-col__label">Version</p>
-            <p className="model-info-col__value">Standard</p>
+            <Dropdown
+              options={model.version}
+              value={model.version[0]}
+              placeholder="Version"
+              placeholderClassName="model-info-col__value"
+              arrowClassName="dropdown-version__arrow"
+              onChange={this.handleVersionChange}
+            />
           </div>
           <div className="model-info__col">
             <p className="model-info-col__label">City</p>
