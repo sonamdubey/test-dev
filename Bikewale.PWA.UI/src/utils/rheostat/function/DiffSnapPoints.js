4ae2a3b128fdc65cc35af2d4a00006fff838ba37
sonam.dubey <sonam.dubey@carwale.com>
2018-05-02 16:54:46 +0530

    removed commented code

--- Bikewale.PWA.UI/src/utils/rheostat/function/DiffSnapPoints.js
+++ Bikewale.PWA.UI/src/utils/rheostat/function/DiffSnapPoints.js
@@ -4,9 +4,6 @@ export const createNewSnapPoints = (algoObj) => {
     for(let i=algoObj.startPoint;i<=algoObj.endPoint;i += algoObj.difference) {
         snapPoints.push(i);
     }
-    // if ((algoObj.endPoint-algoObj.startPoint)%algoObj.difference !== 0){
-    //     snapPoints.push(endPoint);
-    // }
 
     return snapPoints;
 }
