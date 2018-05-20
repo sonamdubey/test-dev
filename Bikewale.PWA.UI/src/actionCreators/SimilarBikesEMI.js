772bc1a4a5cf1c7f28f620b52dbe116dd4192606
cw-ashutoshsharma <ashutosh.sharma@carwale.com>
2018-05-20 20:29:18 +0530

    Bikes with similar emi widget on finance page done

--- Bikewale.PWA.UI/src/actionCreators/SimilarBikesEMI.js
+++ Bikewale.PWA.UI/src/actionCreators/SimilarBikesEMI.js
@@ -1,11 +1,49 @@
 import { similarBikesEMI } from '../actionTypes/SimilarBikesEMI'
+import { getGlobalCity } from '../utils/popUpUtils'
 
-const fetchSimilarBikesData = () => {
-  return {
-    type: similarBikesEMI.FETCH_SIMILAR_BIKES_EMI
+import { fromJS } from 'immutable'
+const getSimilarBikesData = (dispatch, modelEmiObj) => {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        let bikeList = JSON.parse(xhr.responseText);
+        let payload =  {
+          data: bikeList.map(json => ({
+            makeId: json.makeId,
+            makeName: json.makeName,
+            modelId: json.modelId,
+            modelName: json.modelName,
+            modelImage: json.hostUrl + '310x174' +json.originalImagePath,
+            onRoadPriceAmount: json.onRoadPrice
+          })),
+          modelEmiObj
+        }
+        dispatch(
+          {
+            type: similarBikesEMI.FETCH_SIMILAR_BIKES_EMI
+          , payload})
+      }
+      else {
+        return null;
+      }
+    }
   }
+  let url = '/api/pwa/similarbikes/model/' + modelEmiObj.modelId + '/finance/?topCount=9&cityId='+getGlobalCity().id;
+  xhr.open('GET', url);
+  xhr.send();
+
 }
 
-export const fetchSimilarBikes = () => (dispatch) => {
-  dispatch(fetchSimilarBikesData())
+export const fetchSimilarBikes = (modelEmiObj) => (dispatch) => {
+    getSimilarBikesData(dispatch, modelEmiObj)
+}
+export const updateSimilarBikesEmi = (similarBikesList, modelEmiObj) => (dispatch) => {
+    dispatch({ 
+      type: similarBikesEMI.UPDATE_EMI,
+      payload:{
+        similarBikesList,
+        modelEmiObj
+      } 
+     })
 }
