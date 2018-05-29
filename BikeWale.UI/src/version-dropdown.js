1792916ab149b52bd9de8ed7e26ddf1b59540e6e
mayurisakpal <mayuri.sakpal@carwale.com>
2018-05-29 19:00:03 +0530

    code review changes

--- BikeWale.UI/src/version-dropdown.js
+++
@@ -1,4 +0,0 @@
-﻿$('#version').on('click', function () {
-    $(".version-dropdown__submenu, .version-dropdown__tabs").toggleClass("version-active");
-    $('html, body').animate({ scrollTop: $('#version').offset().top - 44 }, 'slow');
-});
