13989755ffcc3cca927b040d61deeeba7a55cb65
snehal.dange <snehal.dange@carwale.com>
2018-05-29 14:33:28 +0530

    Implement the static html changes on capital first pages - Implemented pan city mapping for app
    Signed-off-by: snehal.dange <snehal.dange@carwale.com>

--- Bikewale.Service/AutoMappers/ManufacturerCampaign/ManufacturerCampaignMapper.cs
+++ Bikewale.Service/AutoMappers/ManufacturerCampaign/ManufacturerCampaignMapper.cs
@@ -112,7 +112,7 @@ namespace Bikewale.Service.AutoMappers.ManufacturerCampaign
                 {
                     LeadCampaign.LoanAmount = (uint)(System.Convert.ToUInt32(price) * 0.8);
 
-                    LeadCampaign.PageUrl = string.Format("{8}/m/finance/capitalfirst/?campaingid={0}&amp;dealerid={1}&amp;pqid={2}&amp;leadsourceid={3}&amp;versionid={4}&amp;url=&amp;platformid={5}&amp;bike={6}&amp;loanamount={7}", LeadCampaign.CampaignId, LeadCampaign.DealerId, pqId, LeadCampaign.LeadSourceId, versionId, 3, LeadCampaign.BikeName, LeadCampaign.LoanAmount, BWConfiguration.Instance.BwHostUrl);
+                    LeadCampaign.PageUrl = string.Format("{8}/m/finance/capitalfirst/?campaingid={0}&amp;dealerid={1}&amp;pqid={2}&amp;leadsourceid={3}&amp;versionid={4}&amp;url=&amp;platformid={5}&amp;bike={6}&amp;loanamount={7}&amp;cityid={8}", LeadCampaign.CampaignId, LeadCampaign.DealerId, pqId, LeadCampaign.LeadSourceId, versionId, 3, LeadCampaign.BikeName, LeadCampaign.LoanAmount, BWConfiguration.Instance.BwHostUrl, Bikewale.Utility.GlobalCityArea.GetGlobalCityArea().CityId);
                 }
                 else
                 {
--- Bikewale.Service/AutoMappers/Model/ModelMapper.cs
+++ Bikewale.Service/AutoMappers/Model/ModelMapper.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Bikewale.BAL.ApiGateway.Entities.BikeData;
 using Bikewale.DTO.BikeData;
 using Bikewale.DTO.Campaign;
 using Bikewale.DTO.CMS.Articles;
@@ -875,7 +874,7 @@ namespace Bikewale.Service.AutoMappers.Model
 
                     if (LeadCampaign.DealerId == Bikewale.Utility.BWConfiguration.Instance.CapitalFirstDealerId)
                     {
-                        LeadCampaign.PageUrl = String.Format("{8}/m/finance/capitalfirst/?campaingid={0}&amp;dealerid={1}&amp;pqid={2}&amp;leadsourceid={3}&amp;versionid={4}&amp;url=&amp;platformid={5}&amp;bike={6}&amp;loanamount={7}", LeadCampaign.CampaignId, LeadCampaign.DealerId, 0, LeadCampaign.LeadSourceId, pqEntity.VersionList.FirstOrDefault().VersionId, platformId, LeadCampaign.BikeName, LeadCampaign.LoanAmount, BWConfiguration.Instance.BwHostUrl);
+                        LeadCampaign.PageUrl = String.Format("{8}/m/finance/capitalfirst/?campaingid={0}&amp;dealerid={1}&amp;pqid={2}&amp;leadsourceid={3}&amp;versionid={4}&amp;url=&amp;platformid={5}&amp;bike={6}&amp;loanamount={7}&amp;cityid={8}", LeadCampaign.CampaignId, LeadCampaign.DealerId, 0, LeadCampaign.LeadSourceId, pqEntity.VersionList.FirstOrDefault().VersionId, platformId, LeadCampaign.BikeName, LeadCampaign.LoanAmount, BWConfiguration.Instance.BwHostUrl, Bikewale.Utility.GlobalCityArea.GetGlobalCityArea().CityId);
                     }
                     else
                     {
@@ -1036,7 +1035,7 @@ namespace Bikewale.Service.AutoMappers.Model
         /// <returns></returns>
         private static string GetCategoryDisplayName(int currentIndex)
         {
-            if(currentIndex >=0 && currentIndex <_categoryDisplayNameList.Count )
+            if (currentIndex >= 0 && currentIndex < _categoryDisplayNameList.Count)
                 return _categoryDisplayNameList[currentIndex];
             else
                 return "";
