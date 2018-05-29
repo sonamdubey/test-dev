96e3ac7f76eca3720db0afdb825ca7de0934a3c6
mayurisakpal <mayuri.sakpal@carwale.com>
2018-05-29 10:48:20 +0530

    Added mobile version dropdown

---
+++ BikeWale.UI/src/version-dropdown.js
@@ -0,0 +1,4 @@
+﻿$('#version').on('click', function () {
+    $(".version-dropdown__submenu, .version-dropdown__tabs").toggleClass("version-active");
+    $('html, body').animate({ scrollTop: $('#version').offset().top - 44 }, 'slow');
+});
