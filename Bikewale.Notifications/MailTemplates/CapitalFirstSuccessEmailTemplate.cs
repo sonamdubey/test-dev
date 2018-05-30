7451754fc8a830defaf34d6cf7a954d89b999b0b
snehal.dange <snehal.dange@carwale.com>
2018-05-30 16:09:00 +0530

    Added comma for mail template for capital first
    Signed-off-by: snehal.dange <snehal.dange@carwale.com>

--- Bikewale.Notifications/MailTemplates/CapitalFirstSuccessEmailTemplate.cs
+++ Bikewale.Notifications/MailTemplates/CapitalFirstSuccessEmailTemplate.cs
@@ -11,7 +11,7 @@ namespace Bikewale.Notifications.MailTemplates
         private readonly string emailHtml;
         public CapitalFirstSuccessEmailTemplate(string bikeName, string saleOfficerName, string salesOfficerNumber)
         {
-            emailHtml = string.Format(@"<div><p>Thank you for expressing interest in availing a loan for {0}. We have shared your details with Capital First, who is our finance partner.</p><p></p><p>The Capital First representative {1} will reach out to you for further steps. Alternatively, you can reach out to {1} at {2}.</p><p></p><p>In case of any concerns, please reach out to us at contact@bikewale.com</p><p></p><p>Regards,</p><p>Team BikeWale</p></div>", bikeName, saleOfficerName, salesOfficerNumber);
+            emailHtml = string.Format(@"<div><p>Thank you for expressing interest in availing a loan for {0}. We have shared your details with Capital First, who is our finance partner.</p><p></p><p>The Capital First representative, {1}, will reach out to you for further steps. Alternatively, you can reach out to {1} at {2}.</p><p></p><p>In case of any concerns, please reach out to us at contact@bikewale.com</p><p></p><p>Regards,</p><p>Team BikeWale</p></div>", bikeName, saleOfficerName, salesOfficerNumber);
         }
 
         public override string ComposeBody()
