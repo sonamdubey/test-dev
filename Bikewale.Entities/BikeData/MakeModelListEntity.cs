8f543404b48aa4b0f685ff29e6282bb9286ba3ca
cw-ashutoshsharma <ashutosh.sharma@carwale.com>
2018-06-19 15:38:43 +0530

    MakeModelListEntity made serializable

--- Bikewale.Entities/BikeData/MakeModelListEntity.cs
+++ Bikewale.Entities/BikeData/MakeModelListEntity.cs
@@ -1,12 +1,17 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Bikewale.Entities.BikeData
 {
+    [Serializable, DataContract]
     public class MakeModelListEntity
     {
+        [DataMember]
         public BikeMakeEntityBase MakeBase { get; set; }
+        [DataMember]
         public IEnumerable<BikeModelEntityBase> ModelBase { get; set; }
     }
 }
