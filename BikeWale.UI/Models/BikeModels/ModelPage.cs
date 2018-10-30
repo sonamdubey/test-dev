77afb241ed5695efdffc9dfd3f8980414c6e77e2
Rajan Chauhan <33890781+cw-rajanchauhan@users.noreply.github.com>
2018-10-30 10:52:15 +0530

    Extension of Nearly all India campaign (#235)

--- BikeWale.UI/Models/BikeModels/ModelPage.cs
+++ BikeWale.UI/Models/BikeModels/ModelPage.cs
@@ -333,7 +333,7 @@ namespace Bikewale.Models.BikeModels
             {
                 _objData.IsEditCityOption = cookieValue >= 41 && cookieValue <= 50;
                 _objData.IsAnimatedCTA = cookieValue > 10;
-                _objData.IsNearlyAllIndiaCampaign = cookieValue > 10 && cookieValue <= 20 && _objData.ModelId == 78; // Test for Classic 350
+                _objData.IsNearlyAllIndiaCampaign = cookieValue <= 80 && _objData.ModelId == 78; // Test for Classic 350
                 _objData.IsNonAnimatedCTA = cookieValue > 5 && cookieValue <= 10;
                 _objData.UpfrontLoanCampaign = cookieValue > 20 && cookieValue <= 30 && (_objData.EMICalculator != null && (_objData.EMICalculator.ESEMICampaign != null || _objData.EMICalculator.IsPremiumDealer));
                 _objData.IsOffersShownOnLeadPopup = cookieValue <= 90;
