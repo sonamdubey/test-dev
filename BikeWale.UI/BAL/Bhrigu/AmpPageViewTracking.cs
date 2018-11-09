1ff7605ade0b2cb58c588b23cfd61c54e65b53dd
CW_prabhupuredla <35324834+CW-prabhupuredla@users.noreply.github.com>
2018-11-09 14:35:31 +0530

    amp page views and sessions are tracked in the bhrigu (#294)

---
+++ BikeWale.UI/BAL/Bhrigu/AmpPageViewTracking.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Specialized;
+using Bikewale.Entities.Tracking;
+using Bikewale.BAL.ApiGateway.Adapters.Bhrigu;
+using Bikewale.BAL.ApiGateway.ApiGatewayHelper;
+using Microsoft.Practices.Unity;
+using Bikewale.Notifications;
+using System.Web.Hosting;
+
+namespace Bikewale.BAL.Bhrigu
+{
+    /// <summary>
+    /// Created By : Prabhu Puredla on 06 nov 2018
+    /// </summary>
+    public static class AmpPageViewTracking
+    {
+        static IApiGatewayCaller _apiGatewayCaller;
+        static readonly IUnityContainer _container;
+
+        static AmpPageViewTracking()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IApiGatewayCaller, ApiGatewayCaller>();
+            _apiGatewayCaller = _container.Resolve<IApiGatewayCaller>();
+        }
+        /// <summary>
+        /// Created By : Prabhu Puredla on 06 nov 2018
+        /// Description : Function to call api gateway
+        /// </summary>
+        /// <param name="nvc"></param>
+        private static void CallBhriguTracker(NameValueCollection nvc)
+        {
+            BhriguTrackEntity objTrackingData = FormatTrackingData(nvc);
+            AddTrackEventAdapter adapter = new AddTrackEventAdapter();
+            adapter.AddApiGatewayCall(_apiGatewayCaller, objTrackingData);
+            _apiGatewayCaller.Call();
+        }
+
+        /// <summary>
+        /// Description : Function to push data to bhrigu through adapter
+        /// </summary>
+        /// <param name="nvc"></param>
+        public static void PushDataToBhrigu(NameValueCollection nvc)
+        {
+            try
+            {
+                if (nvc != null && !String.IsNullOrEmpty(nvc["cookieId"]))
+                {
+                    HostingEnvironment.QueueBackgroundWorkItem(f => CallBhriguTracker(nvc));
+                }         
+            }
+            catch(Exception ex)
+            {
+                ErrorClass.LogError(ex, "Bikewale.BAL.Bhrigu.LeadTracking PushDataToBhrigu");
+            }
+        }
+
+        /// <summary>
+        /// Description : Function to convert NameValueCollection to BhriguTrackEntity
+        /// </summary>
+        /// <param name="nvc"></param>
+        /// <returns>BhriguTrackEntity</returns>
+        private static BhriguTrackEntity FormatTrackingData(NameValueCollection nvc)
+        {
+            BhriguTrackEntity objTrackingData = new BhriguTrackEntity();
+
+            objTrackingData.Category = nvc["category"];
+            objTrackingData.Action = nvc["action"];
+            objTrackingData.Label = nvc["label"];
+            objTrackingData.CookieId = nvc["cookieId"];
+            objTrackingData.SessionId = nvc["sessionId"];
+            objTrackingData.PageUrl = nvc["pageUrl"];
+            objTrackingData.QueryString = nvc["queryString"];
+            objTrackingData.Cookie = String.Format("BWC={0}; _cwv={1} _bwtest={2}; location={3};_bwutmz={4};", 
+                nvc["cookieId"], nvc["sessionId"], nvc["_bwtest"], nvc["location"], nvc["_bwutmz"]);                 
+            objTrackingData.ClientIP = nvc["clientIP"];
+            objTrackingData.UserAgent = nvc["userAgent"];
+            objTrackingData.Referrer = nvc["referrer"];
+
+            return objTrackingData;
+        }
+    }
+}
---
+++ BikeWale.UI/Services/Controllers/PageViewController.cs
@@ -0,0 +1,50 @@
+﻿using Bikewale.BAL.Bhrigu;
+using Bikewale.Service.Utilities;
+using Bikewale.Utility;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Bikewale.Services.Controllers
+{
+    public class PageViewController : CompressionApiController//ApiController
+    {
+        private static HashSet<string> _ampCors = new HashSet<string> { "https://www-bikewale-com.cdn.ampproject.org", "https://www-bikewale-com.amp.cloudflare.com", "https://cdn.ampproject.org", "https://www.bikewale.com" };
+        
+        [HttpGet, Route("api/trackamppageview/"), EnableCors("https://www-bikewale-com.cdn.ampproject.org, https://www-bikewale-com.amp.cloudflare.com, https://cdn.ampproject.org,https://www.bikewale.com", "*", "GET")]
+        public IHttpActionResult TrackAmpPageView()
+        {
+            string ampSourceOrigin = HttpUtility.ParseQueryString(Request.RequestUri.Query)["__amp_source_origin"];
+            string ampOrigin = Request.Headers.Contains("Origin") ? Request.Headers.GetValues("Origin").FirstOrDefault() : string.Empty;
+
+
+            //if (!string.IsNullOrEmpty(ampSourceOrigin) && _ampCors.Contains(ampOrigin))
+            //{
+            BWCookies.AddAmpHeaders(ampSourceOrigin, ampOrigin, true);
+            //}
+            NameValueCollection objNVC = new NameValueCollection();
+            var cookies = HttpContext.Current.Response.Cookies;
+
+            objNVC.Add("category", "BWPageViews");
+            objNVC.Add("action", string.Empty);
+            objNVC.Add("label", "IsAmp=true");
+            objNVC.Add("cookieId", cookies["BWC"] != null ? cookies["BWC"].Value : string.Empty);
+            objNVC.Add("sessionId", cookies["_cwv"] != null ? cookies["_cwv"].Value : string.Empty);
+            objNVC.Add("pageUrl", HttpContext.Current.Request.Url.AbsoluteUri);
+            objNVC.Add("queryString", !String.IsNullOrEmpty(ampSourceOrigin) && ampSourceOrigin.Contains("?") ? ampSourceOrigin.Split('?')[1].Replace('&', '|') : String.Empty);
+            objNVC.Add("clientIP", CurrentUser.GetClientIP());
+            objNVC.Add("userAgent", HttpContext.Current.Request.UserAgent);
+            objNVC.Add("referrer", HttpContext.Current.Request.UrlReferrer.ToString());
+            objNVC.Add("_bwtest", cookies["_bwtest"] != null ? cookies["_bwtest"].Value : string.Empty);
+            objNVC.Add("location", HttpContext.Current.Request.Cookies["location"] != null ? HttpContext.Current.Request.Cookies["location"].Value : string.Empty);
+            objNVC.Add("_bwutmz", cookies["_bwutmz"] != null ? cookies["_bwutmz"].Value : string.Empty);
+
+            AmpPageViewTracking.PushDataToBhrigu(objNVC);
+            return Ok();
+        }
+    }
+}
